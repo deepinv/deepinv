7e3c267402fd421d63d01b3b3656059f39e2db09
samuro95 <huraultsamuel@gmail.com>
2023-02-24 10:57:22 +0100

    demo_unroll

--- examples/demo_unrolled_pnp.py
+++ examples/demo_unrolled_pnp.py
@@ -3,10 +3,11 @@ import deepinv as dinv
 import torch
 from torch.utils.data import DataLoader
 from deepinv.pnp.denoiser import Denoiser
-from deepinv.optim.data_fidelity import DataFidelity
+from deepinv.optim.data_fidelity import *
 from deepinv.pnp.pnp import PnP
-from deepinv.pnp.red import RED
-from deepinv.training_utils import train,test
+from deepinv.optim.fixed_point import FixedPoint
+from deepinv.optim.optim_iterator import *
+from deepinv.training_utils import test, train
 from torchvision import datasets, transforms
 import os
 
@@ -27,7 +28,10 @@ sigma_denoiser = sigma_k*noise_level_img
 max_iter = 6
 im_size = 256
 epochs = 2
-
+max_iter = 50
+crit_conv = 1e-5
+verbose = True
+early_stop = True 
 
 if problem == 'CS':
     p = dinv.physics.CompressedSensing(m=300, img_shape=(1, 28, 28), device=dinv.device)
@@ -45,7 +49,7 @@ elif problem == 'deblur':
 else:
     raise Exception("The inverse problem chosen doesn't exist")
 
-data_fidelity = DataFidelity(type='L2')
+data_fidelity = L2()
 
 val_transform = transforms.Compose([
             transforms.CenterCrop(im_size),
@@ -69,18 +73,21 @@ denoiser = Denoiser(denoiser_name=denoiser_name, device=dinv.device, n_channels=
 
 # pnp_algo = 'HQS'
 # pnp = PnP(denoiser=denoiser, sigma_denoiser=sigma_denoiser, algo_name=pnp_algo, data_fidelity=data_fidelity, max_iter=max_iter, stepsize=stepsize, device=dinv.device, unroll=True)
-pnp_algo = 'PGD'
-pnp = RED(denoiser=denoiser, sigma_denoiser=sigma_denoiser, algo_name=pnp_algo, data_fidelity=data_fidelity, max_iter=max_iter, stepsize=stepsize, device=dinv.device, unroll=True)
+
+PnP_module = PnP(denoiser=denoiser, max_iter=max_iter, sigma_denoiser=sigma_denoiser, stepsize=stepsize, unroll=True, weight_tied=True)
+iterator = PGD(prox_g=PnP_module.prox_g, data_fidelity=data_fidelity, stepsize=stepsize, device=dinv.device, update_stepsize = PnP_module.update_stepsize)
+FP = FixedPoint(iterator, max_iter=max_iter, early_stop=early_stop, crit_conv=crit_conv,verbose=verbose)
+model = lambda x,physics : FP(x, x, physics) # FP forward arguments are init, input, physics  
 
 # choose training losses
 losses = []
 losses.append(dinv.loss.SupLoss(metric=dinv.metric.mse()))
 
 # choose optimizer and scheduler
-optimizer = torch.optim.Adam(pnp.parameters(), lr=1e-4, weight_decay=1e-8)
+optimizer = torch.optim.Adam(PnP_module.parameters(), lr=1e-4, weight_decay=1e-8)
 scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=int(epochs*.8))
 
-train(model=pnp,
+train(model=model,
         train_dataloader=dataloader,
         epochs=epochs,
         scheduler=scheduler,
