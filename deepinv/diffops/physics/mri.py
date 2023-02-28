61b2faa0d9918c21fbb6896ec5738dfd8dc23c28
Dongdong Chen <43349309+edongdongchen@users.noreply.github.com>
2023-02-28 16:02:59 +0800

    Update mri.py
    update mri.py

--- deepinv/diffops/physics/mri.py
+++ deepinv/diffops/physics/mri.py
@@ -4,16 +4,15 @@ from fastmri.data import transforms as T
 from deepinv.diffops.physics.forward import Physics
 
 class MRI(Physics):
-    '''
-    TODO
+    r'''
+    Undersampled FFT operator for MRI image reconstruction problems.
+    
+    dependency: 'fastmri' library
     '''
     def __init__(self, acceleration=4, device='cpu', **kwargs):
         '''
-
-        TODO
-
-        :param acceleration: 
-        :param device: 
+        :param acceleration: (int), downsampling factor, 1/acceleration measurements will be sampled and used.
+        :param device:  (str) options = 'cpu', 'cuda=0'
         '''
         super().__init__(**kwargs)
         mask = torch.load(f'../physics/mask_{acceleration}x.pth.tar')['mask']
@@ -27,21 +26,31 @@ class MRI(Physics):
 
 
     def forward(self, x):
+        # fft
         y = fastmri.fft2c(x.permute(0, 2, 3, 1))
+        # add noise
         y = self.noise(y)
+        # sampling by masking
         y, _ = T.apply_mask(y, self.mask_func)
         return y.permute(0,3,1,2)
 
     def apply_mask(self, y):
+        # sampling by masking
         y, _ = T.apply_mask(y, self.mask_func)
         return y
 
-    def A(self, x, add_noise=False):
+    def A(self, x):
+        # fft
         y = fastmri.fft2c(x.permute(0, 2, 3, 1))
-        # no noise
+        # sampling by masking
         y, _ = T.apply_mask(y, self.mask_func)
         return y.permute(0, 3, 1, 2)
 
     def A_dagger(self, y):
+        # ifft 
         x = fastmri.ifft2c(y.permute(0, 2, 3, 1))
         return x.permute(0, 3, 1, 2)
+    
+    def A_adjoint(self, y):
+        # ifft
+        return self.A_dagger(y)
