0fff3494c359a756b6df07e242e85648a335dc7d
Julián Tachella <tachellajulian@gmail.com>
2023-03-24 17:07:30 +0100

    fixed small bugs

--- deepinv/models/dncnn.py
+++ deepinv/models/dncnn.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+import torch
 from .denoiser import register
 
 @register('dncnn')
--- deepinv/optim/optim_iterators/gradient_descent.py
+++ deepinv/optim/optim_iterators/gradient_descent.py
@@ -2,13 +2,13 @@ import torch
 import torch.nn as nn
 from .optim_iterator import OptimIterator
 
-class GDIteration(OptimIterator):  # TODO
-
-    def __init__(self, **kwargs):
-        super(GD, self).__init__(**kwargs)
-
-    def forward(self, x, it, y, physics):
-        x = x[0]
-        x = x - self.stepsize[it] * (
-                    self.lamb * self.data_fidelity.grad(x, y, physics) + self.grad_g(x, g_param[it], it))
-        return (x, )
+# class GDIteration(OptimIterator):  # TODO
+#
+#     def __init__(self, **kwargs):
+#         super(GD, self).__init__(**kwargs)
+#
+#     def forward(self, x, it, y, physics):
+#         x = x[0]
+#         x = x - self.stepsize[it] * (
+#                     self.lamb * self.data_fidelity.grad(x, y, physics) + self.grad_g(x, g_param[it], it))
+#         return (x, )
