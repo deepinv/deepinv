88dfe5e1eb1158d20e246671f1b99e2a78a590ba
Samuel Hurault <samuel.hurault@u-bordeaux.fr>
2023-04-04 12:55:23 +0200

    backtracking + DDPIR + GSPNP ok

--- examples/demo_dpir_deblur.py
+++ examples/demo_dpir_deblur.py
@@ -9,28 +9,28 @@ from deepinv.optim.data_fidelity import *
 from deepinv.optim.optimizers import *
 from deepinv.training_utils import test
 from torchvision import datasets, transforms
+from deepinv.utils.parameters import get_DPIR_params, initialize_stepsizes
+
+torch.manual_seed(0)
 
 num_workers = 4 if torch.cuda.is_available() else 0  # set to 0 if using small cpu, else 4
 problem = 'deblur'
 G = 1
-ckpt_path = None
+ckpt_path = '../checkpoints/drunet_color.pth'
 denoiser_name = 'drunet'
 batch_size = 1
-dataset = 'set3c'
-dataset_path = '../../datasets/set3c'
+dataset = 'starfish'
+dataset_path = f'../../datasets/{dataset}'
 dir = f'../datasets/{dataset}/{problem}/'
 noise_level_img = 0.03
-lamb = 10
-stepsize = 1.
-sigma_k = 2.
-sigma_denoiser = sigma_k*noise_level_img
 max_iter = 8
-crit_conv = 1e-3
 verbose = True
-early_stop = True 
+early_stop = False 
 n_channels = 3
-pretrain = True
 train = False
+crit_conv = 'residual'
+thres_conv = 1e-4
+img_size = 256
 
 if problem == 'CS':
     p = dinv.physics.CompressedSensing(m=300, img_shape=(1, 28, 28), device=dinv.device)
@@ -45,9 +45,10 @@ elif problem == 'blind_deblur':
     p = dinv.physics.BlindBlur(kernel_size=11)
 elif problem == 'deblur':
     kernels = hdf5storage.loadmat('../kernels/Levin09.mat')['kernels']
-    filter_np = kernels[0,0].astype(np.float64)
+    filter_np = kernels[0,1].astype(np.float64)
     filter_torch = torch.from_numpy(filter_np).unsqueeze(0).unsqueeze(0)
-    p = dinv.physics.Blur(filter=filter_torch, device=dinv.device, noise_model = dinv.physics.GaussianNoise(sigma=noise_level_img))
+    p = dinv.physics.BlurFFT(img_size = (3,img_size,img_size), filter=filter_torch, device=dinv.device, noise_model = dinv.physics.GaussianNoise(sigma=noise_level_img))
+    #p = dinv.physics.Blur(filter=filter_torch, device=dinv.device, noise_model = dinv.physics.GaussianNoise(sigma=noise_level_img))
 else:
     raise Exception("The inverse problem chosen doesn't exist")
 
@@ -58,7 +59,7 @@ val_transform = transforms.Compose([
  ])
 dataset = datasets.ImageFolder(root=dataset_path, transform=val_transform)
 dinv.datasets.generate_dataset(train_dataset=dataset, test_dataset=None,
-                               physics=p, device=dinv.device, save_dir=dir, max_datapoints=5,
+                               physics=p, device=dinv.device, save_dir=dir, max_datapoints=3,
                                num_workers=num_workers)
 dataset = dinv.datasets.HDF5Dataset(path=f'{dir}/dinv_dataset0.h5', train=True)
 dataloader = DataLoader(dataset, batch_size=batch_size, num_workers=num_workers, shuffle=False)
@@ -68,30 +69,18 @@ model_spec = {'name': denoiser_name,
               'args': {
                     'in_channels':n_channels+1, 
                     'out_channels':n_channels,
-                    'ckpt_path': ckpt_path,
-                    'pretrain':pretrain, 
+                    'pretrained':ckpt_path, 
                     'train': False, 
                     'device':dinv.device
                     }}
 
-# STEP 2: Defining the model
-def get_rho_sigma(sigma=2.55/255, iter_num=8, modelSigma1=49.0, modelSigma2=2.55, w=1.0):
-    '''
-    One can change the sigma to implicitly change the trade-off parameter
-    between fidelity term and prior term
-    '''
-    modelSigmaS = np.logspace(np.log10(modelSigma1), np.log10(modelSigma2), iter_num).astype(np.float32)
-    modelSigmaS_lin = np.linspace(modelSigma1, modelSigma2, iter_num).astype(np.float32)
-    sigmas = list((modelSigmaS*w+modelSigmaS_lin*(1-w))/255.)
-    rhos = list(map(lambda x: 0.23*(sigma**2)/(x**2), sigmas))
-    return rhos, sigmas
 
-rhos, sigmas = get_rho_sigma(sigma=max(0.255/255., noise_level_img), iter_num=max_iter, modelSigma1=49.0, modelSigma2=2.55, w=1.0)
+lamb, sigma_denoiser, stepsize, max_iter = get_DPIR_params(noise_level_img)
+stepsize, sigma_denoiser = initialize_stepsizes(stepsize, sigma_denoiser, max_iter)
 
-prox_g = ProxDenoiser(model_spec, sigma_denoiser=sigmas, stepsize=rhos, max_iter=max_iter)
-algo_name = 'HQS'
-model = Optim(algo_name, prox_g=prox_g, data_fidelity=data_fidelity, stepsize=prox_g.stepsize, device=dinv.device,
-             g_param=prox_g.sigma_denoiser, max_iter=max_iter, crit_conv=1e-4, verbose=True)
+prox_g = ProxDenoiser(model_spec)
+model = Optim(algo_name = 'HQS', prox_g=prox_g, g_first = False, data_fidelity=data_fidelity, lamb=lamb, stepsize=stepsize, device=dinv.device,
+             g_param=sigma_denoiser, early_stop=early_stop, max_iter=max_iter, crit_conv=crit_conv, thres_conv=thres_conv, backtracking=False, F_fn=None, verbose=True)
 
 test(model=model,  # Safe because it has forward
     test_dataloader=dataloader,
---
+++ examples/demo_gspnp_sr.py
@@ -0,0 +1,87 @@
+import numpy as np
+import deepinv as dinv
+import hdf5storage
+import torch
+import os
+from torch.utils.data import DataLoader
+from deepinv.models.denoiser import ProxDenoiser, ScoreDenoiser
+from deepinv.optim.data_fidelity import *
+from deepinv.optim.optimizers import *
+from deepinv.training_utils import test
+from torchvision import datasets, transforms
+from deepinv.utils.parameters import get_GSPnP_params, initialize_stepsizes
+
+torch.manual_seed(0)
+
+num_workers = 4 if torch.cuda.is_available() else 0  # set to 0 if using small cpu, else 4
+
+train = False
+
+ckpt_path = '../checkpoints/GSDRUNet.ckpt'
+denoiser_name = 'gsdrunet'
+batch_size = 1
+n_channels = 3
+pretrain = True
+
+problem = 'super_resolution'
+G = 1
+img_size = 256
+
+dataset = 'starfish'
+dataset_path = f'../../datasets/{dataset}'
+save_dir = f'/beegfs/shurault/deepinv/datasets/{dataset}/{problem}/'
+
+noise_level_img = 0.03
+
+crit_conv = 'cost'
+thres_conv = 1e-5
+early_stop = True
+verbose = True
+
+k_index = 1
+
+#TODO : add kernel downloading code
+kernels = hdf5storage.loadmat('/beegfs/shurault/deepinv/kernels/Levin09.mat')['kernels']
+filter_np = kernels[0,k_index].astype(np.float64)
+filter_torch = torch.from_numpy(filter_np).unsqueeze(0).unsqueeze(0)
+
+p = dinv.physics.BlurFFT(img_size = (3,img_size,img_size), filter=filter_torch, device=dinv.device, noise_model = dinv.physics.GaussianNoise(sigma=noise_level_img))
+data_fidelity = L2()
+
+val_transform = transforms.Compose([
+            transforms.ToTensor(),
+ ])
+
+dataset = datasets.ImageFolder(root=dataset_path, transform=val_transform)
+dinv.datasets.generate_dataset(train_dataset=dataset, test_dataset=None,
+                               physics=p, device=dinv.device, save_dir=save_dir, max_datapoints=1,
+                               num_workers=num_workers)
+dataset = dinv.datasets.HDF5Dataset(path=f'{save_dir}/dinv_dataset0.h5', train=True)
+dataloader = DataLoader(dataset, batch_size=batch_size, num_workers=num_workers, shuffle=False)
+
+model_spec = {'name': denoiser_name,
+              'args': {
+                    'in_channels':n_channels+1, 
+                    'out_channels':n_channels,
+                    'pretrained': ckpt_path, 
+                    'train': False, 
+                    'device':dinv.device
+                    }}
+
+lamb, sigma_denoiser, stepsize, max_iter = get_GSPnP_params(problem, noise_level_img, k_index)
+stepsize, sigma_denoiser = initialize_stepsizes(stepsize, sigma_denoiser, max_iter)
+grad_g = ScoreDenoiser(model_spec)
+F_fn = lambda x,y,physics,it : lamb*data_fidelity.f(physics.A(x), y) + grad_g.denoiser.potential(x,sigma_denoiser[it])
+model = Optim(algo_name = 'PGD', grad_g=grad_g, g_first = True, data_fidelity=data_fidelity, lamb=lamb, stepsize=stepsize, device=dinv.device,
+             g_param=sigma_denoiser, early_stop = early_stop, max_iter=max_iter, crit_conv=crit_conv, thres_conv=thres_conv, backtracking=False, 
+             F_fn=F_fn, return_dual=True, verbose=True)
+
+test(model=model,  # Safe because it has forward
+    test_dataloader=dataloader,
+    physics=p,
+    device=dinv.device,
+    plot=True,
+    plot_input=True,
+    save_folder='../results/',
+    save_plot_path='../results/results_pnp.png',
+    verbose=verbose)
