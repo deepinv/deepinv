380f7ec2f8afc6bb329a4699ace5c93e1231fdba
Samuel Hurault <samuel.hurault@u-bordeaux.fr>
2023-04-10 21:05:17 +0200

    super resolution working

--- examples/demo_gspnp_deblur.py
+++ examples/demo_gspnp_deblur.py
@@ -4,7 +4,7 @@ import hdf5storage
 import torch
 import os
 from torch.utils.data import DataLoader
-from deepinv.models.denoiser import ProxDenoiser, REDDenoiser
+from deepinv.models.denoiser import REDDenoiser
 from deepinv.optim.data_fidelity import *
 from deepinv.optim.optimizers import *
 from deepinv.training_utils import test
@@ -14,34 +14,29 @@ from deepinv.utils.parameters import get_GSPnP_params
 torch.manual_seed(0)
 
 num_workers = 4 if torch.cuda.is_available() else 0  # set to 0 if using small cpu, else 4
-
 train = False
-
 ckpt_path = '../checkpoints/GSDRUNet.ckpt'
+if not os.path.exists(ckpt_path) : 
+    ckpt_path = None
 denoiser_name = 'gsdrunet'
 batch_size = 1
 n_channels = 3
 pretrain = True
-
 problem = 'deblur'
 G = 1
 img_size = 256
-
-dataset = 'starfish'
-dataset_path = f'../../datasets/{dataset}'
-save_dir = f'/beegfs/shurault/deepinv/datasets/{dataset}/{problem}/'
-
+dataset = 'set3c'
+dataset_path = f'../datasets/{dataset}/images'
+save_dir = f'../datasets/{dataset}/{problem}/'
 noise_level_img = 0.03
-
 crit_conv = 'cost'
 thres_conv = 1e-5
 early_stop = True
 verbose = True
-
 k_index = 1
 
 #TODO : add kernel downloading code
-kernels = hdf5storage.loadmat('/beegfs/shurault/deepinv/kernels/Levin09.mat')['kernels']
+kernels = hdf5storage.loadmat('../kernels/Levin09.mat')['kernels']
 filter_np = kernels[0,k_index].astype(np.float64)
 filter_torch = torch.from_numpy(filter_np).unsqueeze(0).unsqueeze(0)
 
@@ -54,7 +49,7 @@ val_transform = transforms.Compose([
 
 dataset = datasets.ImageFolder(root=dataset_path, transform=val_transform)
 dinv.datasets.generate_dataset(train_dataset=dataset, test_dataset=None,
-                               physics=p, device=dinv.device, save_dir=save_dir, max_datapoints=1,
+                               physics=p, device=dinv.device, save_dir=save_dir, max_datapoints=3,
                                num_workers=num_workers)
 dataset = dinv.datasets.HDF5Dataset(path=f'{save_dir}/dinv_dataset0.h5', train=True)
 dataloader = DataLoader(dataset, batch_size=batch_size, num_workers=num_workers, shuffle=False)
--- examples/demo_gspnp_sr.py
+++ examples/demo_gspnp_sr.py
@@ -4,48 +4,44 @@ import hdf5storage
 import torch
 import os
 from torch.utils.data import DataLoader
-from deepinv.models.denoiser import ProxDenoiser, ScoreDenoiser
+from deepinv.models.denoiser import REDDenoiser
 from deepinv.optim.data_fidelity import *
 from deepinv.optim.optimizers import *
 from deepinv.training_utils import test
 from torchvision import datasets, transforms
-from deepinv.utils.parameters import get_GSPnP_params, initialize_stepsizes
+from deepinv.utils.parameters import get_GSPnP_params
 
 torch.manual_seed(0)
 
 num_workers = 4 if torch.cuda.is_available() else 0  # set to 0 if using small cpu, else 4
-
 train = False
-
 ckpt_path = '../checkpoints/GSDRUNet.ckpt'
+if not os.path.exists(ckpt_path) : 
+    ckpt_path = None
 denoiser_name = 'gsdrunet'
 batch_size = 1
 n_channels = 3
 pretrain = True
-
 problem = 'super_resolution'
 G = 1
 img_size = 256
-
-dataset = 'starfish'
-dataset_path = f'../../datasets/{dataset}'
-save_dir = f'/beegfs/shurault/deepinv/datasets/{dataset}/{problem}/'
-
+dataset = 'set3c'
+dataset_path = f'../datasets/{dataset}/images'
+save_dir = f'../datasets/{dataset}/{problem}/'
 noise_level_img = 0.03
-
 crit_conv = 'cost'
 thres_conv = 1e-5
 early_stop = True
 verbose = True
-
-k_index = 1
+k_index = 3
+factor = 2
 
 #TODO : add kernel downloading code
-kernels = hdf5storage.loadmat('/beegfs/shurault/deepinv/kernels/Levin09.mat')['kernels']
+kernels = hdf5storage.loadmat('../kernels/kernels_12.mat')['kernels']
 filter_np = kernels[0,k_index].astype(np.float64)
 filter_torch = torch.from_numpy(filter_np).unsqueeze(0).unsqueeze(0)
 
-p = dinv.physics.BlurFFT(img_size = (3,img_size,img_size), filter=filter_torch, device=dinv.device, noise_model = dinv.physics.GaussianNoise(sigma=noise_level_img))
+p = dinv.physics.Downsampling(img_size = (n_channels,img_size,img_size), factor=factor, filter=filter_torch, device=dinv.device, noise_model = dinv.physics.GaussianNoise(sigma=noise_level_img))
 data_fidelity = L2()
 
 val_transform = transforms.Compose([
@@ -54,7 +50,7 @@ val_transform = transforms.Compose([
 
 dataset = datasets.ImageFolder(root=dataset_path, transform=val_transform)
 dinv.datasets.generate_dataset(train_dataset=dataset, test_dataset=None,
-                               physics=p, device=dinv.device, save_dir=save_dir, max_datapoints=1,
+                               physics=p, device=dinv.device, save_dir=save_dir, max_datapoints=3,
                                num_workers=num_workers)
 dataset = dinv.datasets.HDF5Dataset(path=f'{save_dir}/dinv_dataset0.h5', train=True)
 dataloader = DataLoader(dataset, batch_size=batch_size, num_workers=num_workers, shuffle=False)
@@ -69,11 +65,11 @@ model_spec = {'name': denoiser_name,
                     }}
 
 lamb, sigma_denoiser, stepsize, max_iter = get_GSPnP_params(problem, noise_level_img, k_index)
-stepsize, sigma_denoiser = initialize_stepsizes(stepsize, sigma_denoiser, max_iter)
-grad_g = ScoreDenoiser(model_spec)
-F_fn = lambda x,y,physics,it : lamb*data_fidelity.f(physics.A(x), y) + grad_g.denoiser.potential(x,sigma_denoiser[it])
-model = Optim(algo_name = 'PGD', grad_g=grad_g, g_first = True, data_fidelity=data_fidelity, lamb=lamb, stepsize=stepsize, device=dinv.device,
-             g_param=sigma_denoiser, early_stop = early_stop, max_iter=max_iter, crit_conv=crit_conv, thres_conv=thres_conv, backtracking=False, 
+params_algo={'stepsize': stepsize, 'g_param': sigma_denoiser, 'lambda': lamb}
+grad_g = REDDenoiser(model_spec)
+F_fn = lambda x,cur_params,y,physics : lamb*data_fidelity.f(physics.A(x), y) + grad_g.denoiser.potential(x,cur_params['g_param'])
+model = Optim(algo_name = 'PGD', grad_g=grad_g, g_first = True, data_fidelity=data_fidelity, device=dinv.device,
+             params_algo=params_algo, early_stop = early_stop, max_iter=max_iter, crit_conv=crit_conv, thres_conv=thres_conv, backtracking=True, 
              F_fn=F_fn, return_dual=True, verbose=True)
 
 test(model=model,  # Safe because it has forward
