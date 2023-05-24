d0a658cee2f4a9de1107285f72c706516dd32fab
Julián Tachella <tachellajulian@gmail.com>
2023-05-24 15:47:22 +0200

    made decolorize decomposable

--- deepinv/physics/range.py
+++ deepinv/physics/range.py
@@ -1,8 +1,8 @@
 import torch
-from deepinv.physics.forward import LinearPhysics
+from deepinv.physics.forward import DecomposablePhysics
 
 
-class Decolorize(LinearPhysics):
+class Decolorize(DecomposablePhysics):
     r"""
     Converts RGB images to grayscale.
 
@@ -13,12 +13,13 @@ class Decolorize(LinearPhysics):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.mask = 1.0
 
-    def A(self, x):
+    def V_adjoint(self, x):
         y = x[:, 0, :, :] * 0.2989 + x[:, 1, :, :] * 0.5870 + x[:, 2, :, :] * 0.1140
         return y.unsqueeze(1)
 
-    def A_adjoint(self, y):
+    def V(self, y):
         return torch.cat([y * 0.2989, y * 0.5870, y * 0.1140], dim=1)
 
 
@@ -26,30 +27,22 @@ class Decolorize(LinearPhysics):
 if __name__ == "__main__":
     device = "cuda:0"
 
+    import deepinv as dinv
     import matplotlib.pyplot as plt
     import torchvision
 
-    x = torchvision.io.read_image(
-        "../../../datasets/celeba/img_align_celeba/010214.jpg"
-    )
-    x = x.unsqueeze(0).float() / 256
+    dinv.device = "cpu"
 
-    pix = 128
-    factor = 3
-
-    x = x[:, :, :pix, :pix].to(device)
+    x = torchvision.io.read_image("../../datasets/celeba/img_align_celeba/085307.jpg")
+    x = x.unsqueeze(0).float().to(dinv.device) / 255
+    x = torchvision.transforms.Resize((128, 128))(x)
 
     physics = Decolorize()
 
     y = physics(x)
 
     print(physics.adjointness_test(x))
-    print(physics.power_method(x))
+    print(physics.compute_norm(x))
     xhat = physics.A_adjoint(y)
 
-    plt.imshow(x.squeeze(0).permute(1, 2, 0).cpu().numpy())
-    plt.show()
-    plt.imshow(y.squeeze(0).permute(1, 2, 0).cpu().numpy())
-    plt.show()
-    plt.imshow(xhat.squeeze(0).permute(1, 2, 0).cpu().numpy())
-    plt.show()
+    dinv.utils.plot([x, xhat, y])
