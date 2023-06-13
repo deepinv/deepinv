35d17f9aff3cefd8921c4c7dbbf61fb37a6338a6
Julián Tachella <tachellajulian@gmail.com>
2023-06-13 17:29:59 +0200

    added pansharpening + operator stacking and TensorLists

--- deepinv/utils/__init__.py
+++ deepinv/utils/__init__.py
@@ -2,4 +2,4 @@ from .logger import AverageMeter, ProgressMeter, get_timestamp
 from .nn import save_model, load_checkpoint, investigate_model
 from .metric import cal_psnr, cal_mse, cal_psnr_complex
 from .plotting import plot, im_save, torch2cpu, plot, make_grid, wandb_imgs
-from .nn import get_freer_gpu
+from .nn import get_freer_gpu, TensorList, rand_like, zeros_like, randn_like
