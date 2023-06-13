f568418835a58d16298adaa421555ce5059a4de3
Julián Tachella <tachellajulian@gmail.com>
2023-06-13 17:31:21 +0200

    added tensorlist

--- deepinv/utils/nn.py
+++ deepinv/utils/nn.py
@@ -3,6 +3,159 @@ import os
 import numpy as np
 
 
+class TensorList:
+    r"""
+
+    Represents a list of torch.Tensors with different shapes.
+    It allows to sum, flatten, append, etc. lists of tensors seamlessly, in a
+    similar fashion to torch.Tensor.
+
+    :param x: a list of torch.Tensor, a single torch.Tensor or a TensorList.
+    """
+
+    def __init__(self, x):
+        super().__init__()
+
+        if isinstance(x, list) or isinstance(x, TensorList):
+            self.x = list(x)
+        elif isinstance(x, torch.Tensor):
+            self.x = [x]
+        else:
+            raise TypeError("x must be a list of torch.Tensor or a single torch.Tensor")
+
+        self.shape = [xi.shape for xi in self.x]
+
+    def __len__(self):
+        r"""
+        Returns the number of tensors in the list.
+        """
+        return len(self.x)
+
+    def __getitem__(self, item):
+        r"""
+        Returns the ith tensor in the list.
+        """
+        return self.x[item]
+
+    def flatten(self):
+        r"""
+        Returns a torch.Tensor with a flattened version of the list of tensors.
+        """
+        return torch.cat([xi.flatten() for xi in self.x])
+
+    def append(self, other):
+        r"""
+        Appends a torch.Tensor or a list of torch.Tensor to the list.
+
+        """
+        if isinstance(other, list):
+            self.x += other
+        elif isinstance(other, TensorList):
+            self.x += other.x
+        elif isinstance(other, torch.Tensor):
+            self.x.append(other)
+        else:
+            raise TypeError(
+                "the appended item must be a list of torch.Tensor or a single torch.Tensor"
+            )
+        return self
+
+    def __add__(self, other):
+        r"""
+
+        Adds two TensorLists. The sizes of the tensor lists must match.
+
+        """
+        if not isinstance(other, list) and not isinstance(other, TensorList):
+            return TensorList([xi + other for xi in self.x])
+        else:
+            return TensorList([xi + otheri for xi, otheri in zip(self.x, other)])
+
+    def __mul__(self, other):
+        r"""
+
+        Multiply two TensorLists. The sizes of the tensor lists must match.
+
+        """
+        if not isinstance(other, list) and not isinstance(other, TensorList):
+            return TensorList([xi * other for xi in self.x])
+        else:
+            return TensorList([xi * otheri for xi, otheri in zip(self.x, other)])
+
+    def __truediv__(self, other):
+        r"""
+
+        Divide two TensorLists. The sizes of the tensor lists must match.
+
+        """
+        if not isinstance(other, list) and not isinstance(other, TensorList):
+            return TensorList([xi / other for xi in self.x])
+        else:
+            return TensorList([xi / otheri for xi, otheri in zip(self.x, other)])
+
+    def __neg__(self):
+        r"""
+
+        Negate a TensorList.
+        """
+        return TensorList([-xi for xi in self.x])
+
+    def __sub__(self, other):
+        r"""
+
+        Substract two TensorLists. The sizes of the tensor lists must match.
+
+        """
+        if not isinstance(other, list) and not isinstance(other, TensorList):
+            return TensorList([xi - other for xi in self.x])
+        else:
+            return TensorList([xi - otheri for xi, otheri in zip(self.x, other)])
+
+
+def randn_like(x):
+    r"""
+    Returns a :class:`deepinv.utils.TensorList` or ``torch.Tensor``
+    with the same type as x, filled with standard gaussian numbers.
+    """
+    if isinstance(x, torch.Tensor):
+        return torch.randn_like(x)
+    else:
+        return TensorList([torch.randn_like(xi) for xi in x])
+
+
+def rand_like(x):
+    r"""
+    Returns a :class:`deepinv.utils.TensorList` or ``torch.Tensor``
+    with the same type as x, filled with random uniform numbers in [0,1].
+    """
+    if isinstance(x, torch.Tensor):
+        return torch.rand_like(x)
+    else:
+        return TensorList([torch.rand_like(xi) for xi in x])
+
+
+def zeros_like(x):
+    r"""
+    Returns a :class:`deepinv.utils.TensorList` or ``torch.Tensor``
+    with the same type as x, filled with zeros.
+    """
+    if isinstance(x, torch.Tensor):
+        return torch.zeros_like(x)
+    else:
+        return TensorList([torch.zeros_like(xi) for xi in x])
+
+
+def ones_like(x):
+    r"""
+    Returns a :class:`deepinv.utils.TensorList` or ``torch.Tensor``
+    with the same type as x, filled with ones.
+    """
+    if isinstance(x, torch.Tensor):
+        return torch.ones_like(x)
+    else:
+        return TensorList([torch.ones_like(xi) for xi in x])
+
+
 def get_freer_gpu():
     """
     Returns the GPU device with the most free memory.
