57c8f4870ca85581fd88c4c99c81d26affb99835
tommoral <thomas.moreau.2010@gmail.com>
2023-10-25 09:57:37 +0200

    FIX blackify

--- deepinv/models/swinir.py
+++ deepinv/models/swinir.py
@@ -271,9 +271,7 @@ class SwinTransformerBlock(nn.Module):
             proj_drop=drop,
         )
 
-        self.drop_path = (
-            DropPath(drop_path) if drop_path > 0.0 else nn.Identity()
-        )
+        self.drop_path = DropPath(drop_path) if drop_path > 0.0 else nn.Identity()
         self.norm2 = norm_layer(dim)
         mlp_hidden_dim = int(dim * mlp_ratio)
         self.mlp = Mlp(
