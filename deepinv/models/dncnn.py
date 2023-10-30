87c37234ff49031f80919fcda72f1b227bc2985f
Matthieu Terris <31830373+matthieutrs@users.noreply.github.com>
2023-10-30 09:58:48 +0100

    init kaiming (#102)
    * init kaiming
    
    * changelog
    
    * fix memory

--- deepinv/models/dncnn.py
+++ deepinv/models/dncnn.py
@@ -94,6 +94,8 @@ class DnCNN(nn.Module):
             self.eval()
             for _, v in self.named_parameters():
                 v.requires_grad = False
+        else:
+            self.apply(weights_init_kaiming)
 
         if device is not None:
             self.to(device)
@@ -113,3 +115,16 @@ class DnCNN(nn.Module):
             x1 = self.nl_list[i + 1](x_l)
 
         return self.out_conv(x1) + x
+
+
+def weights_init_kaiming(m):
+    classname = m.__class__.__name__
+    if classname.find("Conv") != -1:
+        nn.init.kaiming_normal_(m.weight.data, a=0, mode="fan_in")
+    elif classname.find("Linear") != -1:
+        nn.init.kaiming_normal_(m.weight.data, a=0, mode="fan_in")
+    elif classname.find("BatchNorm") != -1:
+        m.weight.data.normal_(mean=0, std=math.sqrt(2.0 / 9.0 / 64.0)).clamp_(
+            -0.025, 0.025
+        )
+        nn.init.constant(m.bias.data, 0.0)
