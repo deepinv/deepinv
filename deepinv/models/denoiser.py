7a100e81400e1bebe9dfb77b7f43c5c5739a4b02
Julián Tachella <38355592+tachella@users.noreply.github.com>
2023-11-29 23:31:42 +0100

    fix mycorelinks (#117)
    * fix mycorelinks
    
    * black fix
    
    * black fix
    
    * fix demo dps

--- deepinv/models/denoiser.py
+++ deepinv/models/denoiser.py
@@ -3,4 +3,4 @@ import torch.nn as nn
 
 
 def online_weights_path():
-    return "https://mycore.core-cloud.net/index.php/s/9EzDqcJxQUJKYul/download?path=%2Fweights&files="
+    return "https://mycore.core-cloud.net/index.php/s/pT5krDvE4jSTDc9/download?path=%2Fweights&files="
