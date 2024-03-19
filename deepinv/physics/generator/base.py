dece541879b9875670343ceeb75a8e45b16f5e08
minh_hai_nguyen <nguyenhai7120qh@gmail.com>
2024-03-19 14:42:35 +0100

    psf generator

--- deepinv/physics/generator/base.py
+++ deepinv/physics/generator/base.py
@@ -2,22 +2,28 @@
 import torch
 import torch.nn as nn
 
+
 class Generator:
     r"""
     Base class for parameter generation of physics.
 
-    :param torch.Tensor params: the parameter of a physic, e.g., the filter of the Blur physic. 
+    :param torch.Tensor params: the parameter of a physic, e.g., the filter of the Blur physic.
     :param dict kwargs: default keyword arguments to be passed to :meth:`Generator` for generating new parameters.
     """
+
     def __init__(self, params: torch.Tensor, **kwargs) -> None:
         self.params = params
         self.kwargs = kwargs
-        
+        self.factory_kwargs = {"device": params.device, "dtype": params.dtype}
+        # Set attributes
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
     def step(self, *args, **kwargs):
         r"""
         Updates the parameter
         """
-        
+
         if not kwargs:
             self.kwargs = kwargs
 
@@ -30,23 +36,25 @@ class Generator:
         r"""
         Return new parameter
         """
-        return torch.rand_like(self.params)
+        return torch.zeros_like(self.params)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
+
     class Physic(nn.Module):
         def __init__(self, *args, **kwargs) -> None:
             super().__init__()
             # self.params = nn.Parameter(torch.tensor([1., 2., 3.]), requires_grad=False)
-            self.params = torch.tensor([1., 2., 3.])
+            self.params = torch.tensor([1.0, 2.0, 3.0])
             self.kwargs = kwargs
-        def forward(self, *args, **kwargs):
-            pass 
 
+        def forward(self, *args, **kwargs):
+            pass
 
     # %%
     P = Physic()
     print(P.params)
-    G = Generator(P.params)
+    G = Generator(P.params, l=1, n=2)
     G.step()
     print(P.params)
     # %%
