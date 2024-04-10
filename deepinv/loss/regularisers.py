b0fc8a9b14bec2e8fbdfeea89f359d934b4a0a70
minh_hai_nguyen <nguyenhai7120qh@gmail.com>
2024-04-10 11:50:09 +0200

    corrected doctest loss/regularisers.py

--- deepinv/loss/regularisers.py
+++ deepinv/loss/regularisers.py
@@ -27,16 +27,16 @@ class JacobianSpectralNorm(Loss):
 
         >>> import torch
         >>> from deepinv.loss.regularisers import JacobianSpectralNorm
+        >>> _ = torch.manual_seed(0)
+        >>> _ = torch.cuda.manual_seed(0)
         >>>
         >>> reg_l2 = JacobianSpectralNorm(max_iter=10, tol=1e-3, eval_mode=False, verbose=True)
-        >>>
         >>> A = torch.diag(torch.Tensor(range(1, 51)))  # creates a diagonal matrix with largest eigenvalue = 50
         >>> x = torch.randn_like(A).requires_grad_()
         >>> out = A @ x
-        >>>
         >>> regval = reg_l2(out, x)
         >>> print(regval) # returns approx 50
-
+        tensor([49.0202])
     """
 
     def __init__(self, max_iter=10, tol=1e-3, eval_mode=False, verbose=False):
