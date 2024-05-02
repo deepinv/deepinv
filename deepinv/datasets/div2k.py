2cb7d9188ea76fe7e0df0020533457d6a0cd23f5
MaximeSongIdris <maxime.song@idris.fr>
2024-05-02 19:36:38 +0200

    [Style] Apply black

--- deepinv/datasets/div2k.py
+++ deepinv/datasets/div2k.py
@@ -19,73 +19,87 @@ class DIV2K(torch.utils.data.Dataset):
 
     # https://data.vision.ee.ethz.ch/cvl/DIV2K/
     zipfile_urls = {
-        'DIV2K_train_HR.zip' : 'http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_HR.zip',
-        'DIV2K_valid_HR.zip' : 'http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_valid_HR.zip',
+        "DIV2K_train_HR.zip": "http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_HR.zip",
+        "DIV2K_valid_HR.zip": "http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_valid_HR.zip",
     }
 
     # for integrity of downloaded data
     checksums = {
-        'DIV2K_train_HR' : 'f9de9c251af455c1021017e61713a48b',
-        'DIV2K_valid_HR' : '542325e500b0a474c7ad18bae922da72',
+        "DIV2K_train_HR": "f9de9c251af455c1021017e61713a48b",
+        "DIV2K_valid_HR": "542325e500b0a474c7ad18bae922da72",
     }
 
-    def __init__(self, root: str, mode: str='train', download: bool=False, transform=None) -> None:
+    def __init__(
+        self, root: str, mode: str = "train", download: bool = False, transform=None
+    ) -> None:
         super().__init__()
 
         self.root = root
         self.mode = mode
         self.transform = transform
-        train_dir_path = os.path.join(self.root, 'DIV2K_train_HR')
-        valid_dir_path = os.path.join(self.root, 'DIV2K_valid_HR')
+        train_dir_path = os.path.join(self.root, "DIV2K_train_HR")
+        valid_dir_path = os.path.join(self.root, "DIV2K_valid_HR")
 
         # verify that dataset is available at self.root and use it
         if self._check_dataset_exists():
-            print(f'''
+            print(
+                f"""
             Dataset is available at `{self.root}`.
             `download` flag is not taken into account.
-            ''')
+            """
+            )
         # otherwise we try to download the whole dataset
         elif download:
             if not os.path.isdir(self.root):
                 os.makedirs(self.root)
             if os.path.exists(train_dir_path):
-                raise ValueError(f'''
+                raise ValueError(
+                    f"""
                 The train folder already exists,
                 thus the download is aborted.
                 Please set `download=False`
-                OR remove `{train_dir_path}`.''')
+                OR remove `{train_dir_path}`."""
+                )
 
             if os.path.exists(valid_dir_path):
-                raise ValueError(f'''
+                raise ValueError(
+                    f"""
                 The val folder already exists,
                 thus the download is aborted.
                 Please set `download=False`
-                OR remove `{valid_dir_path}`.''')
+                OR remove `{valid_dir_path}`."""
+                )
 
             for filename, url in self.zipfile_urls.items():
                 # download zipfile from the Internet and save it locally
-                download_zipfile(url=self.zipfile_urls[filename], save_path=os.path.join(self.root, filename))
+                download_zipfile(
+                    url=self.zipfile_urls[filename],
+                    save_path=os.path.join(self.root, filename),
+                )
                 # extract local zipfile
                 extract_zipfile(os.path.join(self.root, filename), self.root)
         # stop the execution since the dataset is not available and we didn't download it
         else:
-            raise RuntimeError(f'''
+            raise RuntimeError(
+                f"""
             Dataset not found at `{self.root}`.
             Please set `root` correctly (currently `root={self.root}`),
             AND check that `{train_dir_path}` contains ONLY the following files 0001.png, ..., 0800.png
                            `{valid_dir_path}` contains ONLY the followinf files 0801.png, ..., 0900.png
             OR set `download=True` (currently `download={download}`).
-            ''')
+            """
+            )
 
-        if self.mode == 'train':
+        if self.mode == "train":
             self.img_dir = train_dir_path
-        elif self.mode == 'val':
+        elif self.mode == "val":
             self.img_dir = valid_dir_path
         else:
-            raise ValueError(f'Expected `train` or `val` values for `mode` argument, instead got `{self.mode}`')
+            raise ValueError(
+                f"Expected `train` or `val` values for `mode` argument, instead got `{self.mode}`"
+            )
 
         self.img_list = os.listdir(self.img_dir)
-
 
     def __len__(self):
         return len(self.img_list)
@@ -116,5 +130,6 @@ class DIV2K(torch.utils.data.Dataset):
         if not data_dir_exist:
             return False
         return all(
-                calculate_md5_for_folder(os.path.join(self.root, split)) == checksum for split, checksum in self.checksums.items()
-            )
+            calculate_md5_for_folder(os.path.join(self.root, split)) == checksum
+            for split, checksum in self.checksums.items()
+        )
--- deepinv/datasets/utils.py
+++ deepinv/datasets/utils.py
@@ -12,8 +12,10 @@ def check_path_is_a_folder(folder_path: str) -> bool:
         return False
     # Check if everything inside of the directory is a file
     return all(
-            os.path.isfile(os.path.join(folder_path, filename)) for filename in os.listdir(folder_path)
+        os.path.isfile(os.path.join(folder_path, filename))
+        for filename in os.listdir(folder_path)
     )
+
 
 def calculate_md5(fpath: str, chunk_size: int = 1024 * 1024) -> str:
     """From https://github.com/pytorch/vision/blob/main/torchvision/datasets/utils.py#L35"""
@@ -22,6 +24,7 @@ def calculate_md5(fpath: str, chunk_size: int = 1024 * 1024) -> str:
         while chunk := f.read(chunk_size):
             md5.update(chunk)
     return md5.hexdigest()
+
 
 def calculate_md5_for_folder(folder_path: str):
     """Compute the hash of all files in a folder then compute the hash of the folder.
@@ -41,15 +44,16 @@ def download_zipfile(url, save_path):
     # `stream=True` to avoid loading in memory an entire file, instead get a chunk
     # useful when downloading huge file
     response = requests.get(url, stream=True)
-    with open(save_path, 'wb') as file:
+    with open(save_path, "wb") as file:
         # shutil.copyfileobj doesn't require the whole file in memory before writing in a file
         # https://requests.readthedocs.io/en/latest/user/quickstart/#raw-response-content
         shutil.copyfileobj(response.raw, file)
     del response
 
+
 def extract_zipfile(file_path, extract_dir):
     """Extract a local zipfile."""
     # Open the zip file
-    with zipfile.ZipFile(file_path, 'r') as zip_ref:
+    with zipfile.ZipFile(file_path, "r") as zip_ref:
         # Extract all the contents of the zip file to the specified dir
         zip_ref.extractall(extract_dir)
