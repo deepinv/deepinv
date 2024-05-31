45493fbe6429c5cafb4d83505088f85354fcfe9a
Ubuntu <ubuntu@ip-172-31-44-17.eu-central-1.compute.internal>
2024-05-31 20:39:03 +0000

    feat: add notebooks and data

---
+++ experimental/test.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+import numpy as np
+import torch
+
+from datetime import datetime
+
+# datetime object containing current date and time
+now = datetime.now()
+ 
+print("now =", now)
+
+# dd/mm/YY H:M:S
+dt_string = now.strftime("%Y%m%d-%H%M%S")
+print("date and time =", dt_string)
+
+a = np.array([1, 2, 3])
+b = torch.tensor(a)
+
+BASE_DIR = Path(".")
+DATA_DIR = BASE_DIR / "data"
+SAVE_DIR = DATA_DIR / dt_string
+Path(SAVE_DIR).mkdir(parents=True, exist_ok=True)
+
+torch.save(b, SAVE_DIR / "b.pt")
