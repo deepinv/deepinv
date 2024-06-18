d5c3a32f677522016f0ddd1b4830c3b2bd78f239
MaximeSongIdris <maxime.song@idris.fr>
2024-06-18 17:33:48 +0200

    add set14 dataset class

--- deepinv/tests/test_datasets.py
+++ deepinv/tests/test_datasets.py
@@ -3,7 +3,7 @@ import shutil
 import PIL
 import pytest
 
-from deepinv.datasets import DIV2K, Urban100HR
+from deepinv.datasets import DIV2K, Urban100HR, Set14HR
 
 
 @pytest.fixture
@@ -22,7 +22,7 @@ def download_div2k():
 
 
 def test_load_div2k_dataset(download_div2k):
-    """Check that DIV2K/DIV2K_train_HR contains 800 items."""
+    """Check that DIV2K/DIV2K_train_HR contains 800 PIL images."""
     val_dataset = DIV2K(download_div2k, mode="val", download=False)
     assert (
         len(val_dataset) == 100
@@ -48,10 +48,36 @@ def download_Urban100():
 
 
 def test_load_urban100_dataset(download_Urban100):
-    """Check that dataset contains 100 items."""
+    """Check that dataset contains 100 PIL images."""
     dataset = Urban100HR(download_Urban100, download=False)
     assert (
         len(dataset) == 100
+    ), f"Dataset should have been of len 100, instead got {len(dataset)}."
+    assert (
+        type(dataset[0]) == PIL.PngImagePlugin.PngImageFile
+    ), "Dataset image should have been a PIL image."
+
+
+@pytest.fixture
+def download_Set14():
+    """Downloads dataset for tests and removes it after test executions."""
+    tmp_data_dir = "Set14"
+
+    # Download div2K dataset
+    Set14HR(tmp_data_dir, download=True)
+
+    # This will return control to the test function
+    yield tmp_data_dir
+
+    # After the test function complete, any code after the yield statement will run
+    shutil.rmtree(tmp_data_dir)
+
+
+def test_load_set14_dataset(download_Urban100):
+    """Check that dataset contains 14 PIL images."""
+    dataset = Urban100HR(download_Urban100, download=False)
+    assert (
+        len(dataset) == 14
     ), f"Dataset should have been of len 100, instead got {len(dataset)}."
     assert (
         type(dataset[0]) == PIL.PngImagePlugin.PngImageFile
