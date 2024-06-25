37807cfc67d2e6ea3b4c3b9651ed9e8817ef8e4d
maxime.song <maxime.song@idris.fr>
2024-06-25 17:46:39 +0200

    add contrib in changelog and edit docstring

--- deepinv/datasets/fastmri.py
+++ deepinv/datasets/fastmri.py
@@ -31,38 +31,38 @@ class SliceSampleFileIdentifier(NamedTuple):
 class FastMRISliceDataset(torch.utils.data.Dataset):
     """Dataset for `fastMRI <https://fastmri.med.nyu.edu/>`_ that provides access to MR image slices.
 
-    1) The fastMRI dataset includes two types of MRI scans: knee MRIs and 
-    the brain (neuro) MRIs, and containing training, validation, and masked test sets.
-    2) MRIs are volumes (3D) made of slices (2D).
-    3) This class in particular considers one data sample as one slice of a MRI scan,
-    thus slices of the same MRI scan are considered independently in the dataset.
+    | 1) The fastMRI dataset includes two types of MRI scans: knee MRIs and
+    | the brain (neuro) MRIs, and containing training, validation, and masked test sets.
+    | 2) MRIs are volumes (3D) made of slices (2D).
+    | 3) This class in particular considers one data sample as one slice of a MRI scan,
+    | thus slices of the same MRI scan are considered independently in the dataset.
 
 
     **Raw data file structure:** ::
-      
+
         self.root --- file1000005.h5
                    |
                    -- xxxxxxxxxxx.h5
 
-    0) To download raw data, please go to the bottom of the page `https://fastmri.med.nyu.edu/`
-    1) Each MRI scan is stored in a HDF5 file and can be read with the h5py package. 
-    Each file contains the k-space data, ground truth and some meta data related to the scan.
-    2) MRI scans can either be single-coil or multi-coil with each coil in
-    a multi-coil MRI scan focusses on a different region of the image.
-    3) In multi-coil MRIs, k-space has the following shape:
-    (number of slices, number of coils, height, width)
-    4) For single-coil MRIs, k-space has the following shape:
-    (number of slices, height, width)
+    | 0) To download raw data, please go to the bottom of the page `https://fastmri.med.nyu.edu/`
+    | 1) Each MRI scan is stored in a HDF5 file and can be read with the h5py package.
+    | Each file contains the k-space data, ground truth and some meta data related to the scan.
+    | 2) MRI scans can either be single-coil or multi-coil with each coil in
+    | a multi-coil MRI scan focusses on a different region of the image.
+    | 3) In multi-coil MRIs, k-space has the following shape:
+    | (number of slices, number of coils, height, width)
+    | 4) For single-coil MRIs, k-space has the following shape:
+    | (number of slices, height, width)
 
     :param Union[str, Path] root: Path to the dataset.
-    :param bool test: Whether the split is the "test" set. 
+    :param bool test: Whether the split is the "test" set.
     :param str challenge: "singlecoil" or "multicoil" depending on the type of mri scan.
     :param bool load_metadata_from_cache: Whether to load dataset metadata from cache.
     :param bool save_metadata_to_cache: Whether to cache dataset metadata.
     :param Union[str, Path] metadata_cache_file: A file used to cache dataset
         information for faster load times.
-    :param callable, optional sample_filter: A callable object that takes a 
-        SliceSampleFileIdentifier as input and returns a boolean indicating 
+    :param callable, optional sample_filter: A callable object that takes a
+        SliceSampleFileIdentifier as input and returns a boolean indicating
         whether the sample should be included in the dataset.
     :param float, optional sample_rate: A float between 0 and 1. This controls what
         fraction of all slices should be loaded. Defaults to 1.
@@ -104,7 +104,7 @@ class FastMRISliceDataset(torch.utils.data.Dataset):
             target, kspace = dataset[0]
             print(target.shape)
             print(kspace.shape)
-        
+
     """
 
     def __init__(
@@ -123,16 +123,22 @@ class FastMRISliceDataset(torch.utils.data.Dataset):
     ) -> None:
         # check that root is a folder
         if not os.path.isdir(root):
-            raise ValueError(f"The `root` folder doesn't exist. Please set `root` properly. Current value `{root}`.")
+            raise ValueError(
+                f"The `root` folder doesn't exist. Please set `root` properly. Current value `{root}`."
+            )
         # check that root folder contains only hdf5 files
         if not all([file.endswith(".h5") for file in os.listdir(root)]):
-            raise ValueError(f"The `root` folder doesn't contain only hdf5 files. Please set `root` properly. Current value `{root}`.")
+            raise ValueError(
+                f"The `root` folder doesn't contain only hdf5 files. Please set `root` properly. Current value `{root}`."
+            )
         # ensure that challenge is either singlecoil or multicoil
         if challenge not in ("singlecoil", "multicoil"):
             raise ValueError('`challenge` should be either "singlecoil" or "multicoil"')
         # ensure that sample_rate and volume_sample_rate are not used simultaneously
         if sample_rate is not None and volume_sample_rate is not None:
-            raise ValueError("Either set `sample_rate` (sample by slices) or `volume_sample_rate` (sample by volumes) but not both")
+            raise ValueError(
+                "Either set `sample_rate` (sample by slices) or `volume_sample_rate` (sample by volumes) but not both"
+            )
 
         self.transforms = {
             "transform_kspace": transform_kspace,
@@ -144,7 +150,6 @@ class FastMRISliceDataset(torch.utils.data.Dataset):
         self.challenge = challenge
         self.test = test
 
-
         ### LOAD DATA SAMPLE IDENTIFIERS -----------------------------------------------
 
         # should contain all the information to load a slice from the storage
@@ -153,15 +158,15 @@ class FastMRISliceDataset(torch.utils.data.Dataset):
             metadata_cache_file = Path(metadata_cache_file)
             if not metadata_cache_file.exists():
                 raise ValueError(
-                    "`metadata_cache_file` doesn't exist. Please either deactivate" +\
-                    "`load_dataset_from_cache` OR set `metadata_cache_file` properly."
+                    "`metadata_cache_file` doesn't exist. Please either deactivate"
+                    + "`load_dataset_from_cache` OR set `metadata_cache_file` properly."
                 )
             with open(metadata_cache_file, "rb") as f:
                 dataset_cache = pickle.load(f)
                 if dataset_cache.get(root) is None:
                     raise ValueError(
-                        "`metadata_cache_file` doesn't contain the metadata. Please" +\
-                        "either deactivate `load_dataset_from_cache` OR set `metadata_cache_file` properly."
+                        "`metadata_cache_file` doesn't contain the metadata. Please"
+                        + "either deactivate `load_dataset_from_cache` OR set `metadata_cache_file` properly."
                     )
                 print(f"Using dataset cache from {metadata_cache_file}.")
                 self.sample_identifiers = dataset_cache[root]
@@ -185,7 +190,6 @@ class FastMRISliceDataset(torch.utils.data.Dataset):
                 with open(metadata_cache_file, "wb") as cache_f:
                     pickle.dump(dataset_cache, cache_f)
 
-
         ### RANDOM SUBSAMPLING (1 sample = 1 slice from a MRI scan) --------------------
 
         # set default sampling mode to get the full dataset
@@ -198,7 +202,9 @@ class FastMRISliceDataset(torch.utils.data.Dataset):
             random.shuffle(self.sample_identifiers)
             num_samples = round(len(self.sample_identifiers) * sample_rate)
             self.sample_identifiers = self.sample_identifiers[:num_samples]
-        elif volume_sample_rate < 1.0:  # sample by volume / randomly keep a portion of mri scans
+        elif (
+            volume_sample_rate < 1.0
+        ):  # sample by volume / randomly keep a portion of mri scans
             vol_names = list(set([f[0].stem for f in self.sample_identifiers]))
             random.shuffle(vol_names)
             num_volumes = round(len(vol_names) * volume_sample_rate)
@@ -226,7 +232,7 @@ class FastMRISliceDataset(torch.utils.data.Dataset):
 
         if not self.test and self.transforms["transform_target"] is not None:
             # by default, shape is (1, H, W), we want to get rid of the first dimension when moving to complex type
-            target = self.transforms["transform_target"](target)[0] 
+            target = self.transforms["transform_target"](target)[0]
             target = target + 0 * 1j
             target = torch.view_as_real(target)
             target = torch.moveaxis(target, -1, 0)  # shape (2, H, W)
