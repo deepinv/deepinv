0e6f7297fdecb082c7c6a16f8fb7af05de1e6cd6
MaximeSongIdris <102222793+MaximeSongIdris@users.noreply.github.com>
2024-06-25 16:00:48 +0200

    add cbsd68 dataset wrapper (#249)
    * add cbsd68 dataset wrapper
    
    * black magic
    
    * add the library datasets as a dependency to the project
    
    * remove unnecessary var definition in cbsd68
    
    * issue with CBSD68 not found after download -> debugging
    
    * [Fix] Remove __url__ Key from Hugging Face Dataset to Ensure Data Integrity
    
    When downloading a dataset from Hugging Face, the raw data is cached locally.
    The resulting dataset object in Python includes a __url__ key, which contains the absolute paths in the user's filesystem.
    This URL is utilized to load the data samples as needed.
    
    However, since we save the entire dataset into a folder and all data samples are consolidated in an Arrow file, the __url__ key becomes redundant.
    Additionally, retaining these URLs poses a problem: when different users save the dataset on their respective machines, the saved dataset will include different absolute paths.
    This discrepancy breaks the data integrity verification mechanism we have in place.
    
    To address this issue, we have removed the __url__ key from the dataset object before saving it.
    This ensures that the dataset remains consistent across different environments, maintaining data integrity.
    
    * [Fix] Change data integrity verification to only verify the raw data file instead of the whole saved folder.
    
    When saving the dataset, 3 files are generated and I have seen that "dataset_info.json" differ slightly on 2 differents machines.
    And since the difference in file doesn't impact its usage. We removed the file verification to make data integrity verification simpler.
    
    * Remove code used for debugging
    
    * add doctest for every dataset class
    
    * raise issue when data has been incorrectly downloaded
    
    * pr doc correction

--- deepinv/datasets/__init__.py
+++ deepinv/datasets/__init__.py
@@ -3,3 +3,4 @@ from .patch_dataset import PatchDataset
 from .div2k import DIV2K
 from .urban100 import Urban100HR
 from .set14 import Set14HR
+from .cbsd68 import CBSD68
--- deepinv/datasets/set14.py
+++ deepinv/datasets/set14.py
@@ -41,12 +41,17 @@ class Set14HR(torch.utils.data.Dataset):
 
     :Examples:
 
-        Instanciate dataset and download raw data from the Internet: ::
+        Instanciate dataset and download raw data from the Internet:
 
-            root = "/path/to/dataset/Set14"
-            dataset = Set14(root=root, download=True)  # will download dataset at root
-            dataset.check_dataset_exists()             # check that raw data has been downloaded correctly
-            assert len(dataset) == 14                  # check that we have 14 images
+        >>> import shutil
+        >>> from deepinv.datasets import Set14HR
+        >>> dataset = Set14HR(root="Set14_DATA", download=True)  # download raw data at root and load dataset
+        Dataset has been successfully downloaded.
+        >>> dataset.check_dataset_exists()                       # check that raw data has been downloaded correctly
+        True
+        >>> len(dataset)                                         # check that we have 14 images
+        14
+        >>> shutil.rmtree("Set14_DATA")                          # remove raw data from disk
 
     """
 
@@ -92,6 +97,8 @@ class Set14HR(torch.utils.data.Dataset):
 
                     if self.check_dataset_exists():
                         print("Dataset has been successfully downloaded.")
+                    else:
+                        raise ValueError("There is an issue with the data downloaded.")
             # stop the execution since the dataset is not available and we didn't download it
             else:
                 raise RuntimeError(
