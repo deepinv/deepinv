09809a81c19e63a03972cc09f9af6292db1a1838
Andrewwango <andrew.wang27@gmail.com>
2024-06-28 13:19:57 +0100

    black

--- examples/self-supervised-learning/demo_splitting_loss.py
+++ examples/self-supervised-learning/demo_splitting_loss.py
@@ -18,7 +18,7 @@ device = dinv.utils.get_freer_gpu() if torch.cuda.is_available() else "cpu"
 # %%
 # Define loss
 # ~~~~~~~~~~~
-# 
+#
 
 loss = dinv.loss.SplittingLoss(split_ratio=0.6, eval_split_output=True)
 
@@ -26,16 +26,12 @@ loss = dinv.loss.SplittingLoss(split_ratio=0.6, eval_split_output=True)
 # %%
 # Prepare data
 # ~~~~~~~~~~~~
-# 
+#
 
 transform = transforms.Compose([transforms.ToTensor()])
 
-train_dataset = datasets.MNIST(
-    root=".", train=True, transform=transform, download=True
-)
-test_dataset = datasets.MNIST(
-    root=".", train=False, transform=transform, download=True
-)
+train_dataset = datasets.MNIST(root=".", train=True, transform=transform, download=True)
+test_dataset = datasets.MNIST(root=".", train=False, transform=transform, download=True)
 
 physics = dinv.physics.Denoising(dinv.physics.PoissonNoise(0.1))
 
@@ -52,22 +48,17 @@ deepinv_datasets_path = dinv.datasets.generate_dataset(
 train_dataset = dinv.datasets.HDF5Dataset(path=deepinv_datasets_path, train=True)
 test_dataset = dinv.datasets.HDF5Dataset(path=deepinv_datasets_path, train=False)
 
-train_dataloader = DataLoader(
-    train_dataset, shuffle=True
-)
-test_dataloader = DataLoader(
-    test_dataset,  shuffle=False
-)
-
+train_dataloader = DataLoader(train_dataset, shuffle=True)
+test_dataloader = DataLoader(test_dataset, shuffle=False)
 
 
 # %%
 # Define model
 # ~~~~~~~~~~~~
-# 
+#
 # Load pretrained model trained with 1000 images for 100 epochs. We
 # demonstrating training with 100 images for 1 epoch.
-# 
+#
 
 model = dinv.models.ArtifactRemoval(
     dinv.models.UNet(in_channels=1, out_channels=1, scales=2).to(device)
@@ -108,4 +99,3 @@ trainer.test(test_dataloader)
 
 model.eval_split_output = False
 trainer.test(test_dataloader)
-
