7ae0232660efe274e5745d41304ea2ce3dcaac27
maxime.song <maxime.song@idris.fr>
2024-07-25 17:41:23 +0200

    update comments for better readability

--- examples/basics/demo_hf_dataset.py
+++ examples/basics/demo_hf_dataset.py
@@ -2,11 +2,11 @@
 Using huggingface dataset
 ====================================================================================================
 
-This simple example shows how to load and prepare properly a huggingface dataset.
-Context: having a quick access to several datasets available under the huggingface format.
-Available datasets: https://huggingface.co/datasets?search=deepinv
+| This simple example shows how to load and prepare properly a huggingface dataset.
+| Context: having a quick access to several datasets available under the huggingface format.
+| Available datasets: https://huggingface.co/datasets?search=deepinv
 
-Here we use drunet_dataset (https://github.com/samuro95/GSPnP)
+| Here we use `drunet_dataset <https://github.com/samuro95/GSPnP>`_.
 """
 
 # %%
@@ -25,20 +25,25 @@ import deepinv as dinv
 # Stream data from Internet
 # ----------------------------------------------------------------------------------------
 #
+# Stream data from huggingface servers: only a limited number of samples is loaded on memory at all time,
+# which avoid having to save the dataset on disk and avoid overloading the memory capacity.
+#
 
-# stream data from huggingface
-# only a limited number of samples should be in memory and nothing should be saved on disk
-# https://huggingface.co/datasets/deepinv/drunet_dataset
+# source : https://huggingface.co/datasets/deepinv/drunet_dataset
 # type : datasets.iterable_dataset.IterableDataset
 raw_hf_train_dataset = load_dataset(
     "deepinv/drunet_dataset", split="train", streaming=True
 )
 print("Number of data files used to store raw data: ", raw_hf_train_dataset.n_shards)
 
-# in streaming mode, we can only read sequentially the data sample in a certain order
-# thus we are not able to do exact shuffling
-# an alternative way is the buffer shuffling which load a fixed number of samples in memory
-# and let us pick randomly one sample among this fixed number of samples
+# %%
+# Shuffle data with buffer shuffling
+# ----------------------------------------------------------------------------------------
+#
+# | In streaming mode, we can only read sequentially the data sample in a certain order thus we are not able to do exact shuffling.
+# | An alternative way is the buffer shuffling which load a fixed number of samples in memory and let us pick randomly one sample among this fixed number of samples.
+#
+
 # https://huggingface.co/docs/datasets/about_mapstyle_vs_iterable
 raw_hf_train_dataset = raw_hf_train_dataset.shuffle(seed=42, buffer_size=100)
 
@@ -47,8 +52,7 @@ raw_hf_train_dataset = raw_hf_train_dataset.shuffle(seed=42, buffer_size=100)
 # Apply transformation on dataset
 # ----------------------------------------------------------------------------------------
 #
-# We define transformation with `torchvision.transforms` module.
-# You can use any other function.
+# We define transformation with ``torchvision.transforms`` module, but it can be any other function.
 #
 
 # Function that should be applied to a PIL Image
@@ -60,7 +64,7 @@ img_transforms = transforms.Compose(
 )
 
 
-# Class that apply transform on data samples of a datasets.iterable_dataset.IterableDataset
+# Class that apply `transform` on data samples of a datasets.iterable_dataset.IterableDataset
 class HFDataset(IterableDataset):
     r"""
     Creates an iteratble dataset from a Hugging Face dataset to enable streaming.
@@ -87,16 +91,19 @@ hf_train_dataset = HFDataset(raw_hf_train_dataset, transforms=img_transforms)
 # Create a dataloader
 # --------------------------------------------------------------------
 #
-# We iterate over the dataset using a dataloader.
+# | With ``datasets.iterable_dataset.IterableDataset``, data samples are stored in 1 file or in a few files.
+# | In case of few files, we can use ``num_workers`` argument to load data samples in parallel.
 #
 
-# if raw_hf_train_dataset.n_shards > 1,
-# it may be interesting to define argument `num_workers > 1`,
-# to have parallel processing of data samples
-# of course, num_workers <= n_shards (number of data files)
-#            num_workers <= number of available cpu cores
-# DataLoader(hf_train_dataset, batch_size=5, num_workers=?)
-train_dataloader = DataLoader(hf_train_dataset, batch_size=2)
+if raw_hf_train_dataset.n_shards > 1:
+    # num_workers <= raw_hf_train_dataset.n_shards (number of data files)
+    # num_workers <= number of available cpu cores
+    num_workers = ...
+    train_dataloader = DataLoader(
+        hf_train_dataset, batch_size=2, num_workers=num_workers
+    )
+else:
+    train_dataloader = DataLoader(hf_train_dataset, batch_size=2)
 
 # display a batch
 batch = next(iter(train_dataloader))
