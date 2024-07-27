d8a0a8f53a618c43c9cf68906240f9ff7211cc5f
blevac <blevac@utexas.edu>
2024-07-27 10:50:42 -0500

    make necessary fixes to documentation and operator name

--- deepinv/physics/mcmri.py
+++ deepinv/physics/mcmri.py
@@ -7,17 +7,20 @@ from deepinv.physics import adjoint_function
 
 
 
-class mcMRI(LinearPhysics):
+class MultiCoilMRI(LinearPhysics):
     r"""
     Multi-coil MRI operator.
 
-    Here the linear operator is defined as:
+    The linear operator is defined as:
     .. math::
 
-        y = PFSx
+        y_c = \text{diag}(p) F \text{diag}(s_c) x
 
-    where :math:`P` is the subsampling mask, :math:`S` are the coil sensitivity maps, :math:`F` is the Fourier transform.
-
+        for c=1,\dots,C coils, where y_c are the measurements from the cth coil, \text{diag}(p) is the acceleration mask, F is the Fourier transform and \diag(s_c) is the cth coil sensitivity.
+    
+    :param torch.Tensor mask: binary sampling mask which should have shape [B,1,H,W].
+    :param torch.Tensor coil_maps: complex valued coil sensitvity maps which should have shape [B,C,H,W].
+    :param device: specify which device you want to use (i.e, cpu or gpu).
     """
 
     def __init__(
@@ -33,17 +36,29 @@ class mcMRI(LinearPhysics):
         self.coil_maps = coil_maps.to(device)
 
     def A(self, x, **kwargs):
-        x_cplx = torch.view_as_complex(x) # input is real [B,H,W,2]
+        r"""  
+        Applies linear operator.  
+        
+        :param torch.Tensor x: image with shape [B,H,W,2].  
+        :returns: (torch.Tensor) multi-coil kspace measurements with shape [B,C,H,W,2].  
+        """ 
+        x_cplx = torch.view_as_complex(x)
         coil_imgs = self.coil_maps*x_cplx[:,None,...]
         coil_ksp = fft(coil_imgs)
         output = self.mask*coil_ksp
-        return torch.view_as_real(output) # output is real [B,C,H,W,2]
+        return torch.view_as_real(output)
 
     def A_adjoint(self, y, **kwargs):
-        sampled_ksp = self.mask* torch.view_as_complex(y) # assumes y is real [B,C,H,W,2]
+        r"""  
+        Applies adjoint linear operator.  
+        
+        :param torch.Tensor y: multi-coil kspace measurements with shape [B,C,H,W,2].  
+        :returns: (torch.Tensor) image with shape [B,H,W,2]  
+        """ 
+        sampled_ksp = self.mask* torch.view_as_complex(y)
         coil_imgs = ifft(sampled_ksp)
         img_out = torch.sum(torch.conj(self.coil_maps)*coil_imgs,dim=1) 
-        img_out_2ch = torch.view_as_real(img_out) # output is real [B,H,W,2]
+        img_out_2ch = torch.view_as_real(img_out)
         return img_out_2ch 
     
 
