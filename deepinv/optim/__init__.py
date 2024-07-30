3c60b3e6e7273a1e967d125949fcbfea1124a426
samuro95 <huraultsamuel@gmail.com>
2024-07-30 17:10:42 +0200

    import init

--- deepinv/optim/__init__.py
+++ deepinv/optim/__init__.py
@@ -1,4 +1,4 @@
-from .data_fidelity import DataFidelity
+from .data_fidelity import DataFidelity, L2, L1, IndicatorL2, PoissonLikelihood, AmplitudeLoss, LogPoissonLikelihood
 from .optimizers import BaseOptim, optim_builder
 from .fixed_point import FixedPoint
 from .prior import (
