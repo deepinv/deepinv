b45bb511296dbd65f5ca877d2c616c370c33cffc
Julián Tachella <38355592+tachella@users.noreply.github.com>
2024-09-12 10:45:06 +0200

    fix #303 (#304)
    * fix no learning trainer + tests
    
    * fix no learning trainer + tests
    
    * fix no learning trainer + tests
    
    * fix no learning trainer + tests

--- deepinv/training/trainer.py
+++ deepinv/training/trainer.py
@@ -579,10 +579,12 @@ class Trainer:
             else:
                 x_nl = physics.A_dagger(y)
         elif self.no_learning_method == "prox_l2" and hasattr(physics, "prox_l2"):
+            # this is a regularized version of the pseudo-inverse, with an l2 regularization
+            # with parameter set to 5.0 for a mild regularization
             if isinstance(physics, torch.nn.DataParallel):
-                x_nl = physics.module.prox_l2(y)
+                x_nl = physics.module.prox_l2(0.0, y, 5.0)
             else:
-                x_nl = physics.prox_l2(y)
+                x_nl = physics.prox_l2(0.0, y, 5.0)
         elif self.no_learning_method == "y":
             x_nl = y
         else:
