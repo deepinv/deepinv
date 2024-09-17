82fd54fcb6e9e3ad793bdd0127620ef4c13d694c
apolychronou <athpolychronou@gmail.com>
2024-09-17 12:26:04 +0100

    Refactor PatchDataset class to accept stride for the window size and shape for the shape of the output tensor. Also, enable transforms

--- deepinv/datasets/patch_dataset.py
+++ deepinv/datasets/patch_dataset.py
@@ -7,16 +7,19 @@ class PatchDataset(data.Dataset):
 
     :param torch.Tensor imgs: Tensor of images, size: batch size x channels x height x width
     :param int patch_size: size of patches
-    :param callable: data augmentation. callable object, None for no augmentation.
+    :param callable transforms: data augmentation. callable object, None for no augmentation.
+    :param tuple shape: shape of the returned tensor. None returns original C x H x W
     """
 
-    def __init__(self, imgs, patch_size=6, transforms=None):
+    def __init__(self, imgs, patch_size=6, stride=1, transforms=None, shapes=(-1,)):
         self.imgs = imgs
         self.patch_size = patch_size
-        self.patches_per_image = (self.imgs.shape[2] - patch_size + 1) * (
-            self.imgs.shape[3] - patch_size + 1
-        )
+        self.stride=stride
+        self.patches_per_image_x = (self.imgs.shape[2] - patch_size) // stride + 1
+        self.patches_per_image_y = (self.imgs.shape[3] - patch_size) // stride + 1
+        self.patches_per_image = self.patches_per_image_x*self.patches_per_image_y
         self.transforms = transforms
+        self.shapes=shapes
 
     def __len__(self):
         return self.imgs.shape[0] * self.patches_per_image
@@ -24,11 +27,11 @@ class PatchDataset(data.Dataset):
     def __getitem__(self, idx):
         idx_img = idx // self.patches_per_image
         idx_in_img = idx % self.patches_per_image
-        idx_x = idx_in_img // (self.imgs.shape[3] - self.patch_size + 1)
-        idx_y = idx_in_img % (self.imgs.shape[3] - self.patch_size + 1)
+        idx_x = (idx_in_img // self.patches_per_image_y) * self.stride
+        idx_y = (idx_in_img % self.patches_per_image_y) * self.stride
         patch = self.imgs[
             idx_img, :, idx_x : idx_x + self.patch_size, idx_y : idx_y + self.patch_size
         ]
-        if self.transforms and False:
+        if self.transforms:
             patch = self.transforms(patch)
-        return patch.reshape(-1), idx
+        return patch.reshape(self.shapes) if self.shapes else patch, idx
