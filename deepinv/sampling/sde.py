52437f3231ee93e105301c4d3dddf8687fc8acfa
Samuel Hurault <samuel.hurault@u-bordeaux.fr>
2024-10-28 12:00:00 +0100

    merge fix

--- deepinv/sampling/sde.py
+++ deepinv/sampling/sde.py
@@ -1,8 +1,10 @@
+
 import torch
 import math
 from torch import Tensor
 import torch.nn as nn
 from typing import Callable
+
 
 class DiffusionSDE(nn.Module):
     def __init__(
@@ -22,45 +24,37 @@ class DiffusionSDE(nn.Module):
         x_new = x
         stepsize = self.T / num_steps
         for k in range(num_steps):
-            t = stepsize * k
-            dw      = torch.randn_like(x_new)
-            f_dt    = self.f(x_new, t)
-            g_dw    = self.g(t) * dw
-            x_new   = x_new + stepsize*(f_dt + g_dw)
+            t = k * stepsize
+            dw = torch.randn_like(x_new)
+            f_dt = self.f(x_new, t)
+            g_dw = self.g(t) * dw
+            x_new = x_new + stepsize * (f_dt + g_dw)
         return x_new
 
-    def prior_sampling(self, batch_size: int = 1) -> Tensor:
-        # return torch.randn((batch_size, ))
-        pass
-  
     def backward_sde(
         self, x: Tensor, num_steps: int = 1000, alpha: float = 1.0
     ) -> Tensor:
         dt = self.T / num_steps
         t = 0
-        for n in range(num_steps):
-            rt = self.T - t * n
+        for k in range(num_steps):
+            rt = self.T - t * k
             g = self.g(rt)
             drift = self.f(x, rt) - (1 + alpha**2) * g**2 * self.score(x, rt)
             diffusion = alpha * g
+            dw = torch.randn_like(x) * dt
+            x = x + drift * dt + diffusion * dw
+        return x
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     import deepinv as dinv
-    
-    from deepinv.utils.plotting import plot
-    from deepinv.utils.demo import load_url_image
-    device = dinv.utils.get_freer_gpu() if torch.cuda.is_available() else "cpu"
 
-    url = (
-        "https://upload.wikimedia.org/wikipedia/commons/b/b4/"
-        "Lionel-Messi-Argentina-2022-FIFA-World-Cup_%28cropped%29.jpg"
-    )
-    x = load_url_image(url=url, img_size=32).to(device)
+    device = torch.device("cuda")
 
-    DiffSDE = DiffusionSDE()
+    score = dinv.models.WaveletDenoiser(wv="db8", level=4, device=device)
+    OUSDE = DiffusionSDE(score=score, T=1.0)
 
-    x_for = DiffSDE.forward_sde(x, 10)
+    x = torch.randn((2, 1, 28, 28), device=device)
+    sample = OUSDE.backward_sde(x)
 
-
-
-  
+    dinv.utils.plot([x, sample])
