6658fd8d9f0820538e8bf2e5c48465122d15cf57
mhnguyen <nguyenhai7120qh@gmail.com>
2024-10-28 12:11:49 +0100

    add rng

--- deepinv/sampling/sde.py
+++ deepinv/sampling/sde.py
@@ -3,6 +3,7 @@ import math
 from torch import Tensor
 import torch.nn as nn
 from typing import Callable
+import warnings
 
 
 class DiffusionSDE(nn.Module):
@@ -12,6 +13,7 @@ class DiffusionSDE(nn.Module):
         g: Callable = lambda t: math.sqrt(2.0),
         score: Callable = None,
         T: float = 1.0,
+        rng: torch.Generator = None,
     ):
         super().__init__()
         self.f = f
@@ -19,11 +21,15 @@ class DiffusionSDE(nn.Module):
         self.score = score
         self.T = T
 
+        self.rng = rng
+        if rng is not None:
+            self.initial_random_state = rng.get_state()
+
     def forward_sde(self, x: Tensor, num_steps: int) -> Tensor:
         x_new = x
         stepsize = 1.0 / num_steps
         for t in range(num_steps):
-            dw = torch.randn_like(x_new)
+            dw = self.randn_like(x_new)
             f_dt = self.f(x_new, t)
             g_dw = self.g(t) * dw
             x_new = x_new + stepsize * (f_dt + g_dw)
@@ -39,10 +45,47 @@ class DiffusionSDE(nn.Module):
             g = self.g(rt)
             drift = self.f(x, rt) - (1 + alpha**2) * g**2 * self.score(x, rt)
             diffusion = alpha * g
-            dw = torch.randn_like(x) * dt
+            dw = self.randn_like(x) * dt
             x = x + drift * dt + diffusion * dw
 
         return x
+
+    def rng_manual_seed(self, seed: int = None):
+        r"""
+        Sets the seed for the random number generator.
+
+        :param int seed: the seed to set for the random number generator. If not provided, the current state of the random number generator is used.
+            Note: it will be ignored if the random number generator is not initialized.
+        """
+        if seed is not None:
+            if self.rng is not None:
+                self.rng = self.rng.manual_seed(seed)
+            else:
+                warnings.warn(
+                    "Cannot set seed for random number generator because it is not initialized. The `seed` parameter is ignored."
+                )
+
+    def reset_rng(self):
+        r"""
+        Reset the random number generator to its initial state.
+        """
+        self.rng.set_state(self.initial_random_state)
+
+    def randn_like(self, input: torch.Tensor, seed: int = None):
+        r"""
+        Equivalent to `torch.randn_like` but supports a pseudorandom number generator argument.
+        :param int seed: the seed for the random number generator, if `rng` is provided.
+
+        """
+        self.rng_manual_seed(seed)
+        return torch.empty_like(input).normal_(generator=self.rng)
+
+
+class EDMSDE(DiffusionSDE):
+    def __init__(
+        self, score: Callable, T: float, sigma_min: float = 0.002, sigma_max: float = 80
+    ):
+        super().__init__(score=score, T=T)
 
 
 if __name__ == "__main__":
@@ -51,7 +94,8 @@ if __name__ == "__main__":
     device = torch.device("cuda")
 
     score = dinv.models.WaveletDenoiser(wv="db8", level=4, device=device)
-    OUSDE = DiffusionSDE(score=score, T=1.0)
+    rng = torch.Generator(device=device).manual_seed(42)
+    OUSDE = DiffusionSDE(score=score, T=1.0, rng=rng)
 
     x = torch.randn((2, 1, 28, 28), device=device)
     sample = OUSDE.backward_sde(x)
