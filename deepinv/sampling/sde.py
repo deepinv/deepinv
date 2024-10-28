4754a1c194f312317d24fbdf065011fd9a8f1849
mhnguyen <nguyenhai7120qh@gmail.com>
2024-10-28 11:46:04 +0100

    backward_sde

--- deepinv/sampling/sde.py
+++ deepinv/sampling/sde.py
@@ -23,23 +23,23 @@ class DiffusionSDE(nn.Module):
         x_new = x
         stepsize = 1.0 / num_steps
         for t in range(num_steps):
-            dw      = torch.randn_like(x_new)
-            f_dt    = self.f(x_new, t)
-            g_dw    = self.g(t) * dw
-            x_new   = x_new + stepsize*(f_dt + g_dw)
+            dw = torch.randn_like(x_new)
+            f_dt = self.f(x_new, t)
+            g_dw = self.g(t) * dw
+            x_new = x_new + stepsize * (f_dt + g_dw)
         return x_new
-
-    def prior_sampling(self, batch_size: int = 1) -> Tensor:
-        # return torch.randn((batch_size, ))
-        pass
 
     def backward_sde(
         self, x: Tensor, num_steps: int = 1000, alpha: float = 1.0
     ) -> Tensor:
         dt = self.T / num_steps
         t = 0
-        for i in range(num_steps):
-            rt = self.T - t
+        for n in range(num_steps):
+            rt = self.T - t * n
             g = self.g(x, rt)
             drift = self.f(x, rt) - (1 + alpha**2) * g**2 * self.score(x, rt)
             diffusion = alpha * g
+            dw = torch.randn_like(x) * dt
+            x = x + drift * dt + diffusion * dw
+
+        return x
