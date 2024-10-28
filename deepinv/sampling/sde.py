d46bdf6ce8c628637750e8b9725b324d7d120494
Davy Leo <ldavy01@pxe.cbp.ens-lyon.fr>
2024-10-28 11:40:41 +0100

    started to write a test function

--- deepinv/sampling/sde.py
+++ deepinv/sampling/sde.py
@@ -2,7 +2,8 @@ import torch
 from torch import Tensor
 import torch.nn as nn
 from typing import Callable
-
+#from deepinv.utils.plotting import plot
+from deepinv.utils.demo import load_url_image
 
 class DiffusionSDE(nn.Module):
     def __init__(self, f: Callable, g: Callable, score: Callable, T: float = 1.0):
@@ -24,3 +25,16 @@ class DiffusionSDE(nn.Module):
 
     def backward_sde(self, x: Tensor, t: Tensor) -> Tensor:
         pass
+
+if __name__ == '__main__':
+    device = dinv.utils.get_freer_gpu() if torch.cuda.is_available() else "cpu"
+
+    url = (
+        "https://upload.wikimedia.org/wikipedia/commons/b/b4/"
+        "Lionel-Messi-Argentina-2022-FIFA-World-Cup_%28cropped%29.jpg"
+    )
+    x = load_url_image(url=url, img_size=32).to(device)
+
+    DiffSDE = DiffusionSDE()
+
+    
