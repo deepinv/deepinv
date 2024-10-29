81e009566c9173ceabc093a383ca1a0a4d5cf072
mhnguyen <nguyenhai7120qh@gmail.com>
2024-10-29 15:54:47 +0100

    utility function to get sampling parameters of ve, vp, edm in the edm framework

--- deepinv/sampling/sde.py
+++ deepinv/sampling/sde.py
@@ -70,23 +70,24 @@ class Euler_solver(SDE_solver):
     def step(self, t0, t1, x0):
         dt = abs(t1 - t0)
         dW = self.randn_like(x0) * dt**0.5
-        return x0 + self.drift(x0,t0) * dt + self.diffusion(t0) * dW
+        return x0 + self.drift(x0, t0) * dt + self.diffusion(t0) * dW
+
 
 class Heun_solver(SDE_solver):
     def __init__(
         self, drift: Callable, diffusion: Callable, rng: torch.Generator = None
     ):
-        super().__init__(drift, diffusion, rng = rng)
+        super().__init__(drift, diffusion, rng=rng)
 
     def step(self, t0, t1, x0):
         dt = abs(t1 - t0)
         dW = self.randn_like(x0) * dt**0.5
         diff_x0 = self.diffusion(t0)
-        drift_x0 = self.drift(x0,t0)
+        drift_x0 = self.drift(x0, t0)
         x_euler = x0 + drift_x0 * dt + diff_x0 * dW
         diff_x1 = self.diffusion(t1)
-        drift_x1 = self.drift(x_euler,t1)
-        return x0 + 0.5*(drift_x0 + drift_x1) * dt + 0.5*(diff_x0 + diff_x1) * dW
+        drift_x1 = self.drift(x_euler, t1)
+        return x0 + 0.5 * (drift_x0 + drift_x1) * dt + 0.5 * (diff_x0 + diff_x1) * dW
 
 
 class DiffusionSDE(nn.Module):
@@ -96,16 +97,20 @@ class DiffusionSDE(nn.Module):
         g: Callable = lambda t: math.sqrt(2.0),
         prior: Callable = None,
         rng: torch.Generator = None,
-        use_backward_ode = False
+        use_backward_ode=False,
     ):
         super().__init__()
         self.use_backward_ode = use_backward_ode
         self.drift_forw = lambda x, t: f(x, t)
         self.diff_forw = lambda t: g(t)
-        self.forward_sde = Euler_solver(drift=self.drift_forw, diffusion=self.diff_forw, rng=rng)
-        if self.use_backward_ode : 
-            self.drift_back = lambda x, t: f(x, t) - 0.5 * (g(t) ** 2) * (-prior.grad(x, t))
-        else :
+        self.forward_sde = Euler_solver(
+            drift=self.drift_forw, diffusion=self.diff_forw, rng=rng
+        )
+        if self.use_backward_ode:
+            self.drift_back = lambda x, t: f(x, t) - 0.5 * (g(t) ** 2) * (
+                -prior.grad(x, t)
+            )
+        else:
             self.drift_back = lambda x, t: f(x, t) - (g(t) ** 2) * (-prior.grad(x, t))
         self.diff_back = lambda t: g(t)
         self.backward_sde = Euler_solver(
@@ -122,26 +127,40 @@ class EDMSDE(DiffusionSDE):
         s: Callable = lambda t: 1.0,
         s_prime: Callable = lambda t: 0.0,
         beta: Callable = lambda t: 1.0 / t,
-        rng: torch.Generator = None
-    ): 
+        rng: torch.Generator = None,
+    ):
         super().__init__(prior=prior, rng=rng)
-        self.drift_forw = lambda x, t: (- sigma_prime(t) * sigma(t) + beta(t) * sigma(t) ** 2) * (-prior.grad(x, sigma(t)))
+        self.drift_forw = lambda x, t: (
+            -sigma_prime(t) * sigma(t) + beta(t) * sigma(t) ** 2
+        ) * (-prior.grad(x, sigma(t)))
         self.diff_forw = lambda t: sigma(t) * (2 * beta(t)) ** 0.5
-        if self.use_backward_ode :
-            self.diff_back = lambda t: 0.
-            self.drift_back = lambda x, t:  - ((s_prime(t) / s(t)) * x  - (s(t) ** 2) * sigma_prime(t) * sigma(t) * (-prior.grad(x / s(t), sigma(t))))
-        else : 
-            self.drift_back = lambda x, t: (sigma_prime(t) * sigma(t) + beta(t) * sigma(t) ** 2) * (-prior.grad(x, sigma(t)))
+        if self.use_backward_ode:
+            self.diff_back = lambda t: 0.0
+            self.drift_back = lambda x, t: -(
+                (s_prime(t) / s(t)) * x
+                - (s(t) ** 2)
+                * sigma_prime(t)
+                * sigma(t)
+                * (-prior.grad(x / s(t), sigma(t)))
+            )
+        else:
+            self.drift_back = lambda x, t: (
+                sigma_prime(t) * sigma(t) + beta(t) * sigma(t) ** 2
+            ) * (-prior.grad(x, sigma(t)))
             self.diff_back = self.diff_forw
-        self.forward_sde = Euler_solver(drift=self.drift_forw, diffusion=self.diff_forw, rng=rng)
-        self.backward_sde = Heun_solver(drift=self.drift_back, diffusion=self.diff_back, rng=rng)
+        self.forward_sde = Euler_solver(
+            drift=self.drift_forw, diffusion=self.diff_forw, rng=rng
+        )
+        self.backward_sde = Heun_solver(
+            drift=self.drift_back, diffusion=self.diff_back, rng=rng
+        )
 
 
-if __name__ == '__main__' :
+if __name__ == "__main__":
     from edm import load_model
     import numpy as np
     from deepinv.utils.demo import load_url_image, get_image_url
-    import deepinv as dinv 
+    import deepinv as dinv
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = load_model("edm-ffhq-64x64-uncond-ve.pkl").to(device)
@@ -165,4 +184,3 @@ if __name__ == '__main__' :
         noise = torch.randn(2, 3, 64, 64, device=device) * ve_sigma_max
         samples = sde.backward_sde.sample(noise, timesteps=ve_timesteps)
     dinv.utils.plot(samples)
-
--- deepinv/sampling/test_sam.py
+++ deepinv/sampling/test_sam.py
@@ -7,27 +7,33 @@ from sde import EDMSDE
 device = dinv.utils.get_freer_gpu() if torch.cuda.is_available() else "cpu"
 url = get_image_url("CBSD_0010.png")
 x = load_url_image(url=url, img_size=64, device=device)
-x = x * 2 - 1 
+x = x * 2 - 1
+
+
 class my_drunet(dinv.models.DRUNet):
     def __init__(self):
-        super().__init__(pretrained = 'download', device = device)
-    
+        super().__init__(pretrained="download", device=device)
+
     def forward(self, x, sigma):
         x = (x + 1) * 0.5
         out = super().forward(x, 0.5 * sigma)
         out = out * 2 - 1
         return out
+
+
 denoiser = my_drunet()
 prior = dinv.optim.prior.ScorePrior(denoiser=denoiser)
 
-#VP 
-timesteps = torch.linspace(0.001, 1., 100)
-sigma = lambda t : ((np.exp( 0.5 * 19.9 * (t ** 2) + 0.1 * t - 1)) ** 0.5)
-s = lambda t : 1 / (np.exp( 0.5 * 19.9 * (t ** 2) + 0.1 * t)) ** 0.5
+# VP
+timesteps = torch.linspace(0.001, 1.0, 100)
+sigma = lambda t: ((np.exp(0.5 * 19.9 * (t**2) + 0.1 * t - 1)) ** 0.5)
+s = lambda t: 1 / (np.exp(0.5 * 19.9 * (t**2) + 0.1 * t)) ** 0.5
 
-OUSDE = EDMSDE(prior=prior, sigma = sigma, s = s, backward_ode = True)
+OUSDE = EDMSDE(prior=prior, sigma=sigma, s=s, backward_ode=True)
 with torch.no_grad():
     noise = torch.randn_like(x)
-    sample = OUSDE.backward_sde.sample(noise.clone(), timesteps=timesteps.flip(dims=[0]))
+    sample = OUSDE.backward_sde.sample(
+        noise.clone(), timesteps=timesteps.flip(dims=[0])
+    )
     print(sample.min(), sample.max())
 dinv.utils.plot([x, noise, sample])
