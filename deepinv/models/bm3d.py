3f26016fd6fe3854ba9f271d6edb91935b4e36c9
Matthieu Terris <matthieu.terris@gmail.com>
2024-11-04 23:31:54 +0100

    mini fix

--- deepinv/models/bm3d.py
+++ deepinv/models/bm3d.py
@@ -10,11 +10,11 @@ except:
 
 
 class BM3D(nn.Module):
-    """
+    r"""
     BM3D denoiser.
 
     The BM3D denoiser was introduced in "Image denoising by sparse 3D transform-domain collaborative filtering", by
-    Davob et al., IEEE Transactions on Image Processing (2007).
+    Dabov et al., IEEE Transactions on Image Processing (2007).
 
 
     .. note::
@@ -28,7 +28,6 @@ class BM3D(nn.Module):
         It can be installed with ``pip install bm3d``.
 
     """
-
     def __init__(self):
         super().__init__()
         if isinstance(bm3d, ImportError):
@@ -41,7 +40,7 @@ class BM3D(nn.Module):
         Run the denoiser on image with noise level :math:`\sigma`.
 
         :param torch.Tensor x: noisy image
-        :param float sigma: noise level (not used)
+        :param float sigma: noise level
         """
 
         out = torch.zeros_like(x)
