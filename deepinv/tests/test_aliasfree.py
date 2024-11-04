465a35e86dc9ea6be179ee7c673ca0d69bcd9aa0
Jérémy Scanvic <jscanvicz@gmail.com>
2024-11-04 17:16:24 +0100

    add tests for forward operator equivariance

--- deepinv/tests/test_aliasfree.py
+++ deepinv/tests/test_aliasfree.py
@@ -1,6 +1,9 @@
 from deepinv.models import AliasFreeDenoiser, UNet
-from deepinv.transform import Translate, Shift
+from deepinv.transform import Translate, Shift, Rotate
 from deepinv.tests.dummy_datasets.datasets import DummyCircles
+from deepinv.physics import BlurFFT, Inpainting
+from deepinv.physics.blur import gaussian_blur
+from deepinv.physics.generator import BernoulliSplittingMaskGenerator
 
 import torch
 import torch.nn.functional as F
@@ -35,3 +38,26 @@ def test_translation_equivariant():
 def test_not_translation_equivariant():
     err = Translate().equivariance_test(unet, x, metric=linf_metric)
     assert err >= 1e0
+
+def test_forward_operator_equivariance():
+    physics = BlurFFT(filter=gaussian_blur(sigma=1), img_size=x.shape[-3:])
+
+    err = Shift().equivariance_test(physics, x, metric=linf_metric)
+    assert err < 1e-6
+
+    err = Translate().equivariance_test(physics, x, metric=linf_metric)
+    assert err < 1e-6
+
+    gen = BernoulliSplittingMaskGenerator(x.shape[-3:], split_ratio=0.7)
+    params = gen.step(batch_size=1, seed = 0)
+    physics = Inpainting(tensor_size=x.shape[-3:])
+    physics.update_parameters(**params)
+
+    err = Shift().equivariance_test(physics, x, metric=linf_metric)
+    assert err >= 1e-1
+
+    err = Translate().equivariance_test(physics, x, metric=linf_metric)
+    assert err >= 1e0
+
+    err = Rotate().equivariance_test(physics, x, metric=linf_metric)
+    assert err >= 1e-1
