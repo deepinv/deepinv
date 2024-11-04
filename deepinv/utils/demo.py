f4d4f0db619c2416725a51faea2f87f964d05744
Julián Tachella <38355592+tachella@users.noreply.github.com>
2024-11-04 17:45:47 +0100

    Update deepinv/utils/demo.py
    Co-authored-by: Thomas Moreau <thomas.moreau.2010@gmail.com>

--- deepinv/utils/demo.py
+++ deepinv/utils/demo.py
@@ -82,7 +82,8 @@ def get_image_url(file_name):
 def get_data_home():
     """Return folder to store data in.
 
-    This folder can be set by setting
+    This folder can be set by setting the environment variable``DEEPINV_DATA``,
+    or ``XDG_DATA_HOME``. By default, it will be set in ``./datasets``.
     """
     data_home = os.environ.get("DEEPINV_DATA", None)
     if data_home is not None:
