a36fdc24fd15eb828c76e449d0271a7ebaa2c647
Julián Tachella <tachellajulian@gmail.com>
2024-11-06 09:25:48 +0100

    custom.js

--- docs/source/_static/js/custom.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', (event) => {
-    if (!document.URL.includes("index.html")) {
-        return
-    }
-    var menu = document.querySelector(".wy-menu ul li:first-child")
-    if (!menu.classList.contains("current")) {
-        menu.classList.add("current")
-    }
-});
