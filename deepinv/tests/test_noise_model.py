b951f6822c0aef2992f968d48d51f63d39bea382
maxime.song <maxime.song@idris.fr>
2024-11-08 13:58:29 +0100

    Black magic and remove typo

--- deepinv/tests/test_noise_model.py
+++ deepinv/tests/test_noise_model.py
@@ -1,4 +1,4 @@
-ok pimport pytest
+import pytest
 import torch
 import numpy as np
 from deepinv.physics.forward import adjoint_function
@@ -48,13 +48,16 @@ def choose_noise(noise_type, device):
 @pytest.mark.parametrize("device", DEVICES)
 @pytest.mark.parametrize("dtype", DTYPES)
 def test_scalar_mult_gaussian_noise(device, dtype):
-    imsize = (1, 3, 28, 28)
+    b = 2
+    imsize = (b, 3, 28, 28)
     x = torch.rand(imsize, device=device, dtype=dtype)
 
     t = 0.3
     gaussian_noise_model = choose_noise("Gaussian", device)
     new_noise_model = t * gaussian_noise_model
-    assert isinstance(new_noise_model, dinv.physics.GaussianNoise), f"Expected to have a GaussianNoise, instead got {type(noise_model)}"
+    assert isinstance(
+        new_noise_model, dinv.physics.GaussianNoise
+    ), f"Expected to have a GaussianNoise, instead got {type(noise_model)}"
 
     y = new_noise_model(x)
     assert y.shape == torch.Size(imsize)
