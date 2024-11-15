99e7ea4cc57e56c6ab7fdd43d2c4348c764154f8
Andrewwango <andrew.wang27@gmail.com>
2024-11-15 12:50:57 +0000

    black

--- deepinv/utils/nn.py
+++ deepinv/utils/nn.py
@@ -3,6 +3,7 @@ from warnings import warn
 import numpy as np
 import torch
 
+
 class TensorList:
     r"""
 
@@ -254,8 +255,8 @@ def ones_like(x):
 
 def get_freer_gpu(verbose=True):
     """
-    Returns the GPU device with the most free memory. 
-    
+    Returns the GPU device with the most free memory.
+
     Use in conjunction with ``torch.cuda.is_available()``.
     Attempts to use ``nvidia-smi`` with ``bash``, if these don't exist then uses torch commands to get free memory.
 
@@ -263,11 +264,15 @@ def get_freer_gpu(verbose=True):
     :return torch.device device: selected torch cuda device.
     """
     try:
-        os.system("nvidia-smi -q -d Memory |grep -A5 GPU|grep Free >tmp" if os.name == "posix" else 'bash -c "nvidia-smi -q -d Memory |grep -A5 GPU|grep Free >tmp"')
+        os.system(
+            "nvidia-smi -q -d Memory |grep -A5 GPU|grep Free >tmp"
+            if os.name == "posix"
+            else 'bash -c "nvidia-smi -q -d Memory |grep -A5 GPU|grep Free >tmp"'
+        )
         memory_available = [int(x.split()[2]) for x in open("tmp", "r").readlines()]
         idx, mem = np.argmax(memory_available), np.max(memory_available)
         device = torch.device(f"cuda:{idx}")
-    
+
     except:
         if torch.cuda.device_count() == 0:
             warn("Couldn't find free GPU")
@@ -276,8 +281,11 @@ def get_freer_gpu(verbose=True):
         else:
             # Note this is slower and will return slightly different values to nvidia-smi
             idx, mem = max(
-                ((d, torch.cuda.mem_get_info(d)[0] / 1048576) for d in range(torch.cuda.device_count())),
-                key=lambda x: x[1]
+                (
+                    (d, torch.cuda.mem_get_info(d)[0] / 1048576)
+                    for d in range(torch.cuda.device_count())
+                ),
+                key=lambda x: x[1],
             )
             device = torch.device(f"cuda:{idx}")
 
