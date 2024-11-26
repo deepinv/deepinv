51161575dc6cb557ef9f8438a40ff46b4fbc1aaf
Jérémy Scanvic <jscanvicz@gmail.com>
2024-11-26 22:43:55 +0100

    export more artefacts in the demo

--- examples/aliasfree/demo_alias_free.py
+++ examples/aliasfree/demo_alias_free.py
@@ -7,21 +7,48 @@ from torchvision.datasets import ImageFolder
 from torchvision.transforms import Compose, ToTensor, CenterCrop, Resize
 from torchvision.datasets.utils import download_and_extract_archive
 from torchvision.transforms import InterpolationMode
+from torchvision.utils import save_image
+from tqdm import tqdm
 
 import numpy
 import random
 
-from os.path import exists
+from os.path import exists, dirname
+from os import makedirs
 
 device = "cuda:0"
-root = "Urban100"
-dataset_path = f"{root}/dinv_dataset0.h5"
+dataset_root = "Urban100"
+dataset_path = f"{dataset_root}/dinv_dataset0.h5"
+# model_kind = "AliasFreeUNet"
+model_kind = "UNet"
 rotation_equivariant = False
+# out_dir = "results/Inpainting_AliasFreeUNet"
+out_dir = "results/Inpainting_UNet"
 
 torch.manual_seed(0)
 torch.cuda.manual_seed(0)
 numpy.random.seed(0)
 random.seed(0)
+
+# Step 0. Print the configuration.
+
+print()
+print("Configuration:")
+print(f"  Device: {device}")
+print(f"  Dataset root: {dataset_root}")
+print(f"  Dataset path: {dataset_path}")
+print(f"  Model kind: {model_kind}")
+print(f"  Rotation equivariant: {rotation_equivariant}")
+print(f"  Out dir: {out_dir}")
+print()
+
+# Step 1. Synthesize the dataset.
+
+dataset = dinv.datasets.Urban100HR(
+    root=dataset_root,
+    download=True,
+    transform=Compose([ToTensor(), Resize(256), CenterCrop(128)]),
+)
 
 gen = dinv.physics.generator.BernoulliSplittingMaskGenerator(
     (3, 128, 128), split_ratio=0.7
@@ -30,41 +57,53 @@ params = gen.step(batch_size=1, seed=0)
 physics = dinv.physics.Inpainting(tensor_size=(3, 128, 128))
 physics.update_parameters(**params)
 
-dataset = dinv.datasets.Urban100HR(
-    root=root,
-    download=True,
-    transform=Compose([ToTensor(), Resize(256), CenterCrop(128)]),
-)
-
 train_dataset, test_dataset = random_split(dataset, (0.8, 0.2))
 
-# Generate data pairs x,y offline using a physics generator
 dinv.datasets.generate_dataset(
     train_dataset=train_dataset,
     test_dataset=test_dataset,
     physics=physics,
-    save_dir=root,
+    save_dir=dataset_root,
     batch_size=1,
     device="cpu",
 )
 
 physics.mask.to(device)
 
+# Step 2. Load the dataset.
+
+train_dataset = dinv.datasets.HDF5Dataset(dataset_path, train=True)
+test_dataset = dinv.datasets.HDF5Dataset(dataset_path, train=False)
+
 train_dataloader = DataLoader(
-    dinv.datasets.HDF5Dataset(dataset_path, train=True),
+    train_dataset,
     batch_size=4,
     shuffle=True,
 )
+
 test_dataloader = DataLoader(
-    dinv.datasets.HDF5Dataset(dataset_path, train=False), shuffle=False
+    test_dataset,
 )
 
-model = dinv.models.AliasFreeUNet(
-    in_channels=3,
-    out_channels=3,
-    scales=5,
-    rotation_equivariant=rotation_equivariant,
-).to(device)
+# Step 3. Train the model.
+
+if model_kind == "AliasFreeUNet":
+    model = dinv.models.AliasFreeUNet(
+        in_channels=3,
+        out_channels=3,
+        scales=5,
+        rotation_equivariant=rotation_equivariant,
+    )
+elif model_kind == "UNet":
+    model = dinv.models.UNet(
+        in_channels=3,
+        out_channels=3,
+        scales=5,
+    )
+else:
+    raise ValueError(f"Unknown model kind: {model_kind}")
+
+model.to(device)
 
 optimizer = torch.optim.Adam(model.parameters(), lr=5e-4)
 scheduler = None
@@ -91,49 +130,54 @@ trainer = dinv.Trainer(
 
 model = trainer.train()
 
-trainer.plot_images = True
-trainer.test(test_dataloader)
+# Save the final weights.
+weights = model.state_dict()
+path = f"{out_dir}/weights.pt"
+makedirs(dirname(path), exist_ok=True)
+torch.save(weights, path)
 
-transform = dinv.transform.Shift()
+# Step 4. Evaluate the model.
 
-x, y = next(iter(test_dataloader))
-x = x.to(device)
-y = y.to(device)
-params = {"x_shift": [64], "y_shift": [64]}
-dinv.utils.plot(
-    [
-        x,
-        y,
-        model(y),
-        transform(x, **params),
-        transform(y, **params),
-        transform(model(y), **params),
-    ],
-    ["GT", "Noisy", "Denoised", "Shifted GT", "Shifted Noisy", "Shifted Denoised"],
-)
+model.eval()
 
-equiv_psnr = transform.equivariance_test(model, y, metric=dinv.loss.metric.PSNR())
-print(f"Equivariance test (Shift, PSNR): {equiv_psnr.item():.1f} dB")
+splits_map = {
+    "train": train_dataset,
+    "test": test_dataset,
+}
 
-transform = dinv.transform.Rotate(
-    interpolation_mode=InterpolationMode.BILINEAR, padding="circular"
-)
+transforms_map = {
+    "shifts": dinv.transform.Shift(),
+    "rotations": dinv.transform.Rotate(
+        interpolation_mode=InterpolationMode.BILINEAR,
+        padding="circular"
+    ),
+}
 
-x, y = next(iter(test_dataloader))
-x = x.to(device)
-y = y.to(device)
-params = {"theta": [15]}
-dinv.utils.plot(
-    [
-        x,
-        y,
-        model(y),
-        transform(x, **params),
-        transform(y, **params),
-        transform(model(y), **params),
-    ],
-    ["GT", "Noisy", "Denoised", "Rotated GT", "Rotated Noisy", "Rotated Denoised"],
-)
+for split_name, dataset in splits_map.items():
+    for i, (target, predictor) in tqdm(enumerate(dataset)):
+        target = target.to(device).unsqueeze(0)
+        predictor = predictor.to(device).unsqueeze(0)
 
-equiv_psnr = transform.equivariance_test(model, y, metric=dinv.loss.metric.PSNR())
-print(f"Equivariance test (Rotation, PSNR): {equiv_psnr.item():.1f} dB")
+        im_m = model(predictor)
+        path = f"{out_dir}/{split_name}/m/{i}.png"
+        makedirs(dirname(path), exist_ok=True)
+        save_image(im_m, path)
+
+        for transform_dirname, transform in transforms_map.items():
+            # Sample a random transform.
+            params = transform.get_params(predictor)
+
+            im_t = transform(predictor, **params)
+            path = f"{out_dir}/{split_name}/{transform_dirname}/t/{i}.png"
+            makedirs(dirname(path), exist_ok=True)
+            save_image(im_t, path)
+
+            im_mt = model(im_t)
+            path = f"{out_dir}/{split_name}/{transform_dirname}/mt/{i}.png"
+            makedirs(dirname(path), exist_ok=True)
+            save_image(im_mt, path)
+
+            im_tm = transform(im_m, **params)
+            path = f"{out_dir}/{split_name}/{transform_dirname}/tm/{i}.png"
+            makedirs(dirname(path), exist_ok=True)
+            save_image(im_tm, path)
