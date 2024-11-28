5138590f9f868fa4f2fdf2cec35f2fe483c09688
Andrewwango <andrew.wang27@gmail.com>
2024-11-28 22:06:02 +0000

    fix

--- temp/demo_splitting_mri.py
+++ temp/demo_splitting_mri.py
@@ -18,7 +18,6 @@ DATA_DIR = BASE_DIR / "measurements"
 
 loss = dinv.loss.SplittingLoss(
     split_ratio=0.6, eval_split_input=False,
-    mask_generator=dinv.physics.generator.GaussianSplittingMaskGenerator((2, 128, 128), 0.6, rng=rng, device=device)
 ) # SSDU
 
 img_size = 128
@@ -29,7 +28,7 @@ train_dataset = load_dataset("fastmri_knee_singlecoil", transform, train=True, d
 test_dataset = load_dataset("fastmri_knee_singlecoil", transform, train=False, data_dir=BASE_DIR)
 
 physics = dinv.physics.MRI(img_size=(img_size, img_size), device=device)
-physics_generator = dinv.physics.generator.GaussianMaskGenerator(acceleration=4, img_size=(img_size, img_size), center_fraction=0.1, device=device, rng=rng)
+physics_generator = dinv.physics.generator.GaussianMaskGenerator(acceleration=4, img_size=(img_size, img_size), device=device, rng=rng)
 
 deepinv_datasets_path = dinv.datasets.generate_dataset(
     train_dataset=train_dataset,
@@ -66,7 +65,6 @@ trainer = dinv.Trainer(
     save_path=None,
     verbose=True,
     show_progress_bar=False,
-    no_learning_method="A_dagger",  # use pseudo-inverse as no-learning baseline
 )
 
 model = trainer.train()
@@ -86,7 +84,7 @@ trainer.test(test_dataloader)
 
 
 
-model = demo_mri_model(device=device)
+"""model = demo_mri_model(device=device)
 loss = [dinv.loss.EILoss(transform=dinv.transform.CPABDiffeomorphism(device=device)), dinv.loss.MCLoss()]
 
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-8)
@@ -111,4 +109,4 @@ model = trainer.train()
 torch.save({"state_dict": model.state_dict(), "optimizer": optimizer.state_dict()}, "demo_ei_fastmri.pth")
 
 trainer.plot_images = True
-trainer.test(test_dataloader)
+trainer.test(test_dataloader)"""
