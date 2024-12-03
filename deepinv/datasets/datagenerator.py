a4c162e7b3fb1553ef1bbb365261950dc7bf6a12
Andrewwango <andrew.wang27@gmail.com>
2024-12-03 19:07:22 +0000

    datagenerator allows val dataset and arbitrary split

--- deepinv/datasets/datagenerator.py
+++ deepinv/datasets/datagenerator.py
@@ -30,7 +30,8 @@ class HDF5Dataset(data.Dataset):
         We support all dtypes supported by ``h5py`` including complex numbers, which will be stored as complex dtype.
 
     :param str path: Path to the folder containing the dataset (one or multiple HDF5 files).
-    :param bool train: Set to ``True`` for training and ``False`` for testing.
+    :param bool train: Set to ``True`` for training and ``False`` for testing. If ``split`` argument used, then ``train`` is ignored.
+    :param str split: overrides ``train`` argument if not None. Custom dataset split e.g. "train", "test" or "val", which selects the split name used when generating the dataset.
     :param Transform, Callable transform: A deepinv or torchvision transform to apply to the data.
     :param bool load_physics_generator_params: load physics generator params from dataset if they exist (e.g. if dataset created with :meth:`deepinv.datasets.generate_dataset`)
     :param torch.dtype, str dtype: cast all real-valued data to this dtype.
@@ -41,6 +42,7 @@ class HDF5Dataset(data.Dataset):
         self,
         path: str,
         train: bool = True,
+        split: str = None,
         transform: Union[Transform, Callable] = None,
         load_physics_generator_params: bool = False,
         dtype: torch.dtype = torch.float,
@@ -55,10 +57,11 @@ class HDF5Dataset(data.Dataset):
         self.cast = lambda x: x.type(complex_dtype if x.is_complex() else dtype)
 
         hd5 = h5py.File(path, "r")
-        suffix = "_train" if train else "_test"
+        suffix = ("_train" if train else "_test") if split is None else f"_{split}"
+
         self.y = hd5[f"y{suffix}"]
 
-        if train:
+        if train or split == "train":
             if "x_train" in hd5:
                 self.x = hd5[f"x{suffix}"]
             else:
@@ -104,10 +107,12 @@ def generate_dataset(
     physics: Physics,
     save_dir: str,
     test_dataset: Dataset = None,
+    val_dataset: Dataset = None,
     dataset_filename: str = "dinv_dataset",
     overwrite_existing: bool = True,
     train_datapoints: int = None,
     test_datapoints: int = None,
+    val_datapoints: int = None,
     physics_generator: PhysicsGenerator = None,
     save_physics_generator_params: bool = True,
     batch_size: int = 4,
@@ -116,7 +121,7 @@ def generate_dataset(
     verbose: bool = True,
     show_progress_bar: bool = False,
     device: Union[torch.device, str] = "cpu",
-) -> Union[str, Tuple[str]]:
+):
     r"""
     Generates dataset of signal/measurement pairs from base dataset.
 
@@ -144,8 +149,8 @@ def generate_dataset(
         It can be either a single operator or a list of forward operators. In the latter case, the dataset will be
         assigned evenly across operators.
     :param str save_dir: folder where the dataset and forward operator will be saved.
-    :param torch.data.Dataset test_dataset: if included, the function will also generate measurements associated to the
-        test dataset.
+    :param torch.data.Dataset test_dataset: if included, the function will also generate measurements associated to the test dataset.
+    :param torch.data.Dataset val_dataset: if included, the function will also generate measurements associated to the validation dataset.
     :param str dataset_filename: desired filename of the dataset (without extension).
     :param bool overwrite_existing: if ``True``, create new dataset file, overwriting any existing dataset with the same ``dataset_filename``.
         If ``False`` and dataset file already exists, does not create new dataset.
@@ -154,6 +159,7 @@ def generate_dataset(
         augmentation (which should be chosen in the train_dataset).
     :param int, None test_datapoints: Desired number of datapoints in the test dataset. If set to ``None``, it will use the
         number of datapoints in the base test dataset.
+    :param int, None val_datapoints: Desired number of datapoints in the val dataset.
     :param None, deepinv.physics.generator.PhysicsGenerator physics_generator: Optional physics generator for generating
             the physics operators. If not None, the physics operators are randomly sampled at each iteration using the generator.
     :param bool save_physics_generator_params: save physics generator params too, ignored if ``physics_generator`` not used.
@@ -168,10 +174,9 @@ def generate_dataset(
     :param bool show_progress_bar: Show progress bar during the generation
         of the dataset (if verbose is set to ``True``).
     :param torch.device, str device: device, e.g. cpu or gpu, on which to generate measurements. All data is moved back to cpu before saving.
-    :return: filename(s) of generated dataset or datasets
-    :rtype: str, tuple[str]
+
     """
-    if test_dataset is None and train_dataset is None:
+    if test_dataset is None and train_dataset is None and val_dataset is None:
         raise ValueError("No train or test datasets provided.")
 
     if not os.path.exists(save_dir):
@@ -188,21 +193,17 @@ def generate_dataset(
     )
 
     if train_dataset is not None:
-        if train_datapoints is not None:
-            datapoints = int(train_datapoints)
-        else:
-            datapoints = len(train_dataset)
-
-        n_train = datapoints  # min(len(train_dataset), datapoints)
+        n_train = train_datapoints or len(train_dataset)
         n_train_g = int(n_train / G)
-        n_dataset_g = int(min(len(train_dataset), datapoints) / G)
+        n_dataset_g = int(min(len(train_dataset), n_train) / G)
 
     if test_dataset is not None:
-        test_datapoints = (
-            test_datapoints if test_datapoints is not None else len(test_dataset)
-        )
-        n_test = min(len(test_dataset), test_datapoints)
+        n_test = min(len(test_dataset), test_datapoints or len(test_dataset))
         n_test_g = int(n_test / G)
+
+    if val_dataset is not None:
+        n_val = min(len(val_dataset), val_datapoints or len(val_dataset))
+        n_val_g = int(n_val / G)
 
     hf_paths = []
 
@@ -228,6 +229,8 @@ def generate_dataset(
             x0 = train_dataset[0]
         elif test_dataset is not None:
             x0 = test_dataset[0]
+        elif val_dataset is not None:
+            x0 = val_dataset[0]
 
         x0 = x0[0] if isinstance(x0, (list, tuple)) else x0
         x0 = x0.to(device).unsqueeze(0)
@@ -292,9 +295,8 @@ def generate_dataset(
                     pin_memory=False if device == "cpu" else True,
                 )
 
-                batches = len(train_dataloader) - int(train_dataloader.drop_last)
                 iterator = iter(train_dataloader)
-                for _ in range(batches):
+                for _ in range(len(train_dataloader) - int(train_dataloader.drop_last)):
                     x = next(iterator)
                     x = x[0] if isinstance(x, list) or isinstance(x, tuple) else x
                     x = x.to(device)
@@ -336,20 +338,18 @@ def generate_dataset(
                 pin_memory=True,
             )
 
-            batches = len(test_dataloader) - int(test_dataloader.drop_last)
             iterator = iter(test_dataloader)
-            for i in range(batches):
+            for i in range(len(test_dataloader) - int(test_dataloader.drop_last)):
                 x = next(iterator)
                 x = x[0] if isinstance(x, list) or isinstance(x, tuple) else x
                 x = x.to(device)
 
-                # Calculate batch size
                 bsize = x.size()[0]
 
                 # choose operator and generate measurement
                 y, params = measure(x, b=bsize, g=g)
 
-                if i == 0:  # create dict
+                if i == 0:
                     hf.create_dataset(
                         "x_test", (n_test_g,) + x.shape[1:], dtype=x.cpu().numpy().dtype
                     )
@@ -373,6 +373,54 @@ def generate_dataset(
                             params[p].to("cpu").numpy()
                         )
                 index = index + bsize
+
+        if val_dataset is not None:
+            index = 0
+            val_dataloader = DataLoader(
+                Subset(
+                    val_dataset, indices=list(range(g * n_val_g, (g + 1) * n_val_g))
+                ),
+                batch_size=batch_size,
+                num_workers=num_workers,
+                pin_memory=True,
+            )
+
+            iterator = iter(val_dataloader)
+            for i in range(len(val_dataloader) - int(val_dataloader.drop_last)):
+                x = next(iterator)
+                x = x[0] if isinstance(x, list) or isinstance(x, tuple) else x
+                x = x.to(device)
+
+                bsize = x.size()[0]
+
+                # choose operator and generate measurement
+                y, params = measure(x, b=bsize, g=g)
+
+                if i == 0:
+                    hf.create_dataset(
+                        "x_val", (n_val_g,) + x.shape[1:], dtype=x.cpu().numpy().dtype
+                    )
+                    hf.create_dataset(
+                        "y_val", (n_val_g,) + y.shape[1:], dtype=y.cpu().numpy().dtype
+                    )
+                    if save_physics_generator_params:
+                        for k, p in params.items():
+                            hf.create_dataset(
+                                f"{k}_val",
+                                (n_val_g,) + p.shape[1:],
+                                dtype=p.cpu().numpy().dtype,
+                            )
+
+                hf["x_val"][index : index + bsize] = x.to("cpu").numpy()
+                hf["y_val"][index : index + bsize] = y.to("cpu").numpy()
+
+                if save_physics_generator_params:
+                    for p in params.keys():
+                        hf[f"{p}_val"][index : index + bsize] = (
+                            params[p].to("cpu").numpy()
+                        )
+                index = index + bsize
+
         hf.close()
 
         if verbose:
