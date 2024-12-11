f95002f7a5b6d1446d6b00eae8c7529dfa755469
Andrewwango <andrew.wang27@gmail.com>
2024-12-11 18:31:43 +0000

    remove libcpab

--- deepinv/transform/libcpab/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Code taken directly from https://github.com/Andrewwango/libcpab
-which is a modified version of the original https://github.com/SkafteNicki/libcpab
-See LICENSE for more details
-"""
-
-# %%
-from .cpab import Cpab
