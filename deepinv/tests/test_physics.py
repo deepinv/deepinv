0c75262a4e33f39be6fedece8005c48babacb265
Nils Laurent <nils.laurent@ens-lyon.fr>
2024-12-13 10:29:50 +0100

    improve coarse blur kernel, improve multiscale for linearphysics

--- deepinv/tests/test_physics.py
+++ deepinv/tests/test_physics.py
@@ -77,15 +77,16 @@ NOISES = [
 ]
 
 
-def find_operator(name, device):
+def find_operator(name, device, sf=1):
     r"""
     Chooses operator
 
     :param name: operator name
     :param device: (torch.device) cpu or cuda
+    :param sf: (float) scale factor for multiscale tests
     :return: (deepinv.physics.Physics) forward operator.
     """
-    img_size = (3, 16, 8)
+    img_size = (3, sf*16, sf*8)
     norm = 1
     dtype = torch.float
     padding = None
@@ -211,9 +212,9 @@ def find_operator(name, device):
             1 + np.sqrt(np.prod(img_size) / m)
         ) ** 2 - 3.7  # Marcenko-Pastur law, second term is a small n correction
     elif name.startswith("deblur"):
-        img_size = (3, 17, 19)
+        img_size = (3, sf*17, sf*19)
         p = dinv.physics.Blur(
-            filter=dinv.physics.blur.gaussian_blur(sigma=(0.25, 0.1), angle=45.0),
+            filter=dinv.physics.blur.gaussian_blur(sigma=(sf*0.25, sf*0.1), angle=45.0),
             padding=padding,
             device=device,
         )
@@ -282,7 +283,7 @@ def find_operator(name, device):
             dtype=dtype,
         )
     elif name == "complex_compressed_sensing":
-        img_size = (1, 8, 8)
+        img_size = (1, sf*8, sf*8)
         m = 50
         p = dinv.physics.CompressedSensing(
             m=m,
@@ -335,7 +336,7 @@ def find_operator(name, device):
             noise_model=dinv.physics.GaussianNoise(0.0, rng=rng),
         )
     elif name == "structured_random":
-        img_size = (1, 8, 8)
+        img_size = (1, sf*8, sf*8)
         p = dinv.physics.StructuredRandom(
             input_shape=img_size, output_shape=img_size, device=device
         )
@@ -1040,26 +1041,24 @@ def test_decolorize(srf, device, imsize):
 
 
 @pytest.mark.parametrize("name", OPERATORS)
-def test_coarse_scale_similarity(name, device):
+def test_coarse_physics_validity(name, device):
     r"""
     Tests the similarity between a linear physics and its coarse version.
 
     :param name: operator name (see find_operator)
     :param device: (torch.device) cpu or cuda:x
     """
-    physics, imsize, _, dtype = find_operator(name, device)
+    physics, imsize, _, dtype = find_operator(name, device, sf=2)
     if not isinstance(physics, dinv.physics.LinearPhysics):
-        return
+        pytest.skip("Skip "+name+" : not LinearPhysics")
+    if not len(imsize) == 3:
+        pytest.skip("Skip "+name+" : not proper data shape")
 
-    x = torch.rand(imsize, device=device).unsqueeze(0)
-    physics.downsample_signal(x)
+    x = torch.rand(imsize, device=device, dtype=dtype).unsqueeze(0)
+    x_coarse = physics.downsample_signal(x)
 
-    try:
-        p_coarse = physics.to_coarse()
-    except NotImplementedError:
-        return
+    p_coarse = physics.to_coarse()
 
-    # linear case with to_carse implemented
     D = physics.downsampling_operator.A
     D_adj = physics.downsampling_operator.A_adjoint
     A = physics.A
@@ -1067,9 +1066,9 @@ def test_coarse_scale_similarity(name, device):
     Ac = p_coarse.A
     Ac_adj = p_coarse.A_adjoint
 
-    op_cmp = lambda x: D(A_adj(A(x))) - Ac_adj(Ac(D(x)))
-    op_adj = lambda x: A_adj(A(D_adj(x))) - D_adj(Ac_adj(Ac(x)))
-    cmp = dinv.physics.LinearPhysics(A=op_cmp, A_adjoint=op_adj)
-    error = cmp.compute_norm(x)
+    f = physics.downsampling_operator.factor
+    op_cmp = lambda xc: D(A_adj(A((f**2) * D_adj(xc)))) - Ac_adj(Ac(xc))
+    cmp = dinv.physics.LinearPhysics(A=op_cmp, A_adjoint=op_cmp)
+    error = cmp.compute_norm(x_coarse)
 
     assert error < 0.2
