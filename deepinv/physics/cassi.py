4e4cd96dd835a17b438d3c3d1e4414d5640bc5c0
Andrewwango <andrew.wang27@gmail.com>
2024-12-18 23:24:34 +0000

    cassi docs

--- deepinv/physics/cassi.py
+++ deepinv/physics/cassi.py
@@ -7,22 +7,45 @@ from deepinv.physics.generator import BernoulliSplittingMaskGenerator
 
 
 class CompressiveSpectralImaging(DecomposablePhysics):
-    """Coded-aperture snapshot spectral imaging operator.
+    """Compressive Hyperspectral Imaging operator.
+    
+    Coded-aperture snapshot spectral imaging (CASSI) operator, which is a popular
+    approach for hyperspectral imaging.
 
-    :param Tuple[int, int, int] img_size: C,H,W
-    :param Union[Tensor] mask: _description_, defaults to None
+    The CASSI operator performs a combination of masking ("coded aperture"), shearing,
+    and flattening in the channel dim.
+    We provide two specific implementations of CASSI: Single-Disperser (i.e. only spatial encoding)
+    and Spatial-Spectral encoding:
+
+    .. math::
+
+
+
+    For more details see e.g. `this overview <https://zaguan.unizar.es/record/75680/files/texto_completo.pdf>`_.
+
+    |sep|
+
+    :Examples:
+
+    
+
+    :param tuple img_size: image size, must be of form (C,H,W) where C is number of bands.
+    :param Tensor, float mask: coded-aperture mask. If ``None``, generate random mask using
+        :class:`deepinv.physics.generator.BernoulliSplittingMaskGenerator` with masking ratio
+        of 0.5, if mask is ``float``, sets mask ratio to this.
     :param str mode: 'sd' = single disperser (i.e. only spatial encoding) or
-        'ss' = spatial-spectral encoding, defaults to 'ss'.
-    :param float shear_factor: _description_, defaults to 0.5
-    :param str shear_dim: _description_, defaults to "h"
-    :param torch.device device: _description_, defaults to "cpu"
-    :param torch.Generator rng: _description_, defaults to None
+        'ss' = spatial-spectral encoding, defaults to 'ss'. See above for details.
+    :param float shear_factor: shear amount for spatial/spectral encoding. If 1, this equates
+        to integer pixel shear. Defaults to 0.5.
+    :param str shear_dim: shear in H-C plane or W-C plane where C is channel dim, defaults to "h"
+    :param torch.device device: torch device, only used if ``mask`` is ``None`` or ``float``
+    :param torch.Generator rng: torch random generator, only used if ``mask`` is ``None`` or ``float``
     """
 
     def __init__(
         self,
         img_size: Tuple[int, int, int],  # C,H,W
-        mask: Union[Tensor] = None,
+        mask: Union[Tensor, float] = None,
         mode: str = "ss",
         shear_factor: float = 0.5,
         shear_dim: str = "h",
@@ -30,6 +53,7 @@ class CompressiveSpectralImaging(DecomposablePhysics):
         rng: torch.Generator = None,
         **kwargs,
     ):
+        
         super().__init__(**kwargs)
 
         if len(img_size) != 3:
@@ -49,7 +73,7 @@ class CompressiveSpectralImaging(DecomposablePhysics):
             raise ValueError("mode must be either 'ss' or 'sd'.")
         self.mode = mode.lower()
 
-        if mask is None or isinstance(mask, int):
+        if mask is None or isinstance(mask, float):
             mask = BernoulliSplittingMaskGenerator(
                 img_size,
                 split_ratio=0.5 if mask is None else mask,
