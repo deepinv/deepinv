0ba3b3dffb93e174f0076e78ab2a99d6f1ed65e9
Julián Tachella <tachellajulian@gmail.com>
2025-01-09 09:44:21 +0100

    time docs

--- deepinv/physics/time.py
+++ deepinv/physics/time.py
@@ -19,8 +19,8 @@ class TimeMixin:
 
         Lets non-dynamic algorithms process dynamic data by treating time frames as batches.
 
-        :param torch.Tensor x: input tensor of shape (B, C, T, H, W)
-        :return torch.Tensor: output tensor of shape (B*T, C, H, W)
+        :param x: input tensor of shape (B, C, T, H, W)
+        :return: output tensor of shape (B*T, C, H, W)
         """
         B, C, T, H, W = x.shape
         return x.permute(0, 2, 1, 3, 4).reshape(B * T, C, H, W)
@@ -29,9 +29,9 @@ class TimeMixin:
     def unflatten(x: torch.Tensor, batch_size=1) -> torch.Tensor:
         """Creates new time dim from batch dim. Opposite of ``flatten``.
 
-        :param torch.Tensor x: input tensor of shape (B*T, C, H, W)
+        :param x: input tensor of shape (B*T, C, H, W)
         :param int batch_size: batch size, defaults to 1
-        :return torch.Tensor: output tensor of shape (B, C, T, H, W)
+        :return: output tensor of shape (B, C, T, H, W)
         """
         BT, C, H, W = x.shape
         return x.reshape(batch_size, BT // batch_size, C, H, W).permute(0, 2, 1, 3, 4)
@@ -42,19 +42,19 @@ class TimeMixin:
 
         Use when channel dim doesn't matter and you don't want to deal with annoying batch dimension problems (e.g. for transforms).
 
-        :param torch.Tensor x: input tensor of shape (B, C, T, H, W)
-        :return torch.Tensor: output tensor of shape (B, C*T, H, W)
+        :param x: input tensor of shape (B, C, T, H, W)
+        :return: output tensor of shape (B, C*T, H, W)
         """
         return x.reshape(x.shape[0], x.shape[1] * x.shape[2], x.shape[3], x.shape[4])
 
     @staticmethod
-    def wrap_flatten_C(f: Callable[[torch.Tensor], torch.Tensor]) -> torch.Tensor:
+    def wrap_flatten_C(f: Callable[[torch.Tensor], torch.Tensor]) -> Callable:
         """Flatten time dim into channel dim, apply function, then unwrap.
 
         The first argument is assumed to be the tensor to be flattened.
 
-        :param Callable f: function to be wrapped
-        :return Callable: wrapped function
+        :param f: function to be wrapped
+        :return: wrapped function
         """
 
         def wrapped(x: torch.Tensor, *args, **kwargs):
@@ -73,12 +73,13 @@ class TimeMixin:
         x: torch.Tensor, mask: torch.Tensor = None, dim: int = 2
     ) -> torch.Tensor:
         """Flatten time dim of x by averaging across frames.
+
         If mask is non-overlapping in time dim, then this will simply be the sum across frames.
 
-        :param torch.Tensor x: input tensor of shape (B,C,T,H,W) (e.g. time-varying k-space)
-        :param torch.Tensor mask: mask showing where ``x`` is non-zero. If not provided, then calculated from ``x``.
-        :param int dim: time dimension, defaults to 2 (i.e. shape B,C,T,H,W)
-        :return torch.Tensor: flattened tensor with time dim removed of shape (B,C,H,W)
+        :param x: input tensor of shape `(B,C,T,H,W)` (e.g. time-varying k-space)
+        :param mask: mask showing where ``x`` is non-zero. If not provided, then calculated from ``x``.
+        :param dim: time dimension, defaults to 2 (i.e. shape `B,C,T,H,W`)
+        :return: flattened tensor with time dim removed of shape `(B,C,H,W)`
         """
         _x = x.sum(dim)
         out = zeros_like(_x)
@@ -91,10 +92,10 @@ class TimeMixin:
     def repeat(x: torch.Tensor, target: torch.Tensor, dim: int = 2) -> torch.Tensor:
         """Repeat static image across new time dim T times. Opposite of ``average``.
 
-        :param torch.Tensor x: input tensor of shape (B,C,H,W)
-        :param torch.Tensor target: any tensor of desired shape (B,C,T,H,W)
-        :param int dim: time dimension, defaults to 2 (i.e. shape B,C,T,H,W)
-        :return torch.Tensor: tensor with new time dim of shape B,C,T,H,W
+        :param x: input tensor of shape `(B,C,H,W)`
+        :param target: any tensor of desired shape `(B,C,T,H,W)`
+        :param dim: time dimension, defaults to 2 (i.e. shape `B,C,T,H,W`)
+        :return: tensor with new time dim of shape `(B,C,T,H,W)`
         """
         return x.unsqueeze(dim=dim).expand_as(target)
 
