3f610085dc05bf4065c4c93230b2c4d1ed40999d
mathurinm <mathurin.massias@gmail.com>
2025-02-14 15:58:47 +0100

    progress example

--- examples/plug-and-play/demo_PnPflow.py
+++ examples/plug-and-play/demo_PnPflow.py
@@ -2,12 +2,35 @@ r"""
 Implementing PnP-Flow
 =====================
 
-In this tutorial, we revisit the implementation of the PnP-Flow flow matching
-algorithm for image reconstruction from
-`Martin et al. (ICLR 2025) <https://arxiv.org/abs/2410.02423>`_. The full 
-algorithm is implemented in :class:`deepinv.optim.pnpflow.PnPFlow`.
-"""
+The example implements the PnP-Flow flow matching algorithm for image reconstruction from `Martin et al. (ICLR 2025) <https://arxiv.org/abs/2410.02423>`_. 
+ 
+The full algorithm is implemented in :class:`deepinv.optim.pnpflow.PnPFlow`.
 
+PnPFlow iterates on timesteps :math:`t \in [0,1]`. It alternates between 1) 
+a gradient step on the data fidelity, 2) an interpolation step and  3) 
+a denoising step.
+
+With a datafit term :math:`f`, the iterations are:
+
+.. math::
+
+    \begin{equation*}
+    \begin{aligned}
+    &x_t = x_{t-1} - \eta \nabla f(x_{t-1}) \\
+    &z_t = (1-t) x_t + t \varepsilon_t \\
+    &x_{t+1} = D_\theta(z_t, t)
+    \end{aligned}
+    \end{equation*}
+
+where :math:`\varepsilon_t \sim \mathcal N(0, \mathrm{Id})`, and the denoiser
+:math:`D_\theta` builds upon the velocity field :math:`v_\theta` of a 
+pretrained flow matching model:
+
+.. math::
+
+    D_\theta(x, t) = x + (1-t) v_\theta(x,t),
+
+"""
 
 import numpy as np
 import torch
@@ -32,58 +55,26 @@ torch.manual_seed(0)
 device = dinv.utils.get_freer_gpu() if torch.cuda.is_available() else "cpu"
 
 # %%
-# Load the Flow Matching generative model.
-#  --------------------------------------------------------------------------------
-# This generative model is an Optimal Transport Flow Matching model trained on the CelebA dataset.
+# Load the Flow Matching generative model
+#  --------------------------------------
+# PnPFlow uses a denoiser which is built on a flow matching velocity.
+# In this example, we will use a Flow Matching model trained with minibatch OT
+# on the CelebA dataset.
 
-mynet = FlowUNet(input_channels=3, input_height=128,
-                 pretrained="download", device=device)
+velocity = FlowUNet(input_channels=3, input_height=128,
+                    pretrained="download", device=device)
+
 
 # %%
-# Set up the PnP Flow algorithm to solve inverse problems.
-#  --------------------------------------------------------------------------------
-# This method iterates on timesteps :math:`t \in [0,1]`.
-# It alternates between a gradient step on the data fidelity, an interpolation step and a denoising step.
-# The interpolation step at time :math:`t` writes as
-#
-# .. math::
-#
-#           z_t = (1-t) x_t + t \epsilon
-#
-# where :math:`\epsilon \sim \mathcal N(0, \mathrm{Id})`.
-#
-# The denoiser is defined as
-#
-# .. math::
-#
-#     D_\theta(x, t) = x + (1-t) v_\theta(x,t),
-#
-# where :math:`v_\theta` is the velocity field given by the pretrained flow mathching model.
-
-
-### INPAINTING EXAMPLE ###
-pnpflow = PnPFlow(
-    mynet,
-    data_fidelity=L2(),
-    verbose=True,
-    max_iter=100,
-    device=device,
-    lr=1.0,
-    lr_exp=0.6,
-)
-
-# %%
-# In this tutorial we first consider mask inpainting as the inverse problem, where the forward operator is implemented
-# in :class:`deepinv.physics.Inpainting`. In the example that we use, the mask is a centered black square with size 60x60,
-# and we will additionally have Additive White Gaussian Noise (AWGN) of standard deviation  12.75/255.
-
-print("Running inpainting example")
-
+# First, we consider the problem of mask inpainting. The forward operator is
+# # implemented in :class:`deepinv.physics.Inpainting`. The mask we use is
+# a centered black square with size 60x60, and We additionally use Additive
+# White Gaussian Noise of standard deviation  12.75/255.
 
 url = get_image_url("celeba_example.jpg")
 x_true = load_url_image(url=url, img_size=128,
                         resize_mode="resize", device=device)
-x = x_true.clone()
+x = 2 * x_true.clone() - 1  # values in [-1, 1]
 mask = torch.ones_like(x)
 mask[:, :, 32:96, 32:96] = 0
 sigma_noise = 12.5 / 255.0  # noise level
@@ -94,15 +85,29 @@ physics = dinv.physics.Inpainting(
     noise_model=dinv.physics.GaussianNoise(sigma=sigma_noise),
     device=device,
 )
-y = physics(2 * x - 1)
+y = physics(x)
 
 # %%
 # Run the PnP method
 # ------------------
-x_hat = pnpflow(y, physics)
+max_iter = 100
+delta = 1 / max_iter
+lr = 1.0,
+lr_exp = 0.6,
+x_hat = physics.A_adjoint(y)
 
+for it in tqdm(range(max_iter)):
+    t = torch.ones(len(x), device=device) * delta * it
+    lr_t = self.lr * (1 - t.view(-1, 1, 1, 1)) ** self.lr_exp
+    z = x - lr_t * self.data_fidelity.grad(x, y, physics)
+    x_new = torch.zeros_like(x)
+    for _ in range(self.n_avg):
+        z_tilde = self.interpolation_step(z, t.view(-1, 1, 1, 1))
+        x_new += self.denoiser(z_tilde, t)
+    x_new /= self.n_avg
+    x = x_new
 
-imgs = [y, x_true, (x_hat + 1.0) * 0.5]
+imgs = [y, x_true, x_hat]
 plot(
     imgs,
     titles=["measurement", "ground-truth", "reconstruction"],
@@ -121,7 +126,7 @@ print("PSNR restored image :", dinv.metric.PSNR()
 
 print("Running deblurring example")
 pnpflow = PnPFlow(
-    mynet,
+    velocity,
     data_fidelity=L2(),
     verbose=True,
     max_iter=100,
@@ -157,3 +162,17 @@ plot(
 print("PSNR noisy image :", dinv.metric.PSNR()((y + 1.0) * 0.5, x_true).item())
 print("PSNR restored image :", dinv.metric.PSNR()
       ((x_hat + 1.0) * 0.5, x_true).item())
+
+
+##
+# To run the algorithm directly, one can do:
+pnpflow = PnPFlow(
+    velocity,
+    data_fidelity=L2(),
+    verbose=True,
+    max_iter=100,
+    device=device,
+    lr=1.0,
+    lr_exp=0.6,
+)
+pnpflow(y, physics)
