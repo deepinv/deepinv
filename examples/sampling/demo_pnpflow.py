6b7dfb09419efeb63d15f97668ffa0593cd97fd9
Anne Gagneux <anne63gg@hotmail.fr>
2025-02-14 13:53:17 +0100

    doc example

--- examples/sampling/demo_pnpflow.py
+++ examples/sampling/demo_pnpflow.py
@@ -25,10 +25,39 @@ from deepinv.utils.plotting import config_matplotlib
 config_matplotlib()
 
 
+# Set the global random seed from pytorch to ensure
+# the reproducibility of the example.
+torch.manual_seed(0)
 device = dinv.utils.get_freer_gpu() if torch.cuda.is_available() else "cpu"
-torch.manual_seed(1)
 
-mynet = FlowUNet(input_channels=3, input_height=128, pretrained="True", device=device)
+# %%
+# Load the Flow Matching generative model.
+#  --------------------------------------------------------------------------------
+# This generative model is an Optimal Transport Flow Matching model trained on the CelebA dataset.
+
+mynet = FlowUNet(input_channels=3, input_height=128,
+                 pretrained="download", device=device)
+
+# %%
+# Set up the PnP Flow algorithm to solve inverse problems.
+#  --------------------------------------------------------------------------------
+# This method iterates on timesteps :math:`t \in [0,1]`.
+# It alternates between a gradient step on the data fidelity, an interpolation step and a denoising step.
+# The interpolation step at time :math:`t` writes as
+#
+# .. math::
+#
+#           z_t = (1-t) x_t + t \epsilon
+#
+# where :math:`\epsilon \sim \mathcal N(0, \mathrm{Id})`.
+#
+# The denoiser is defined as
+#
+# .. math::
+#
+#     D_\theta(x, t) = x + (1-t) v_\theta(x,t),
+#
+# where :math:`v_\theta` is the velocity field given by the pretrained flow mathching model.
 
 
 ### INPAINTING EXAMPLE ###
@@ -41,11 +70,18 @@ pnpflow = PnPFlow(
     lr=1.0,
     lr_exp=0.6,
 )
+
+# %%
+# In this tutorial we first consider mask inpainting as the inverse problem, where the forward operator is implemented
+# in :class:`deepinv.physics.Inpainting`. In the example that we use, the mask is a centered black square with size 60x60,
+# and we will additionally have Additive White Gaussian Noise (AWGN) of standard deviation  12.75/255.
+
 print("Running inpainting example")
 
+
 url = get_image_url("celeba_example.jpg")
-# url = get_image_url("69037.png")
-x_true = load_url_image(url=url, img_size=128, resize_mode="resize", device=device)
+x_true = load_url_image(url=url, img_size=128,
+                        resize_mode="resize", device=device)
 x = x_true.clone()
 mask = torch.ones_like(x)
 mask[:, :, 32:96, 32:96] = 0
@@ -58,6 +94,10 @@ physics = dinv.physics.Inpainting(
     device=device,
 )
 y = physics(2 * x - 1)
+
+# %%
+# Run the PnP method
+# ------------------
 x_hat = pnpflow.forward(y, physics)
 
 
@@ -69,10 +109,15 @@ plot(
     save_dir=".",
 )
 
-print("PSNR noisy image :", dinv.metric.PSNR()(y, x_true).item())
-print("PSNR restored image :", dinv.metric.PSNR()((x_hat + 1.0) * 0.5, x_true).item())
+print("PSNR noisy image :", dinv.metric.PSNR()((y + 1.0) * 0.5, x_true).item())
+print("PSNR restored image :", dinv.metric.PSNR()
+      ((x_hat + 1.0) * 0.5, x_true).item())
 
-### DEBLURRING EXAMPLE ###
+# %%
+# Next, we consider a deblurring problme, where the forward operator is implemented
+# in :class:`deepinv.physics.BlurFFT`. In the example, we use Gaussian blur
+# and we will additionally have Additive White Gaussian Noise (AWGN) of standard deviation  12.75/255.
+
 print("Running deblurring example")
 pnpflow = PnPFlow(
     mynet,
@@ -85,7 +130,8 @@ pnpflow = PnPFlow(
 )
 
 url = get_image_url("celeba_example2.jpg")
-x_true = load_url_image(url=url, img_size=128, resize_mode="resize", device=device)
+x_true = load_url_image(url=url, img_size=128,
+                        resize_mode="resize", device=device)
 x = x_true.clone()
 sigma_noise = 12.75 / 255.0  # noise level
 
@@ -107,5 +153,6 @@ plot(
     save_dir=".",
 )
 
-print("PSNR noisy image :", dinv.metric.PSNR()(y, x_true).item())
-print("PSNR restored image :", dinv.metric.PSNR()((x_hat + 1.0) * 0.5, x_true).item())
+print("PSNR noisy image :", dinv.metric.PSNR()((y + 1.0) * 0.5, x_true).item())
+print("PSNR restored image :", dinv.metric.PSNR()
+      ((x_hat + 1.0) * 0.5, x_true).item())
