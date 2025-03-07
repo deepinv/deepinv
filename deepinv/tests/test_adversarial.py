5d6e6dda4925d0409cb05183f596bbb844e0b91f
Andrewwango <andrew.wang27@gmail.com>
2025-03-07 12:29:48 +0000

    csgm test reaches tol

--- deepinv/tests/test_adversarial.py
+++ deepinv/tests/test_adversarial.py
@@ -25,7 +25,9 @@ def choose_adversarial_combo(combo_name, imsize, device):
     ).to(device)
 
     csgm_generator = dinv.models.CSGMGenerator(
-        dinv.models.DCGANGenerator(nz=10, ngf=8, nc=imsize[0]), inf_max_iter=20
+        dinv.models.DCGANGenerator(nz=10, ngf=8, nc=imsize[0]),
+        inf_max_iter=10,
+        inf_tol=1,
     ).to(device)
 
     if combo_name == "DeblurGAN":
