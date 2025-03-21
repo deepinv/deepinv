8a8e2097836a712664c0e634bf6c07ca7bfa2a8f
Samuel Hurault <samuel.hurault@u-bordeaux.fr>
2025-03-21 10:25:00 +0100

    black

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,11 +36,11 @@ extensions = [
     "sphinxemoji.sphinxemoji",
     "sphinx_copybutton",
     "sphinx_design",
-    'sphinxcontrib.bibtex'
+    "sphinxcontrib.bibtex",
 ]
 
-bibtex_bibfiles = ['refs.bib']
-bibtex_default_style = 'plain'
+bibtex_bibfiles = ["refs.bib"]
+bibtex_default_style = "plain"
 copybutton_exclude = ".linenos, .gp"
 
 intersphinx_mapping = {
