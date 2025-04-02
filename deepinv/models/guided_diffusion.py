combine model and precond. Fixing scaling

deepinv/models/guided_diffusion.py
@@ -11,12 +11,12 @@ from .base import Denoiser
 from torch.nn import Linear, GroupNorm
 from math import floor
 from .utils import get_weights_url
-import warnings
-
 
 class ADMUNet(Denoiser):
     r"""
     Re-implementation of the architecture from the paper: `Diffusion Models Beat GANs on Image Synthesis <https://arxiv.org/abs/2105.05233>`_.
+
+    The model is also pre-conditioned by the method described in the paper `Elucidating the Design Space of Diffusion-Based Generative Models <https://arxiv.org/pdf/2206.00364>`_.
 
     Equivalent to the original implementation by Dhariwal and Nichol, available at: https://github.com/openai/guided-diffusion.
     The architecture consists of a series of convolution layer, down-sampling residual blocks and up-sampling residual blocks with skip-connections.
@@ -40,6 +40,7 @@ class ADMUNet(Denoiser):
         online repository (the default model is a conditional model trained on ImageNet at 64x64 resolution (`imagenet64-cond`) with default architecture).
         Finally, ``pretrained`` can also be set as a path to the user's own pretrained weights.
         See :ref:`pretrained-weights <pretrained-weights>` for more details.
+    :param float sigma_data: The standard deviation of the data distribution. Default to `0.5`.
     :param torch.device device: Instruct our module to be either on cpu or on gpu. Default to ``None``, which suggests working on cpu.
     """
 
@@ -63,6 +64,7 @@ class ADMUNet(Denoiser):
         dropout=0.10,  # List of resolutions with self-attention.
         label_dropout=0,  # Dropout probability of class labels for classifier-free guidance.
         pretrained: str = None,
+        sigma_data:float = 0.5,
         device=None,
         *args,
         **kwargs,
@@ -176,22 +178,57 @@ class ADMUNet(Denoiser):
                 ckpt = torch.hub.load_state_dict_from_url(
                     url, map_location=lambda storage, loc: storage, file_name=name
                 )
-                warnings.warn(
-                    "The pre-trained model was trained on `[-1,1]` data and should be used together with the pre-conditioner `deepinv.models.EDMPrecond`."
-                )
-                self.train_on_minus_one_one = True
+
+                self._train_on_minus_one_one = True
             else:
                 ckpt = torch.load(pretrained, map_location=lambda storage, loc: storage)
             self.load_state_dict(ckpt, strict=True)
 
         self.eval()
+        self.sigma_data = sigma_data
         if device is not None:
             self.to(device)
             self.device = device
 
-    def forward(self, x, sigma, class_labels=None, augment_labels=None):
+    def forward(self, x, sigma, class_labels=None, augment_labels=None, *args, **kwargs):
         r"""
         Run the denoiser on noisy image.
+
+        :param torch.Tensor x: noisy image
+        :param Union[torch.Tensor, float]  sigma: noise level
+        :param torch.Tensor class_labels: class labels
+        :param torch.Tensor augment_labels: augmentation labels
+        :return torch.Tensor: denoised image.
+        """
+        if class_labels is not None:
+            class_labels = class_labels.to(torch.float32)
+        sigma = self._handle_sigma(sigma, torch.float32, x.device, x.size(0))
+        
+        # Rescale [0,1] input to [-1,-1] 
+        if self._train_on_minus_one_one:
+            x = (x - 0.5) * 2.0 
+            sigma = sigma * 2.
+        c_skip = self.sigma_data**2 / (sigma**2 + self.sigma_data**2)
+        c_out = sigma * self.sigma_data / (sigma**2 + self.sigma_data**2).sqrt()
+        c_in = 1 / (self.sigma_data**2 + sigma**2).sqrt()
+        c_noise = sigma.log() / 4
+
+        F_x = self.forward_unet(c_in * x,
+            c_noise.flatten(),
+            class_labels=class_labels,
+            augment_labels=augment_labels,
+        )
+        D_x = c_skip * x + c_out * F_x
+
+        # Rescale [-1,1] output to [0,-1] 
+        if self._train_on_minus_one_one:
+            return (D_x + 1.) / 2.
+        else:
+            return D_x
+
+    def forward_unet(self, x, sigma, class_labels=None, augment_labels=None):
+        r"""
+        Run the unet on noisy image.
 
         :param torch.Tensor x: noisy image
         :param Union[torch.Tensor, float] sigma: noise level
@@ -201,7 +238,6 @@ class ADMUNet(Denoiser):
         :return: (:class:`torch.Tensor`) denoised image.
         """
         # Mapping.
-        sigma = self._handle_sigma(sigma, x.dtype, x.device, x.size(0))
         emb = self.map_noise(sigma)
         if self.map_augment is not None and augment_labels is not None:
             emb = emb + self.map_augment(augment_labels)

deepinv/sampling/diffusion_sde.py
@@ -111,24 +111,15 @@ class _RescaledDenoiser(nn.Module):
     sampling, where Gaussian noise (often with large magnitudes) is added, requiring consistent input/output
     ranges across training and inference.
 
-
     :param deepinv.models.Denoiser model: The original denoiser model trained on `[0, 1]` image data.
     """
 
     def __init__(self, model):
         super().__init__()
         self.model = model
-        if hasattr(model, "train_on_minus_one_one"):
-            self.train_on_minus_one_one = model.train_on_minus_one_one
-        else:
-            self.train_on_minus_one_one = False
 
     def forward(self, x: Tensor, t: float, *args, **kwargs):
-        if self.train_on_minus_one_one:
-            return self.model(x, t, *args, **kwargs)
-        else:
-            return self.model((x + 1.0) / 2.0, t / 2.0, *args, **kwargs) * 2.0 - 1.0
-
+        return self.model((x + 1.0) / 2.0, t / 2.0, *args, **kwargs) * 2.0 - 1.0
 
 class DiffusionSDE(BaseSDE):
     r"""
@@ -215,7 +206,7 @@ class DiffusionSDE(BaseSDE):
         """
         raise NotImplementedError
 
-    def mean_t(self, t: Union[Tensor, float]) -> Tensor:
+    def scale_t(self, t: Union[Tensor, float]) -> Tensor:
         r"""
         The mean :math:`mu_t` of the condition distribution :math:`p(x_t \vert x_0) \sim \mathcal{N}(\mu_t * x_t, \sigma_t^2 \mathrm{Id})`.
 
@@ -317,8 +308,8 @@ class VarianceExplodingDiffusion(DiffusionSDE):
         t = self._handle_time_step(t)
         return self.sigma_min * (self.sigma_max / self.sigma_min) ** t
 
-    def mean_t(self, t: Union[Tensor, float]) -> Tensor:
-        return 0.0
+    def scale_t(self, t: Union[Tensor, float]) -> Tensor:
+        return torch.ones_like(self._handle_time_step(t))
 
     def score(self, x: Tensor, t: Union[Tensor, float], *args, **kwargs) -> Tensor:
         std = self.sigma_t(t)
@@ -421,17 +412,16 @@ class VariancePreservingDiffusion(DiffusionSDE):
             1.0 - torch.exp(-0.5 * t**2 * self.beta_d - t * self.beta_min)
         )
 
-    def mean_t(self, t: Union[Tensor, float]) -> Tensor:
+    def scale_t(self, t: Union[Tensor, float]) -> Tensor:
         t = self._handle_time_step(t)
-        return torch.exp(-0.25 * t**2 * self.beta_d - 0.5 * t * self.beta_min)
+        scale = torch.exp(-0.25 * t**2 * self.beta_d - 0.5 * t * self.beta_min)
+        return torch.clip(scale, min=1e-3)
 
     def score(self, x: Tensor, t: Union[Tensor, float], *args, **kwargs) -> Tensor:
-        std = self.sigma_t(t)
-        mean = self.mean_t(t).view(-1, 1, 1, 1)
-        denoised = self.denoiser(
-            x.to(torch.float32), std.to(torch.float32), *args, **kwargs
-        ).to(self.dtype)
-        score = (denoised * mean - x.to(self.dtype)) / std.view(-1, 1, 1, 1).pow(2)
+        sigma = self.sigma_t(t)
+        scale = self.scale_t(t)
+        denoised = self.denoiser((x / scale.view(-1, 1, 1, 1)).to(torch.float32), (sigma / scale).to(torch.float32), *args, **kwargs).to(self.dtype)
+        score = (denoised * scale.view(-1, 1, 1, 1) - x.to(self.dtype)) / sigma.view(-1, 1, 1, 1).pow(2)
         return score
 
 
@@ -597,10 +587,9 @@ class PosteriorDiffusion(Reconstructor):
         if isinstance(self.data_fidelity, ZeroFidelity):
             return self.sde.score(x, t, *args, **kwargs).to(self.dtype)
         else:
-            from functools import partial
-
-            sigma = self.sde.sigma_t(t).to(torch.float32)
-            score = self.sde.score(x, t, *args, **kwargs) - self.data_fidelity.grad(
-                x.to(torch.float32), y.to(torch.float32), physics=physics, sigma=sigma
-            )
+            unconditional_score = self.sde.score(x, t, *args, **kwargs)
+            scale = self.sde.scale_t(t)
+            sigma = self.sde.sigma_t(t)
+            conditional_score =  - self.data_fidelity.grad((x / scale).to(torch.float32) / scale, y.to(torch.float32), physics = physics, sigma = (sigma / scale).to(torch.float32))
+            score = unconditional_score + conditional_score
             return score.to(self.dtype)

deepinv/sampling/sde_solver.py
@@ -123,7 +123,7 @@ class BaseSDESolver(nn.Module):
                 timesteps = torch.from_numpy(timesteps.copy())
             timesteps = timesteps.to(sde.device, sde.dtype)
 
-        for t_cur, t_next in tqdm(zip(timesteps[:-1], timesteps[1:])):
+        for t_cur, t_next in tqdm(zip(timesteps[:-1], timesteps[1:]), total=timesteps.size(0)-1):
             x, cur_nfe = self.step(sde, t_cur, t_next, x, *args, **kwargs)
             nfe += cur_nfe
             if get_trajectory:

examples/sampling/demo_diffusion_sde.py
@@ -46,12 +46,12 @@ We implement various data-fidelity terms in `the documentations <https://deepinv
 
 import torch
 import deepinv as dinv
-from deepinv.models import NCSNpp, EDMPrecond
+from deepinv.models import NCSNpp
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 dtype = torch.float64
 figsize = 2.5
-gif_frequency = 5  # Increase this value to save the GIF saving time
+gif_frequency = 50  # Increase this value to save the GIF saving time
 # %%
 from deepinv.sampling import (
     PosteriorDiffusion,
@@ -64,8 +64,7 @@ from deepinv.optim import ZeroFidelity
 # In this example, we use the pre-trained FFHQ-64 model from the
 # EDM framework: https://arxiv.org/pdf/2206.00364 .
 # The network architecture is from Song et al: https://arxiv.org/abs/2011.13456 .
-unet = NCSNpp(pretrained="download")
-denoiser = EDMPrecond(model=unet).to(device)
+denoiser = NCSNpp(pretrained="download").to(device)
 
 # The solution is obtained by calling the SDE object with a desired solver (here, Euler).
 # The reproducibility of the SDE Solver class can be controlled by providing the pseudo-random number generator.
@@ -76,7 +75,7 @@ solver = EulerSolver(timesteps=timesteps, rng=rng)
 
 
 sigma_min = 0.02
-sigma_max = 20
+sigma_max = 40
 sde = VarianceExplodingDiffusion(
     sigma_max=sigma_max,
     sigma_min=sigma_min,
@@ -84,7 +83,7 @@ sde = VarianceExplodingDiffusion(
     device=device,
     dtype=dtype,
 )
-
+# sde = VariancePreservingDiffusion(device=device, dtype=dtype)
 
 # %%
 # Reverse-time SDE as sampling process
@@ -107,6 +106,7 @@ sample_seed_1, trajectory_seed_1 = model(
     x_init=(1, 3, 64, 64),
     seed=1,
     get_trajectory=True,
+
 )
 dinv.utils.plot(
     sample_seed_1,
@@ -392,9 +392,10 @@ timesteps = torch.linspace(1, 0.001, 400)
 solver = EulerSolver(timesteps=timesteps, rng=rng)
 denoiser = dinv.models.DRUNet(pretrained="download").to(device)
 
-sde = VarianceExplodingDiffusion(
-    sigma_max=sigma_max, sigma_min=sigma_min, alpha=0.75, device=device, dtype=dtype
-)
+# sde = VarianceExplodingDiffusion(
+#     sigma_max=sigma_max, sigma_min=sigma_min, alpha=0.75, device=device, dtype=dtype
+# )
+sde = VariancePreservingDiffusion(beta_max=5., beta_min=0.01, device=device, dtype=dtype)
 x = dinv.utils.load_url_image(
     dinv.utils.demo.get_image_url("butterfly.png"), img_size=256, resize_mode="resize"
 ).to(device)
@@ -491,11 +492,11 @@ sigma_max = 100
 rng = torch.Generator(device)
 timesteps = torch.linspace(1, 0.001, 300)
 solver = EulerSolver(timesteps=timesteps, rng=rng)
-sde = VarianceExplodingDiffusion(
-    sigma_max=sigma_max, sigma_min=sigma_min, alpha=1.0, device=device, dtype=dtype
-)
+# sde = VarianceExplodingDiffusion(
+#     sigma_max=sigma_max, sigma_min=sigma_min, alpha=1.0, device=device, dtype=dtype
+# )
 
-sde = VarianceExplodingDiffusion(device=device, dtype=dtype)
+sde = VariancePreservingDiffusion(device=device, dtype=dtype)
 x = dinv.utils.load_url_image(
     dinv.utils.demo.get_image_url("celeba_example.jpg"),
     img_size=256,
@@ -511,7 +512,7 @@ physics = dinv.physics.Inpainting(
 y = physics(x)
 
 model = PosteriorDiffusion(
-    data_fidelity=DPSDataFidelity(),
+    data_fidelity=DPSDataFidelity(denoiser=denoiser),
     denoiser=denoiser,
     sde=sde,
     solver=solver,