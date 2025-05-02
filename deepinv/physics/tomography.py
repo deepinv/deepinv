dd6301cb420e3fce8e87d7c85c164d2a12063b52
romainvo <romain.vo@hotmail.fr>
2025-05-03 00:23:45 +0200

    Add astra option to demos

--- deepinv/physics/tomography.py
+++ deepinv/physics/tomography.py
@@ -205,7 +205,10 @@ class Tomography(LinearPhysics):
 
 
 class TomographyWithAstra(LinearPhysics):
-    r"""Computed Tomography operator with ``astra-toolbox`` backend. It is more memory efficient than the ``Tomography`` operator and support 3d geometries.
+    r"""Computed Tomography operator with `astra-toolbox <https://astra-toolbox.com/>`_ backend.
+    It is more memory efficient than the ``Tomography`` operator and support 3d geometries.
+    See documentation of :class:`deepinv.physics.functional.XrayTransform` for more
+    information on the ``astra`` wrapper.
 
     Mathematically, it is described as a ray transform
     :math:`R` which linearly integrates an object :math:`x` along straight
@@ -219,27 +222,32 @@ class TomographyWithAstra(LinearPhysics):
     trajectory. Given different acquisition systems, the lines along which
     the integrals are computed follow different geometries:
 
-    * parallel. (2d and 3d) Per view, all rays intersecting the
-        object are parallel. In 2d, all rays live on the same plane, perpendicular
+    * parallel. (2d and 3d)
+        Per view, all rays intersecting the object are parallel. In 2d, all rays live on the same plane, perpendicular
         to the axis of rotation.
-    * fanbeam. (2d) All rays come from a single source and intersect the object
-        at a certain angle. The detector consists of a 1d line of cells. Similar to
+    * fanbeam. (2d)
+        Per view, all rays come from a single source and intersect the object at a certain angle. The detector consists of a 1d line of cells. Similar to
         the 2d "parallel", all rays live on the same plane, perpendicular to the axis of rotation.
-    * conebeam. (3d) All rays come from a single source. The detector consists
-        of a 2d grid of cells.
+    * conebeam. (3d)
+        Per view, all rays come from a single source. The detector consists of a 2d grid of cells. Apart from the central plane, the set of rays coming onto
+        a line of cells live on a tilted plane.
+
+    .. note::
+
+        The :class:`deepinv.physics.functional.XrayTransform` sequentially processes batch elements, which can make the 2d parallel beam operator with :class:`TomographyWithAstra` significantly slower than the its native-torch counterpart with :class:`deepinv.physics.Tomography`.
 
     .. note::
 
         The pseudo-inverse is computed using the filtered back-projection
-        algorithm with a Ramp filter, and its equivalent for conebeam 3d the
+        algorithm with a Ramp filter, and its equivalent for conebeam 3d, the
         Feldkamp-Davis-Kress algorithm. This is not the exact linear pseudo-inverse
         of the Ray Transform, but it is a good approximation which is robust to noise.
-        
+
     .. note::
-    
+
         In the default configuration, reconstruction cells and detector cells are
-        set to have isotropic unit lengths. It correspond to a 2d parallel beam 
-        and matches the default configuration of the ``Tomography`` operator with 
+        set to have isotropic unit lengths. It correspond to a 2d parallel beam
+        and matches the default configuration of the :class:`deepinv.physics.Tomography` operator with
         ``circle=False``.
 
     .. warning::
@@ -250,7 +258,7 @@ class TomographyWithAstra(LinearPhysics):
         is approximated by rescaling the backprojector.
 
     :param tuple[int, ...] img_shape: Shape of the object grid, either a 2 or 3-element tuple, for respectively 2d or 3d.
-    :param int num_angles: Number of angular positions sampled uniformly in ``angular_range``.
+    :param int num_angles: Number of angular positions sampled uniformly in ``angular_range``. (default: 180)
     :param int | tuple[int, ...] | None num_detectors: In 2d, specify an integer for a single line of detector cells. In 3d, specify a 2-element tuple for (row,col) shape of the detector. (default: None)
     :param tuple[float, float] angular_range: Angular range, defaults to (0,``math.pi``).
     :param float | tuple[float, float] detector_spacing: In 2d the width of a detector cell. In 3d a 2-element tuple specifying the (vertical, horizontal) dimensions of a detector cell. (default: 1.0)
@@ -258,20 +266,20 @@ class TomographyWithAstra(LinearPhysics):
     :param tuple[float, ...] | None aabb: Axis-aligned bounding-box of the reconstruction area [min_x, max_x, min_y, max_y, ...]. Optional argument, if specified, overrides argument ``object_spacing``. (default: None)
     :param torch.Tensor | None angles: Tensor containing angular positions in radii. Optional, if specified, overrides arguments ``num_angles`` and ``angular_range``. (default: None)
     :param str geometry_type: The type of geometry among ``'parallel'``, ``'fanbeam'`` in 2d and ``'parallel'`` and ``'conebeam'`` in 3d. (default: ``'parallel'``)
-    :param dict[str, str] | None geometry_parameters: Contains extra parameters specific to certain geometries. When ``geometry_type='fanbeam'`` or  ``'conebeam'``, the dictionnary should contains the keys 
+    :param dict[str, str] | None geometry_parameters: Contains extra parameters specific to certain geometries. When ``geometry_type='fanbeam'`` or  ``'conebeam'``, the dictionnary should contains the keys
 
         - "source_radius" distance between the x-ray source and the rotation axis, denoted :math:`D_{s0}` (default: 57.5)
 
         - "detector_radius" distance between the x-ray detector and the rotation axis, denoted :math:`D_{0d}` (default: 57.5)
-    :param bool normalize: If ``True``
+    :param bool normalize: If ``True`` :func:`A` and :func:`A_adjoint` are normalized so that the operator has unit norm. (default: ``False``)
     :param torch.device | str device: The operator only supports CUDA computation. (default: ``torch.device('cuda')``)
 
     |sep|
-    
+
     :Examples:
 
         Tomography operator with a 2d ``'fanbeam'`` geometry, 10 uniformly sampled angles in ``[0,2*math.pi]``, a detector line of 5 cells with length 2., a source-radius of 20.0 and a detector_radius of 20.0 for 5x5 image:
-        
+
         >>> from deepinv.physics import TomographyWithAstra
         >>> seed = torch.manual_seed(0)  # Random seed for reproducibility
         >>> x = torch.randn(1, 1, 5, 5, device='cuda') # Define random 5x5 image
@@ -284,7 +292,7 @@ class TomographyWithAstra(LinearPhysics):
                 detector_spacing=2.0,
                 geometry_type='fanbeam',
                 geometry_parameters={
-                    'source_radius': 20, 
+                    'source_radius': 20,
                     'detector_radius': 20
                 }
             )
@@ -299,7 +307,7 @@ class TomographyWithAstra(LinearPhysics):
                   [-2.4195,  3.1875,  1.8132, -2.3952, -3.5968],
                   [-1.6350,  1.4374,  2.2693, -2.2185, -3.7328],
                   [-1.9789,  0.1986, -0.2281, -1.7952, -0.3667]]]], device='cuda:0')
-                  
+
         Tomography operator with a 3d ``'conebeam'`` geometry, 10 uniformly sampled angles in ``[0,2*math.pi]``, a detector grid of 5x5 cells of size (2.,2.), a source-radius of 20.0 and a detector_radius of 20.0 for a 5x5x5 volume:
 
         >>> from deepinv.physics import TomographyWithAstra
@@ -314,7 +322,7 @@ class TomographyWithAstra(LinearPhysics):
                 detector_spacing=(2.0,2.0),
                 geometry_type='conebeam',
                 geometry_parameters={
-                    'source_radius': 20, 
+                    'source_radius': 20,
                     'detector_radius': 20
                 }
             )
@@ -323,19 +331,15 @@ class TomographyWithAstra(LinearPhysics):
         tensor([[[[[-2.0464,  0.4064, -1.5184, -0.9225,  1.5369],
                    [-2.3398, -0.9323,  2.0437,  0.5806, -1.5659],
                    [-1.0852,  2.0659,  1.1105, -1.7271, -2.6104]],
-
                   [[ 1.4757, -0.2731,  0.9386,  0.5791,  0.2995],
                    [-0.8362,  2.5918,  1.0941,  1.0576, -1.4501],
                    [-1.1313,  3.8354, -0.9572, -2.3721,  3.5149]],
-
                   [[ 0.6392,  0.1564, -0.8063, -3.8958,  1.2547],
                    [ 0.5294, -1.0241, -0.1792, -0.5054, -1.4253],
                    [-1.1961, -1.6911,  0.4279, -1.3608,  0.9488]],
-
                   [[ 0.5134,  2.1534, -3.8697,  0.3571,  0.1060],
                    [ 0.4687, -3.0669,  1.5911,  1.5235, -0.8031],
                    [-1.1990,  0.2637,  2.0889, -0.8894,  0.2550]],
-
                   [[-1.4643, -0.2128,  1.3425,  2.8803, -0.6605],
                    [ 0.9605,  1.1056,  4.2324, -3.5795, -0.1718],
                    [ 0.9207,  1.6948,  1.6556, -1.6624,  0.9960]]]]], device='cuda:0')
@@ -344,7 +348,7 @@ class TomographyWithAstra(LinearPhysics):
     def __init__(
         self,
         img_shape: tuple[int, ...],
-        num_angles: int,
+        num_angles: int = 180,
         num_detectors: int | tuple[int, ...] = None,
         angular_range: tuple[float, float] = (0, math.pi),
         detector_spacing: float | tuple[float, float] = 1.0,
@@ -353,6 +357,7 @@ class TomographyWithAstra(LinearPhysics):
         angles: Iterable[float] | None = None,
         geometry_type: str = "parallel",
         geometry_parameters: dict[str, Any] | None = None,
+        normalize: bool = False,
         device: torch.device | str = torch.device("cuda"),
         **kwargs,
     ):
@@ -362,19 +367,25 @@ class TomographyWithAstra(LinearPhysics):
             2,
             3,
         ), f"len(img_shape) is {len(img_shape)}, must be either 2 or 3 (for 2d and 3d respectively)"
-        
+
         if torch.device(device).type != "cuda":
-            raise RuntimeError("TomographyWithAstra only supports CUDA Tensors and CUDA operations")
+            raise RuntimeError(
+                "TomographyWithAstra only supports CUDA Tensors and CUDA operations"
+            )
 
         self.img_shape = img_shape
         self.is_2d = len(img_shape) == 2
-        self.num_detectors = math.ceil(math.sqrt(2) * img_shape[0]) if num_detectors is None else num_detectors
-        print(self.num_detectors, type(self.num_detectors))
+        self.num_detectors = (
+            math.ceil(math.sqrt(2) * img_shape[0])
+            if num_detectors is None
+            else num_detectors
+        )
         self.geometry_type = geometry_type
+        self.normalize = False
         self.device = device
 
         if angles is None:
-            angles = torch.linspace(*angular_range, steps=num_angles+1)[:-1]
+            angles = torch.linspace(*angular_range, steps=num_angles + 1)[:-1]
 
         self.object_geometry = create_object_geometry(
             *img_shape, aabb=aabb, spacing=object_spacing, is_2d=self.is_2d
@@ -397,13 +408,11 @@ class TomographyWithAstra(LinearPhysics):
 
         self.filter = RampFilter(dtype=torch.float32, device=self.device)
 
-    @property
-    def operator_norm(self) -> torch.Tensor:
-        if not hasattr(self, '_operator_norm'):
-            self._operator_norm = self.compute_norm(
-                torch.randn(self.img_shape, device=self.device)[None,None]
+        if normalize:
+            self.operator_norm = self.compute_norm(
+                torch.randn(self.img_shape, device=self.device)[None, None]
             ).sqrt()
-        return self._operator_norm
+            self.normalize = normalize
 
     @property
     def measurement_shape(self) -> tuple[int, ...]:
@@ -417,20 +426,20 @@ class TomographyWithAstra(LinearPhysics):
         return self.xray_transform.range_shape[1]
 
     def fbp_weighting(self, sinogram: torch.Tensor) -> torch.Tensor:
-        r"""Scale the computation by the inverse number of views and 
+        r"""Scales the computation by the inverse number of views and
         object-to-dector cell ratio.
-        
+
         In conebeam 3d, compute FDK weights to correct inflated distances due to
         tilted rays. Given coordinate (x,y)  of a detector cell, the corresponding
-        weight is :math:`\omega(s,t) = \frac{D_{s0}}{\sqrt{D_{sd}^2 + s^2 + t^2}}`.
-        
+        weight is :math:`\omega(x,y) = \frac{D_{s0}}{\sqrt{D_{sd}^2 + x^2 + y^2}}`.
+
         :param torch.Tensor sinogram: Sinogram of shape [B,C,...,A,N].
         :return: Weighted sinogram.
         """
         sinogram_scaled = torch.clone(sinogram)
         is_3d = len(sinogram.shape) == 5
 
-        if self.geometry_type == 'conebeam' and is_3d:
+        if self.geometry_type == "conebeam" and is_3d:
             # dimensions (V,N) are (col,row) of the 2D detector
             # A is the number of angles
             B, C, V, A, N = sinogram.shape
@@ -475,7 +484,6 @@ class TomographyWithAstra(LinearPhysics):
 
                 sinogram_scaled[:, :, :, i, :] *= weights
 
-        # 
         sinogram_scaled *= self.xray_transform.detector_cell_v_length
         sinogram_scaled /= self.xray_transform.object_cell_volume
         sinogram_scaled *= math.pi / (2 * self.num_angles)
@@ -489,27 +497,33 @@ class TomographyWithAstra(LinearPhysics):
         :return: projection of shape ``(B,C,...,A,N)``
         """
         out = AutogradTransform.apply(x, self.xray_transform)
+        if self.normalize:
+            out /= self.operator_norm
 
         return out
 
     def A_dagger(self, y: torch.Tensor, **kwargs) -> torch.Tensor:
         r"""Pseudo-inverse estimated using filtered back-projection.
-        
+
         :param torch.Tensor y: input of shape ``(B,C,...,A,N)``
         :return: torch.Tensor filtered back-projection of shape ``(B,C,...,H,W)``
         """
 
         filtered_y = self.filter(y, dim=-1)
         out = self.A_adjoint(self.fbp_weighting(filtered_y))
+        if self.normalize:
+            out *= self.operator_norm **2
 
         return out
 
     def A_adjoint(self, y: torch.Tensor, **kwargs) -> torch.Tensor:
         """Approximation of the adjoint.
-        
+
         :param torch.Tensor y: input of shape ``(B,C,...,A,N)``
         :return: torch.Tensor filtered back-projection of shape ``(B,C,...,H,W)``
         """
         out = AutogradTransform.apply(y, self.xray_transform.T)
+        if self.normalize:
+            out /= self.operator_norm
 
         return out
