a7e4b019bd0ef42c549311dcbcd89db86a40b20f
Jérémy Scanvic <jscanvicz@gmail.com>
2025-05-14 19:41:10 +0200

    black

--- deepinv/tests/test_poc.py
+++ deepinv/tests/test_poc.py
@@ -4,12 +4,9 @@ from deepinv.datasets import LidcIdriSliceDataset
 
 from unittest.mock import patch
 
+
 def test_lidc():
-    dummy_df = pd.DataFrame(columns=[
-            "Modality",
-            "Subject ID",
-            "File Location"
-        ])
+    dummy_df = pd.DataFrame(columns=["Modality", "Subject ID", "File Location"])
     with patch("os.path.isdir", lambda *args, **kwargs: True):
         with patch("os.path.exists", lambda *args, **kwargs: True):
             with patch("pandas.read_csv", lambda *args, **kwargs: dummy_df):
