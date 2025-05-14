b2cbd1dddb39a5474a3e95a3047037860429e32e
Jérémy Scanvic <jscanvicz@gmail.com>
2025-05-14 19:37:24 +0200

    poc

---
+++ deepinv/tests/test_poc.py
@@ -0,0 +1,17 @@
+import torch
+import pandas as pd
+from deepinv.datasets import LidcIdriSliceDataset
+
+from unittest.mock import patch
+
+def test_lidc():
+    dummy_df = pd.DataFrame(columns=[
+            "Modality",
+            "Subject ID",
+            "File Location"
+        ])
+    with patch("os.path.isdir", lambda *args, **kwargs: True):
+        with patch("os.path.exists", lambda *args, **kwargs: True):
+            with patch("pandas.read_csv", lambda *args, **kwargs: dummy_df):
+                dataset = LidcIdriSliceDataset(root="/dummy")
+                assert len(dataset) == 0
