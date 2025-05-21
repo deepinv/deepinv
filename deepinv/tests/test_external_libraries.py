c0d93bfa82ed755e63edbc2f473da4a2ba5699b3
romainvo <romain.vo@hotmail.fr>
2025-05-21 20:38:27 +0200

    Group astra test logic into a TestClass

--- deepinv/tests/test_external_libraries.py
+++ deepinv/tests/test_external_libraries.py
@@ -5,99 +5,107 @@ import pytest
 import unittest.mock as mock
 
 
-def dummy_compute_norm(self, x0: torch.Tensor) -> torch.Tensor:
-    return torch.tensor(1.0).to(x0)
+class TestTomographyWithAstra:
+    def dummy_compute_norm(cls, x0: torch.Tensor) -> torch.Tensor:
+        return torch.tensor(1.0).to(x0)
 
+    def dummy_projection(cls, x: torch.Tensor, out: torch.Tensor) -> None:
+        out[:] = 1.0
 
-def dummy_projection(self, x: torch.Tensor, out: torch.Tensor) -> None:
-    out[:] = 1.0
+    @pytest.mark.parametrize(
+        "is_2d,geometry_type,normalize",
+        [
+            (True, "parallel", False),
+            (True, "parallel", True),
+            (True, "fanbeam", False),
+            (True, "fanbeam", True),
+            (False, "parallel", False),
+            (False, "parallel", True),
+            (False, "conebeam", False),
+            (False, "conebeam", True),
+        ],
+    )
+    @mock.patch.object(
+        dinv.physics.functional.XrayTransform,
+        "_forward_projection",
+        new=dummy_projection,
+    )
+    @mock.patch.object(
+        dinv.physics.functional.XrayTransform, "_backprojection", new=dummy_projection
+    )
+    @mock.patch.object(
+        dinv.physics.TomographyWithAstra, "compute_norm", new=dummy_compute_norm
+    )
+    def test_tomography_with_astra_logic(self, is_2d, geometry_type, normalize):
+        r"""
+        Tests tomography operator with astra backend which does not have a numerically precise adjoint.
 
+        :param bool is_2d: Runs the test with 2D geometry, else 3D.
+        :param str geometry_type: In 2D, expects ``parallel`` or ``fanbeam``. In 3D expects ``parallel`` or ``conebeam``.
+        :param bool normalize: Initializes the operator with ``normalize=normalize``.
+        """
 
-@pytest.mark.parametrize(
-    "is_2d,geometry_type,normalize",
-    [
-        (True, "parallel", False),
-        (True, "parallel", True),
-        (True, "fanbeam", False),
-        (True, "fanbeam", True),
-        (False, "parallel", False),
-        (False, "parallel", True),
-        (False, "conebeam", False),
-        (False, "conebeam", True),
-    ],
-)
-@mock.patch.object(
-    dinv.physics.functional.XrayTransform, "_forward_projection", new=dummy_projection
-)
-@mock.patch.object(
-    dinv.physics.functional.XrayTransform, "_backprojection", new=dummy_projection
-)
-@mock.patch.object(
-    dinv.physics.TomographyWithAstra, "compute_norm", new=dummy_compute_norm
-)
-def test_tomography_with_astra_logic(is_2d, geometry_type, normalize):
-    r"""
-    Tests tomography operator with astra backend which does not have a numerically precise adjoint.
-
-    :param bool is_2d: Runs the test with 2D geometry, else 3D.
-    :param str geometry_type: In 2D, expects ``parallel`` or ``fanbeam``. In 3D expects ``parallel`` or ``conebeam``.
-    :param bool normalize: Initializes the operator with ``normalize=normalize``.
-    """
-
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-
-    ## Test 2d transforms
-    if is_2d:
-        img_shape = (16, 16)
-        num_detectors = 2 * img_shape[0]
-        num_angles = 2 * img_shape[0]
-        physics = dinv.physics.TomographyWithAstra(
-            img_shape=img_shape,
-            num_detectors=num_detectors,
-            num_angles=num_angles,
-            angular_range=(
-                (0, torch.pi) if geometry_type == "parallel" else (0, 2 * torch.pi)
-            ),
-            geometry_type=geometry_type,
-            normalize=normalize,
-            device=device,
+        pytest.importorskip(
+            "astra",
+            reason="This test requires astra-toolbox. It should be "
+            "installed with `conda install -c astra-toolbox -c nvidia astra-toolbox`",
         )
 
-        x = torch.rand(1, 1, *img_shape, device=device)
-    else:
-        ## Test 3d transforms
-        img_shape = (16, 16, 16)
-        num_detectors = (32, 32)
-        num_angles = 2 * img_shape[0]
-        physics = dinv.physics.TomographyWithAstra(
-            img_shape=img_shape,
-            num_angles=num_angles,
-            angular_range=(
-                (0, torch.pi) if geometry_type == "parallel" else (0, 2 * torch.pi)
-            ),
-            num_detectors=num_detectors,
-            geometry_type=geometry_type,
-            detector_spacing=(1.0, 1.0),
-            object_spacing=(1.0, 1.0, 1.0),
-            normalize=normalize,
-            device=device,
-        )
-        x = torch.rand(1, 1, *img_shape, device=device)
+        device = "cuda" if torch.cuda.is_available() else "cpu"
 
-    ## ------ Test forward -------
-    Ax = physics.A(x)
+        ## Test 2d transforms
+        if is_2d:
+            img_shape = (16, 16)
+            num_detectors = 2 * img_shape[0]
+            num_angles = 2 * img_shape[0]
+            physics = dinv.physics.TomographyWithAstra(
+                img_shape=img_shape,
+                num_detectors=num_detectors,
+                num_angles=num_angles,
+                angular_range=(
+                    (0, torch.pi) if geometry_type == "parallel" else (0, 2 * torch.pi)
+                ),
+                geometry_type=geometry_type,
+                normalize=normalize,
+                device=device,
+            )
 
-    ## ----- Test backward -------
-    y = torch.rand_like(Ax)
-    At_y = physics.A_adjoint(y)
+            x = torch.rand(1, 1, *img_shape, device=device)
+        else:
+            ## Test 3d transforms
+            img_shape = (16, 16, 16)
+            num_detectors = (32, 32)
+            num_angles = 2 * img_shape[0]
+            physics = dinv.physics.TomographyWithAstra(
+                img_shape=img_shape,
+                num_angles=num_angles,
+                angular_range=(
+                    (0, torch.pi) if geometry_type == "parallel" else (0, 2 * torch.pi)
+                ),
+                num_detectors=num_detectors,
+                geometry_type=geometry_type,
+                detector_spacing=(1.0, 1.0),
+                object_spacing=(1.0, 1.0, 1.0),
+                normalize=normalize,
+                device=device,
+            )
+            x = torch.rand(1, 1, *img_shape, device=device)
 
-    ## --- Test pseudo-inverse ---
-    x_hat = physics.A_dagger(y)
+        ## -------- Test forward --------
+        Ax = physics.A(x)
+        assert Ax.shape == (1, 1, *physics.measurement_shape)
 
-    if is_2d:
-        assert Ax.shape == (1, 1, num_angles, num_detectors)
-    else:
-        assert Ax.shape == (1, 1, num_detectors[0], num_angles, num_detectors[1])
+        ## ------- Test backward --------
+        y = torch.rand_like(Ax)
+        At_y = physics.A_adjoint(y)
+        assert At_y.shape == (1, 1, *img_shape)
 
-    assert At_y.shape == (1, 1, *img_shape)
-    assert x_hat.shape == (1, 1, *img_shape)
+        ## ---- Test pseudo-inverse -----
+        x_hat = physics.A_dagger(y)
+        assert x_hat.shape == (1, 1, *img_shape)
+
+        ## --- Test autograd.Function ---
+        pred = torch.zeros_like(x, requires_grad=True)
+        loss = torch.linalg.norm(physics.A(pred) - Ax)
+        loss.backward()
+        assert pred.grad is not None
