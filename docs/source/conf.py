3f48f6e0eff26204f90ae92747c25f729e2135a1
Julian Tachella <tachellajulian@gmail.com>
2025-06-08 20:36:58 +0200

    address

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -64,7 +64,8 @@ autodoc_preserve_defaults = True
 nitpicky = True
 # Create link to the API in the auto examples
 autodoc_inherit_docstrings = False
-
+# for sitemap
+html_baseurl = 'https://deepinv.github.io/deepinv/'
 
 ####  userguide directive ###
 from docutils import nodes
