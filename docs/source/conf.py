3a9291ea78e2c3ab15a7d3df1f667e4c92bf2cea
Jérémy Scanvic <jscanvicz@gmail.com>
2025-06-17 17:44:04 +0200

    remove advanced from conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -194,7 +194,6 @@ sphinx_gallery_conf = {
             "../../examples/patch-priors",
             "../../examples/self-supervised-learning",
             "../../examples/adversarial-learning",
-            "../../examples/advanced",
             "../../examples/external-libraries",
         ]
     ),
