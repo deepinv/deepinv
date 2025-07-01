6b07abe9636135fd063ee5479f357c770143442a
Tom. <davies.evan.tom@gmail.com>
2025-07-01 13:09:10 +0100

    Refactor Sampling (#397)
    * rstring + uv files
    
    * base of new sampling classes added
    
    * create utils.py
    
    * ULA + Skrock
    
    * formatting
    
    * untracked oldsampling + formatting
    
    * Added the old sampling modules back in + merged __init__.py
    
    * Update deepinv/optim/optim_iterators/pgd.py
    
    fix small typo
    
    Co-authored-by: Andrewwango <andrew.wang27@gmail.com>
    
    * Added doc strings to sampling + moved BaseSample to nn.Module
    
    * raise an exception if an iterator doesn't implement forward
    
    * typo in SKRock
    
    * added clipping
    
    * added sampling_build wrapper
    
    * Uncertainty quantification example + Custom Kernel example
    
    * default params
    
    * moved g_iterators to forward of BaseSample + removed auto step size for now
    
    * Move ULA + SKRock to BaseSample
    
    * Move DiffusionSampler to inherit from BaseSample
    
    * Remove old examples from langevin.py
    
    * Samll typo in SKRock
    
    * added callbacks
    
    * no defaults for ULA/SKROCK and clipping is now an iterator property
    
    * tests for sample_builder
    
    * cleaned up comments
    
    * small typos
    
    * docs
    
    * black formatting
    
    * fix ula import name
    
    * add usage of typing Union (proposed alternative not compatible with pytohn3.9 which is supported in pyproject.toml)
    
    * use torch for sqrt instead of numpy
    
    * remove unused import
    
    * going back to numpy. It seems it's better to compute in numpy cpu rather than transfer to torch gpu
    
    * type hints + g_statistics plural in ULA and SKRock + removed compute_step_size
    
    * small fixes
    
    * doc strings
    
    * python 3.9 type hints + black formatting
    
    * algorithm parameters now a property of the iterator
    
    * examples
    
    * moved sampling params to the iterator init
    
    * iteration number passed to iterator
    
    * black formatting
    
    * user guide
    
    * Update deepinv/sampling/samplers.py
    
    Co-authored-by: Andrewwango <andrew.wang27@gmail.com>
    
    * add support for latent variables
    
    * organise imports
    
    * rename to sampling
    
    * rename samplers.py to sampling.py
    
    * rename samplers.py to sampling.py
    
    * rename sample_iterator.py to sampling_iterator.py, and docs for DiffusionIterator
    
    * fix type hints in sampling iterators
    
    * introduce X,x,y
    
    * x vs X typing and docs
    
    * rename PULA to PreconULA
    
    * user guide for sampling iteration
    
    * Better callback docs
    
    * black formatting
    
    * Apply suggested docstring changes
    
    Co-authored-by: Andrewwango <andrew.wang27@gmail.com>
    
    * Move g_statistics to forward in ULA/SKRock
    
    * Initialize latent variables
    
    * refer to example in userguide rather than custom mcmc algorithm for legacy classes
    
    * merge main
    
    * improve docs
    
    * black
    
    * ruff UP006
    
    * better doc references + langevin deprecation warnings
    
    * fix deprecation warning
    
    * fix sampling doctest
    
    * fix doctests
    
    ---------
    
    Co-authored-by: Andrewwango <andrew.wang27@gmail.com>
    Co-authored-by: Tobias Liaudat <tobiasliaudat@gmail.com>

--- deepinv/sampling/__init__.py
+++ deepinv/sampling/__init__.py
@@ -1,5 +1,13 @@
-from .langevin import ULA, MonteCarlo, SKRock
+from .sampling import BaseSampling, sampling_builder
+from .langevin import ULA, SKRock
 from .diffusion import DDRM, DiffusionSampler, DiffPIR, DPS
+from .sampling_iterators import (
+    ULAIterator,
+    SKRockIterator,
+    SamplingIterator,
+    SKROCKIterator,
+    DiffusionIterator,
+)
 from . import diffusion_sde, sde_solver
 from .noisy_datafidelity import NoisyDataFidelity, DPSDataFidelity
 from .diffusion_sde import (
