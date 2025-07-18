486e632b44a66d6f848408a1b4d95fe65e77475b
Jérémy Scanvic <jscanvicz@gmail.com>
2025-07-18 19:06:57 +0200

    add missing import

--- examples/self-supervised-learning/demo_r2r_denoising.py
+++ examples/self-supervised-learning/demo_r2r_denoising.py
@@ -16,6 +16,7 @@ from torchvision import transforms, datasets
 
 import deepinv as dinv
 from deepinv.utils.demo import get_data_home
+from deepinv.models.utils import get_weights_url
 
 # %%
 # Setup paths for data loading and results.
