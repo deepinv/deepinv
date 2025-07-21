82fbe4b4dba6a8f9d611c050e5a99797b1875af3
Andrewwango <andrew.wang27@gmail.com>
2025-07-21 09:55:09 +0100

    here too

--- deepinv/physics/__init__.py
+++ deepinv/physics/__init__.py
@@ -24,6 +24,7 @@ from .noise import (
     LogPoissonNoise,
     GammaNoise,
     SaltPepperNoise,
+    ZeroNoise
 )
 from .mri import MRI, DynamicMRI, SequentialMRI, MultiCoilMRI, MRIMixin
 from .tomography import Tomography, TomographyWithAstra
