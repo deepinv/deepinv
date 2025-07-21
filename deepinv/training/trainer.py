79dafc89bdf02ecc508a098632c649ac1935fc68
Thibaut Modrzyk <52449813+Tmodrzyk@users.noreply.github.com>
2025-07-21 09:45:32 +0200

    fix docstring

--- deepinv/training/trainer.py
+++ deepinv/training/trainer.py
@@ -79,7 +79,7 @@ class Trainer:
     :Basics:
 
     :param deepinv.models.Reconstructor, deepinv.models.Denoiser, torch.nn.Module model: Reconstruction network, which can be :ref:`any reconstruction network <reconstructors>`.
-        or any other custom reconstruction network. If the model is a ``Denoiser``, it will be wrapped in an :class:`ArtifactRemoval` model.
+        or any other custom reconstruction network. If the model is a ``Denoiser``, it will be wrapped in an :class:`deepinv.models.ArtifactRemoval` model.
     :param deepinv.physics.Physics, list[deepinv.physics.Physics] physics: :ref:`Forward operator(s) <physics_list>`.
     :param torch.utils.data.DataLoader, list[torch.utils.data.DataLoader] train_dataloader: Train data loader(s), see options 1 to 3
         above for how we expect data to be provided.
