4a835456a561ada013bcf4a51778874a019f884e
jscanvic <jscanvic@users.noreply.github.com>
2025-07-26 12:44:53 +0000

    deploy: 09be3a92aac0d08ef908c992a4515e5fea034e05

---
+++ _downloads/cc9a5ecb95e119b6c76f21fc1c992746/demo_ptychography.py
@@ -0,0 +1,124 @@
+r"""
+Ptychography phase retrieval
+============================
+
+This example shows how to create a Ptychography phase retrieval operator and generate phaseless measurements from a given image.
+"""
+
+# %%
+# General setup
+# -------------
+# Imports the necessary libraries and modules, including ptychography phase retrieval function from `deepinv`.
+# It sets the device to GPU if available, otherwise uses the CPU.
+import matplotlib.pyplot as plt
+import torch
+import numpy as np
+import deepinv as dinv
+from deepinv.utils.demo import load_example
+from deepinv.utils.plotting import plot
+from deepinv.physics import Ptychography
+from deepinv.optim.data_fidelity import L1
+from deepinv.optim.phase_retrieval import correct_global_phase
+
+device = dinv.utils.get_freer_gpu() if torch.cuda.is_available() else "cpu"
+
+
+# %%
+# Load image from the internet
+# ----------------------------
+# Loads a sample image from a URL, resizes it to 128x128 pixels, and extracts only one color channel.
+
+size = 128
+image = load_example("CBSD_0010.png", grayscale=False, img_size=(size, size))
+
+x = image[:, 0, ...].unsqueeze(1)  # Take only one channel
+print(x.shape)
+plot([x], figsize=(10, 10))
+
+
+# %%
+# Prepare phase input
+# -------------------
+# We use the original image as the phase information for the complex signal. The original value range is [0, 1], and we map it to the phase range [0, pi].
+
+phase = x / x.max() * np.pi  # between 0 and pi
+input = torch.exp(1j * phase.to(torch.complex64)).to(device)
+
+
+# %%
+# Set up ptychography physics model
+# ---------------------------------
+# Initializes the ptychography physics model with parameters like the probe and shifts.
+# This model will be used to simulate ptychography measurements.
+
+img_size = (1, size, size)
+n_img = 10**2
+probe = dinv.physics.phase_retrieval.build_probe(
+    img_size, type="disk", probe_radius=30, device=device
+)
+shifts = dinv.physics.phase_retrieval.generate_shifts(img_size, n_img=n_img, fov=170)
+
+physics = Ptychography(
+    img_size=img_size,
+    probe=probe,
+    shifts=shifts,
+    device=device,
+)
+
+# %%
+# Display probe overlap
+# ---------------------
+# Calculates and displays the overlap of probe regions in the image, helping visualize the ptychography pattern.
+
+overlap_img = physics.B.get_overlap_img(physics.B.shifts).cpu()
+overlap2probe = physics.B.get_overlap_img(physics.B.shifts[55:57]).cpu()
+plot(
+    [overlap2probe.unsqueeze(0), overlap_img.unsqueeze(0)],
+    titles=["Overlap 2 probe", "Overlap images"],
+)
+
+
+# %%
+# Generate and visualize probe and measurements
+# ---------------------------------------------
+# Displays the ptychography probe and a sum of the generated measurement data.
+
+probe = physics.probe[:, 55].cpu()
+y = physics(input)
+plot(
+    [torch.abs(probe), y[0].sum(dim=0).log().unsqueeze(0)],
+    titles=["Probe", "y"],
+)
+
+
+# %%
+# Gradient descent for phase retrieval
+# ------------------------------------
+# Implements a simple gradient descent algorithm to minimize the L1 data fidelity loss for phase retrieval.
+
+data_fidelity = L1()
+lr = 0.1
+n_iter = 200
+x_est = torch.randn_like(x).to(device)
+loss_hist = []
+
+for i in range(n_iter):
+    x_est = x_est - lr * data_fidelity.grad(x_est, y, physics)
+    loss_hist.append(data_fidelity(x_est, y, physics).cpu())
+    if i % 10 == 0:
+        print(f"Iter {i}, loss: {loss_hist[i]}")
+
+# Plot the loss curve
+plt.plot(loss_hist)
+plt.title("loss curve (gradient descent with random initialization)")
+plt.show()
+
+# %%
+# Display final estimated phase retrieval
+# ---------------------------------------
+# Corrects the global phase of the estimated image to match the original phase and plots the result.
+# This final visualization shows the original image and the estimated phase after retrieval.
+
+x_est = x_est.detach().cpu()
+final_est = correct_global_phase(x_est, x)
+plot([x, torch.angle(final_est)], titles=["x", "Final estimate"])
---
+++ _downloads/e323befa10e13808c0c4ad9c8bde473c/demo_PnP_mirror_descent.py
@@ -0,0 +1,136 @@
+r"""
+Plug-and-Play algorithm with Mirror Descent for Poisson noise inverse problems.
+====================================================================================================
+
+This is a simple example to show how to use a mirror descent algorithm for solving an inverse problem with Poisson noise.
+See :footcite:t:`hurault2023convergent` for more details.
+
+The Mirror descent with RED denoiser writes
+
+.. math::
+
+    x_{k+1} = \nabla \phi ( \nabla \phi^*(x_k) - \tau \nabla \distance{A(x_k)}{y} - \tau ( x_k - D_\sigma(x)))
+
+where :math:`\phi` is a convex Bergman potential, :math:`\distance{A(x)}{y}` is the data fidelity term and :math:`D_\sigma(x)` is a denoiser.
+
+In this example, we use the DnCNN denoiser :footcite:t:`zhang2017beyond`. As the observation has been corrupted with Poisson noise, we use the :class:`deepinv.optim.PoissonLikelihood` data-fidelity term.
+In :footcite:t:`Bolte2016descent`, it is shown that, with this data-fidelity term, the right Bregman potential to use is Burg's entropy :class:`deepinv.optim.bregman.BurgEntropy`.
+"""
+
+import deepinv as dinv
+from pathlib import Path
+import torch
+from deepinv.optim.data_fidelity import PoissonLikelihood
+from deepinv.optim.prior import RED
+from deepinv.optim import optim_builder
+from deepinv.optim.bregman import BurgEntropy
+from deepinv.utils.demo import load_example
+from deepinv.utils.plotting import plot, plot_curves
+
+# %%
+# Setup paths for data loading and results.
+# ----------------------------------------------------------------------------------------
+#
+
+BASE_DIR = Path(".")
+ORIGINAL_DATA_DIR = BASE_DIR / "datasets"
+DATA_DIR = BASE_DIR / "measurements"
+RESULTS_DIR = BASE_DIR / "results"
+CKPT_DIR = BASE_DIR / "ckpts"
+
+# Set the global random seed from pytorch to ensure reproducibility of the example.
+torch.manual_seed(0)
+
+img_size = 64
+device = dinv.utils.get_freer_gpu() if torch.cuda.is_available() else "cpu"
+x_true = load_example("butterfly.png", img_size=img_size).to(device)
+x = x_true.clone()
+
+
+n_channels = 3  # 3 for color images, 1 for gray-scale images
+operation = "deblurring"
+
+# Degradation parameters
+noise_level_img = 1 / 40  # Poisson Noise gain
+
+# Generate the gaussian blur operator with Poisson noise.
+physics = dinv.physics.BlurFFT(
+    img_size=(n_channels, img_size, img_size),
+    filter=dinv.physics.blur.gaussian_blur(),
+    device=device,
+    noise_model=dinv.physics.PoissonNoise(gain=noise_level_img),
+)
+
+# %%
+# Define the PnP algorithm.
+# ----------------------------------------------------------------------------------------
+# The chosen algorithm is here MD (Mirror Descent).
+
+
+# Select the data fidelity term, here Poisson likelihood due to the use of Poisson noise in the forward operator.
+data_fidelity = PoissonLikelihood(gain=noise_level_img)
+
+# Set up the denoising prior. Note that we use a Gaussian noise denoiser, even if the observation noise is Poisson.
+prior = RED(denoiser=dinv.models.DnCNN(depth=20, pretrained="download").to(device))
+
+# Set up the optimization parameters
+max_iter = 200  # number of iterations
+stepsize = 1.0  # stepsize of the algorithm
+sigma_denoiser = 0.05  # noise level parameter of the Gaussian denoiser
+params_algo = {  # wrap all the restoration parameters in a 'params_algo' dictionary. In particular, this is here that we define the bregman potential used in the mirror descent algorithm.
+    "stepsize": stepsize,
+    "g_param": sigma_denoiser,
+}
+
+# Logging parameters
+verbose = True
+
+# Define the unfolded trainable model.
+model = optim_builder(
+    iteration="MD",
+    prior=prior,
+    data_fidelity=data_fidelity,
+    early_stop=True,
+    max_iter=max_iter,
+    verbose=verbose,
+    params_algo=params_algo,
+    bregman_potential=BurgEntropy(),
+)
+
+
+# %%
+# Evaluate the model on the problem and plot the results.
+# --------------------------------------------------------------------
+#
+# The model returns the output and the metrics computed along the iterations.
+# For computing PSNR, the ground truth image ``x_gt`` must be provided.
+
+y = physics(x)
+x_lin = physics.A_adjoint(y)
+
+# run the model on the problem.
+with torch.no_grad():
+    x_model, metrics = model(
+        y, physics, x_gt=x, compute_metrics=True
+    )  # reconstruction with PnP algorithm
+
+# compute PSNR
+print(f"Linear reconstruction PSNR: {dinv.metric.PSNR()(x, x_lin).item():.2f} dB")
+print(f"PnP reconstruction PSNR: {dinv.metric.PSNR()(x, x_model).item():.2f} dB")
+
+# plot images. Images are saved in RESULTS_DIR.
+imgs = [y, x, x_lin, x_model]
+plot(
+    imgs,
+    titles=["Input", "GT", "Linear", "Recons."],
+    save_dir=RESULTS_DIR / "images",
+    show=True,
+)
+
+# plot convergence curves. Metrics are saved in RESULTS_DIR.
+plot_curves(metrics, save_dir=RESULTS_DIR / "curves", show=True)
+
+# %%
+# :References:
+#
+# .. footbibliography::
