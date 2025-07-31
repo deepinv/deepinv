imresive uses backprop adjoint + fix tests

deepinv/physics/blur.py
@@ -5,7 +5,7 @@ import torchvision
 import torch
 import torch.fft as fft
 from torch import Tensor
-from deepinv.physics.forward import LinearPhysics, DecomposablePhysics
+from deepinv.physics.forward import LinearPhysics, DecomposablePhysics, adjoint_function
 from deepinv.physics.functional import (
     conv2d,
     conv_transpose2d,
@@ -14,6 +14,7 @@ from deepinv.physics.functional import (
     product_convolution2d_adjoint,
     conv3d_fft,
     conv_transpose3d_fft,
+    imresize,
 )
 
 
@@ -779,10 +780,7 @@ def bicubic_filter(factor=2):
 class DownsamplingMatlab(Downsampling):
     """Downsampling with MATLAB imresize
 
-    Downsamples with default MATLAB imresize, using a bicubic kernel, antialiasing and reflect padding.
-
-    This requires the `bicubic_pytorch` package which you can install from our `maintained fork <https://github.com/Andrewwango/bicubic_pytorch>`_
-    using ``pip install bicubic-pytorch``.
+    Downsamples with default MATLAB `imresize`, using a bicubic kernel, antialiasing and reflect padding.
 
     Wraps `imresize` from a modified version of the `original implementation <https://github.com/sanghyun-son/bicubic_pytorch>`_.
 
@@ -791,6 +789,7 @@ class DownsamplingMatlab(Downsampling):
     :param str padding: MATLAB padding type, supports only `reflect` for reflect padding.
     :param bool antialiasing: whether to perform antialiasing in MATLAB downsampling.
         Recommended to set to `True` to match MATLAB.
+    :param bool adjoint_via_backprop: if ``True``, the adjoint will be computed via :func:`deepinv.physics.adjoint_function` which is slower. Otherwise `imresize` with reciprocal of scale is used, but this has an adjoint mismatch.
     """
 
     def __init__(
@@ -799,21 +798,15 @@ class DownsamplingMatlab(Downsampling):
         kernel: str = "cubic",
         padding: str = "reflect",
         antialiasing: bool = True,
+        adjoint_via_backprop: bool = True,
         **kwargs,
     ):
         super().__init__(filter=None, factor=factor, **kwargs)
 
-        try:
-            from bicubic_pytorch import imresize
-        except ImportError:
-            raise ImportError(
-                "Install 'bicubic_pytorch' using 'pip install bicubic-pytorch'"
-            )
-
-        self.imresize = imresize
         self.kernel = kernel
         self.padding = padding
         self.antialiasing = antialiasing
+        self.adjoint_via_backprop = adjoint_via_backprop
 
     def A(self, x, factor: Union[int, float] = None, **kwargs):
         """Downsample forward operator
@@ -823,7 +816,7 @@ class DownsamplingMatlab(Downsampling):
         """
         self.update_parameters(factor=factor, **kwargs)
         # Clone because of in-place ops
-        return self.imresize(
+        return imresize(
             x.clone(),
             scale=1 / self.factor,
             antialiasing=self.antialiasing,
@@ -838,10 +831,20 @@ class DownsamplingMatlab(Downsampling):
         :param int, float factor: downsampling factor. If not `None`, use this factor and store it as current factor.
         """
         self.update_parameters(factor=factor, **kwargs)
-        return self.imresize(
-            y.clone(),
-            scale=self.factor,
-            antialiasing=self.antialiasing,
-            kernel=self.kernel,
-            padding_type=self.padding,
-        )
+
+        if self.adjoint_via_backprop:
+            adj = adjoint_function(
+                self.A,
+                (*y.shape[:2], y.shape[-2] * self.factor, y.shape[-1] * self.factor),
+                device=y.device,
+                dtype=y.dtype,
+            )
+            return adj(y)
+        else:
+            return imresize(
+                y.clone(),
+                scale=self.factor,
+                antialiasing=self.antialiasing,
+                kernel=self.kernel,
+                padding_type=self.padding,
+            )

deepinv/tests/test_physics.py
@@ -349,15 +349,10 @@ def find_operator(name, device, get_physics_param=False):
         )
         params = []
     elif name == "super_resolution_matlab":
-        pytest.importorskip(
-            "bicubic_pytorch",
-            reason="This test requires bicubic_pytorch. Install with `pip install bicubic-pytorch`.",
-        )
-
         img_size = (1, 32, 32)
         factor = 2
-        norm = 1.0
-        p = dinv.physics.DownsamplingMatlab(factor=factor)
+        norm = 1.0 / factor**2
+        p = dinv.physics.DownsamplingMatlab(factor=factor, adjoint_via_backprop=True)
         params = []
     elif name.startswith("super_resolution"):
         img_size = (1, 32, 32)
@@ -558,9 +553,6 @@ def test_operators_adjointness(name, device, rng):
     if name == "radio":
         dtype = torch.cfloat
 
-    if name == "super_resolution_matlab":
-        pytest.skip("DownsamplingMatlab has bad adjoint.")
-
     x = torch.randn(imsize, device=device, dtype=dtype, generator=rng).unsqueeze(0)
     error = physics.adjointness_test(x).abs()
     assert error < 1e-3
@@ -646,9 +638,6 @@ def test_pseudo_inverse(name, device, rng):
     :return: asserts error is less than 1e-3
     """
     physics, imsize, _, dtype = find_operator(name, device)
-
-    if name == "super_resolution_matlab":
-        pytest.skip("DownsamplingMatlab has bad adjoint.")
 
     x = torch.randn(imsize, device=device, dtype=dtype, generator=rng).unsqueeze(0)
 
@@ -831,9 +820,6 @@ def test_concatenation(name, device):
     if "pansharpen" in name:  # TODO: fix pansharpening
         return
     physics, imsize, _, dtype = find_operator(name, device)
-
-    if name == "super_resolution_matlab":
-        pytest.skip("DownsamplingMatlab has bad adjoint.")
 
     x = torch.randn(imsize, device=device, dtype=dtype).unsqueeze(0)
     y = physics(x)

deepinv/tests/test_physics_functional.py
@@ -5,9 +5,10 @@ Created on Thu Jul 11 14:48:05 2024
 
 @author: fsarron
 """
-
-import deepinv as dinv
+import pytest
+from contextlib import nullcontext
 import torch
+import deepinv as dinv
 
 
 def test_conv2d_adjointness(device):
@@ -134,3 +135,26 @@ def test_conv3d_adjointness(device):
                     Atyx = torch.sum(Aty * x)
 
                     assert torch.abs(Axy - Atyx) < 1e-3
+
+
+@pytest.mark.parametrize("kernel", ["cubic", "gaussian"])
+@pytest.mark.parametrize("scale", [2, 0.5])
+@pytest.mark.parametrize("antialiasing", [True, False])
+def test_imresize(kernel, scale, antialiasing):
+    sigma = 2
+    img_size = (1, 64, 64)
+    x = torch.randn(1, *img_size)
+    y = dinv.physics.functional.imresize(
+        x,
+        scale=scale,
+        kernel=kernel,
+        sigma=sigma,
+        padding_type="reflect",
+        antialiasing=antialiasing,
+    )
+    assert y.shape == (
+        1,
+        img_size[0],
+        int(img_size[1] * scale),
+        int(img_size[2] * scale),
+    )