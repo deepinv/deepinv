113d5f8054ba86add682f4f5a7a238b922b4e656
Andrewwango <andrew.wang27@gmail.com>
2025-09-02 14:21:13 +0100

    docstring + changelog

--- deepinv/utils/compat.py
+++ deepinv/utils/compat.py
@@ -1,7 +1,15 @@
 from itertools import zip_longest
 
-
 def zip_strict(*iterables):
+    """
+    Strict version of :func:`zip` for Python < 3.10.
+
+    Equivalent to ``zip(*iterables, strict=True)`` in Python 3.10+,
+    raising a :class:`ValueError` if the iterables have different lengths.
+
+    :param iterables: One or more iterables to zip together.
+    :type iterables: iterable
+    """
     sentinel = object()
     for combo in zip_longest(*iterables, fillvalue=sentinel):
         if sentinel in combo:
