b7e8137522bdd7766e44393fa69caacfbe7707c0
Andrewwango <andrew.wang27@gmail.com>
2025-09-02 14:40:31 +0100

    ugh

--- deepinv/__init__.py
+++ deepinv/__init__.py
@@ -70,9 +70,5 @@ import warnings
 # Check Python version
 if sys.version_info < (3, 10):  # pragma: no cover
     warnings.warn(
-        "You are using a Python version lower than 3.10. "
-        "deepinv officially supports Python >= 3.10. "
-        "Running on < 3.10 may work, but it is unsupported, unstable, "
-        "and may lead to unexpected bugs.",
-        "For more information on updating to Python 3.10, see https://www.python.org/downloads",
+        "You are using a Python version lower than 3.10. deepinv officially supports Python >= 3.10. Running on < 3.10 may work, but it is unsupported, unstable and may lead to unexpected bugs. For more information on updating to Python 3.10, see https://www.python.org/downloads"
     )
