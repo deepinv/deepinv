672c1b02b8e555ef79a726d743a7f302b68388b6
“PriscillaZixin” <zxwangyz@outlook.com>
2025-09-06 17:04:45 +0100

    fix np2tensor of dpri.py

--- deepinv/optim/dpir.py
+++ deepinv/optim/dpir.py
@@ -6,7 +6,7 @@ from deepinv.optim.data_fidelity import L2
 from deepinv.optim.prior import PnP
 from deepinv.optim.optimizers import create_iterator
 from deepinv.optim import BaseOptim
-
+import torch
 
 
 def get_DPIR_params(noise_level_img):
