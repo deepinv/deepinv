3a8eb7b3738a30dfae04be1fa095985512cdb136
Brayan Monroy <brayansneider22@gmail.com>
2025-09-09 03:13:52 -0500

    update

--- deepinv/physics/spatial_unwrapping.py
+++ deepinv/physics/spatial_unwrapping.py
@@ -38,7 +38,7 @@ class SpatialUnwrapping(Physics):
         >>> x = torch.tensor([[0.5, 1.2, 2.7]])
         >>> physics = SpatialUnwrapping(threshold=1.0, mode="round")
         >>> y = physics(x)
-        >>> print(torch.round(y, 1))
+        >>> print(torch.round(y, decimals=1))
         tensor([[ 0.5,  0.2, -0.3]])
 
     """
