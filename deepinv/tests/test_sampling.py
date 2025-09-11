88d6c9279e2d15b01efe817972608acc525b8106
Julián Tachella <38355592+tachella@users.noreply.github.com>
2025-09-11 10:24:36 +0200

    fix conda forge test + log poisson noise docs (#753)
    * fix test + logpoisson noise
    
    * fix N0 + mu

--- deepinv/tests/test_sampling.py
+++ deepinv/tests/test_sampling.py
@@ -187,7 +187,7 @@ def test_algo_inpaint(name_algo, device):
     mean_target_inmask = 1 / 3.0
 
     assert (mean_target_inmask - mean_crop).abs() < 0.2
-    assert (mean_target_masked - mean_outside_crop).abs() < 0.01
+    assert (mean_target_masked - mean_outside_crop).abs() < 0.02
 
 
 # tests for sample_builder
