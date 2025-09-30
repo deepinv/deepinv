6923488a96fc1612036aa77d25e7ad9079bbee47
Andrewwango <andrew.wang27@gmail.com>
2025-09-30 13:00:17 +0100

    jeremy comment

--- deepinv/utils/plotting.py
+++ deepinv/utils/plotting.py
@@ -326,7 +326,7 @@ def plot(
     :param None, matplotlib.axes.Axes axs: matplotlib Axes object to plot on. If None, create new Axes. Defaults to None.
     :param bool return_fig: return the figure object.
     :param bool return_axs: return the axs object.
-    :param imshow_kwargs: keyword args to pass to the matplotlib `imshow` call. See
+    :param imshow_kwargs: keyword args to pass to the matplotlib `imshow` calls. See
         `imshow docs <https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.imshow.html>`_ for possible kwargs.
     """
     import matplotlib.pyplot as plt
