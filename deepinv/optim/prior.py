eb4eaa5d9b9a1e821b3fad520444d1b4281b57fc
Johannes Hertrich <j.hertrich@ucl.ac.uk>
2025-10-06 12:08:29 +0100

    fix devices

--- deepinv/optim/prior.py
+++ deepinv/optim/prior.py
@@ -779,7 +779,8 @@ class WCRR(Prior):
         )
 
         self.dirac = torch.zeros(
-            1, in_channels, 2 * self.filter_size - 1, 2 * self.filter_size - 1
+            (1, in_channels, 2 * self.filter_size - 1, 2 * self.filter_size - 1),
+            device=device,
         )
         self.dirac[0, 0, self.filter_size - 1, self.filter_size - 1] = 1.0
 
