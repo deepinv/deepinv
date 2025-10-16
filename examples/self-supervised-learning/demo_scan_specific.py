e24b150e3e717fbd56059d4cd000429a05960dfb
Andrewwango <andrew.wang27@gmail.com>
2025-10-16 23:24:59 +0100

    upload example

--- examples/self-supervised-learning/demo_scan_specific.py
+++ examples/self-supervised-learning/demo_scan_specific.py
@@ -3,174 +3,182 @@ Scan-specific zero-shot measurement splitting for MRI
 =====================================================
 
 We demonstrate scan-specific self-supervised learning, that is, learning to 
-reconstruct MRI scans from a single sample without ground truth.
+reconstruct MRI scans from a single accelerated sample without ground truth.
 
-Here, we demonstrate training with the :class:`SSDU loss <deepinv.loss.SplittingLoss>` :footcite:p:`yaman2020self` and :class:`weighted SSDU <deepinv.loss.WeightedSplittingLoss>` :footcite:p:`millard2023theoretical`.
+Here, we demonstrate training with the :class:`weighted SSDU <deepinv.loss.WeightedSplittingLoss>` :footcite:p:`millard2023theoretical,yaman2020self`.
 However, note that any of the :ref:`self-supervised losses <self-supervised-losses>` can be used to do this with varying performance,
-for example see the :ref:`example using Equivariant Imaging sphx_glr_auto_examples_self-supervised-learning_demo_scan_specific.py>` :footcite:p:`chen2021equivariant`.
+for example see the :ref:`example using Equivariant Imaging <sphx_glr_auto_examples_self-supervised-learning_demo_ei.py>` :footcite:p:`chen2021equivariant`.
 
 """
-
-from pathlib import Path
-
+from tqdm import tqdm
 import torch
 from torch.utils.data import DataLoader
-from torchvision import transforms, datasets
-
+from torch.utils.data._utils.collate import default_collate
 import deepinv as dinv
-from deepinv.utils.demo import get_data_home
-from deepinv.models.utils import get_weights_url
 
-torch.manual_seed(0)
 device = dinv.utils.get_freer_gpu() if torch.cuda.is_available() else "cpu"
-
-BASE_DIR = Path(".")
-DATA_DIR = BASE_DIR / "measurements"
-ORIGINAL_DATA_HOME = get_data_home()
-
-# TODO download fastmri knee singlecoil test raw kspace sample using FastMRISliceDataset
-
+rng = torch.Generator(device=device).manual_seed(0)
+rng_cpu = torch.Generator(device="cpu").manual_seed(0)
 
 # %%
-# Define loss
-# ~~~~~~~~~~~
+# Data
+# ----
+# First, download a single volume from the FastMRI brain dataset, via HuggingFace.
 #
-# Our implementation has multiple optional parameters that control how the
-# splitting is to be achieved. For example, you can:
-#
-# -  Use ``split_ratio`` to set the ratio of pixels used in the forward
-#    pass vs the loss;
-# -  Define custom masking methods using a ``mask_generator`` such as
-#    :class:`deepinv.physics.generator.BernoulliSplittingMaskGenerator`
-#    or :class:`deepinv.physics.generator.GaussianSplittingMaskGenerator`;
-# -  Use ``eval_n_samples`` to set how many realisations of the random
-#    mask is used at evaluation time;
-# -  Optionally disable measurement splitting at evaluation time using
-#    ``eval_split_input`` (as is the case in SSDU :footcite:t:`yaman2020self`).
-# -  Average over both input and output masks at evaluation time using
-#    ``eval_split_output``. See :class:`deepinv.loss.SplittingLoss` for
-#    details.
-#
-# Note that after the model has been defined, the loss must also "adapt"
-# the model.
-#
+# .. important::
+#    By using this dataset, you confirm that you have agreed to and signed the `FastMRI data use agreement <https://fastmri.med.nyu.edu/>`_.
 
-loss = dinv.loss.SplittingLoss(split_ratio=0.6, eval_split_input=True, eval_n_samples=5)
+DATA_DIR = dinv.utils.get_data_home() / "fastMRI" / "multicoil_train"
+SLICE_DIR = DATA_DIR / "slices"
+OUT_DIR = DATA_DIR / "out"
+DATA_DIR.mkdir(parents=True, exist_ok=True) ; SLICE_DIR.mkdir(exist_ok=True) ; OUT_DIR.mkdir(exist_ok=True)
 
+dinv.utils.download_example("demo_fastmri_brain_multicoil.h5", DATA_DIR)
 
 # %%
-# Prepare data
-# ~~~~~~~~~~~~
-#
-# We use the ``torchvision`` MNIST dataset, and use noisy tomography
-# physics (with number of angles equal to the image size) for the forward
-# operator.
+# We use the FastMRI slice dataset provided in DeepInverse to load the volume and return all 16 slices.
+# The data is returned in the format `x, y, params` where `params` is a dictionary containing
+# the acceleration mask (simulated Gaussian mask with acceleration 6) and the
+# estimated coil sensitivity map.
 #
 # .. note::
-#
-#      We use a subset of the whole training set to reduce the computational load of the example.
-#      We recommend to use the whole set by setting ``train_datapoints=test_datapoints=None`` to get the best results.
-#
+#     This loading takes a few seconds per slice, as it must estimate the coil sensitivity map
+#     on the fly.
 
-transform = transforms.Compose([transforms.ToTensor()])
-
-train_dataset = datasets.MNIST(
-    root=ORIGINAL_DATA_HOME, train=True, transform=transform, download=True
+dataset = dinv.datasets.FastMRISliceDataset(
+    DATA_DIR,
+    slice_index="all",
+    transform=dinv.datasets.MRISliceTransform(
+        mask_generator=dinv.physics.generator.GaussianMaskGenerator(
+            img_size=(256, 256),
+            acceleration=6,
+            center_fraction=0.08,
+            device="cpu",
+            rng=rng_cpu,
+        ),
+        estimate_coil_maps=True,
+    ),
 )
-test_dataset = datasets.MNIST(
-    root=ORIGINAL_DATA_HOME, train=False, transform=transform, download=True
-)
-
-physics = dinv.physics.Tomography(
-    angles=28,
-    img_width=28,
-    noise_model=dinv.physics.noise.GaussianNoise(0.1),
-    device=device,
-)
-
-deepinv_datasets_path = dinv.datasets.generate_dataset(
-    train_dataset=train_dataset,
-    test_dataset=test_dataset,
-    physics=physics,
-    device=device,
-    save_dir=DATA_DIR,
-    train_datapoints=100,
-    test_datapoints=10,
-)
-
-train_dataset = dinv.datasets.HDF5Dataset(path=deepinv_datasets_path, train=True)
-test_dataset = dinv.datasets.HDF5Dataset(path=deepinv_datasets_path, train=False)
-
-train_dataloader = DataLoader(train_dataset, shuffle=True)
-test_dataloader = DataLoader(test_dataset, shuffle=False)
-
 
 # %%
-# Define model
-# ~~~~~~~~~~~~
-#
-# We use a simple U-Net architecture with 2 scales as the denoiser
-# network.
-#
-# To reduce training time, we use a pretrained model. Here we demonstrate
-# training with 100 images for 1 epoch, after having loaded a pretrained
-# model trained that was with 1000 images for 20 epochs.
-#
-# .. note::
-#
-#      When using the splitting loss, the model must be "adapted" by the loss, as its forward pass takes only a subset of the pixels, not the full image.
-#
+# When training with data that is slow to be loaded, it is faster to save the pre-loaded slices:
 
-model = dinv.models.ArtifactRemoval(
-    dinv.models.UNet(in_channels=1, out_channels=1, scales=2).to(device), pinv=True
-)
-model = loss.adapt_model(model)
-
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-8)
-
-# Load pretrained model
-file_name = "demo_measplit_mnist_tomography.pth"
-url = get_weights_url(model_name="measplit", file_name=file_name)
-ckpt = torch.hub.load_state_dict_from_url(
-    url, map_location=lambda storage, loc: storage, file_name=file_name
-)
-
-model.load_state_dict(ckpt["state_dict"])
-optimizer.load_state_dict(ckpt["optimizer"])
-
+if not any(SLICE_DIR.iterdir()):
+    for i, (x, y, params) in tqdm(enumerate(dataset)):
+        torch.save([x, y, params], SLICE_DIR / f"{i}.pt")
 
 # %%
-# Train and test network
-# ----------------------
+# Then the dataset can be loaded very quickly. We also pre-normalize to bring the data into a more friendly range.
+# We also load a rough noise level as a param to be passed into the physics.
+# The ground truth is loaded for evaluation later.
+
+def loader(f):
+    x, y, params = torch.load(f, weights_only=True)
+    return x * 1e5, y * 1e5, params | {"sigma": 1e-5 * 1e5}
+
+dataset = dinv.datasets.ImageFolder(SLICE_DIR, x_path="*.pt", loader=loader)
+
+# %%
+# Physics
+# -------
+# The multicoil physics is defined every easily:
+
+physics = dinv.physics.MultiCoilMRI(device=device, noise_model=dinv.physics.GaussianNoise(0.))
+
+# %%
+# Model
+# -----
+# For the model we use a fairly large MoDL with a UNet backbone, with 12 iterations:
+
+denoiser = dinv.models.UNet(2, 2, scales=5, batch_norm=False)
+model = dinv.models.MoDL(denoiser=denoiser, num_iter=12).to(device)
+
+# %%
+# Loss
+# ----
+# We define the weighted SSDU loss by first defining the generator that generates the splitting masks.
+# These splitting masks are multiplied with the measurements as per the original paper.
+# Finally, the weighted SSDU loss requires knowledge of the original physics generator to define the weight.
 #
+# .. info::
+#     Feel free to use any self-supervised loss you like here!
+
+split_generator = dinv.physics.generator.GaussianMaskGenerator((256, 256), acceleration=2, center_fraction=0., device=device)
+mask_generator = dinv.physics.generator.MultiplicativeSplittingMaskGenerator((256, 256), split_generator, device=device)
+physics_generator = dinv.physics.generator.GaussianMaskGenerator((256, 256), acceleration=6, center_fraction=0.04, rng=rng, device=device)
+loss = dinv.loss.mri.WeightedSplittingLoss(mask_generator=mask_generator, physics_generator=physics_generator)
+
+# %%
+# Training
+# --------
+# We train the model using the self-supervised loss. We randomly split the dataset into training and validation for 
+# early stopping (up to a maximum of 100 epochs).
+# Because the FastMRI ground truth are cropped magnitude root-sum-of-squares reconstructions, we define a helper metric for evaluation later.
+
+def crop(x_net, x):
+    """Crop to GT shape then take magnitude."""
+    return dinv.utils.MRIMixin().rss(
+        dinv.utils.MRIMixin().crop(x_net, shape=x.shape), multicoil=False
+    )
+
+class CropPSNR(dinv.metric.PSNR):
+    def forward(self, x_net=None, x=None, *args, **kwargs):
+        return super().forward(crop(x_net, x), x, *args, **kwargs)
+
+metric = CropPSNR(max_pixel=None)
+
+train_dataset, val_dataset = torch.utils.data.random_split(dataset, (0.8, 0.2), generator=rng_cpu)
 
 trainer = dinv.Trainer(
     model=model,
     physics=physics,
-    epochs=1,
     losses=loss,
-    optimizer=optimizer,
-    device=device,
-    train_dataloader=train_dataloader,
-    plot_images=False,
+    metrics=metric,
+    optimizer=torch.optim.Adam(model.parameters(), lr=1e-6),
+    train_dataloader=DataLoader(train_dataset, shuffle=True),
+    eval_dataloader=DataLoader(val_dataset),
+    epochs=100,
     save_path=None,
-    verbose=True,
-    show_progress_bar=False,
-    no_learning_method="A_dagger",  # use pseudo-inverse as no-learning baseline
+    show_progress_bar=True,
+    early_stop=True,
+    device=device,
 )
 
 model = trainer.train()
-
+model.eval()
 
 # %%
-# Test and visualize the model outputs using a small test set. We set the
-# output to average over 5 iterations of random mask realisations. The
-# trained model improves on the no-learning reconstruction by ~7dB.
-#
+# Evaluation
+# ----------
+# Now that the model is trained, we test the model on 3 samples
+# by evaluating the model, plotting and saving the reconstructions and evaluation metrics.
 
-trainer.plot_images = True
-trainer.test(test_dataloader)
+for i in [len(dataset) // 2 - 1, len(dataset) // 2, len(dataset) // 2 + 1]:
+    # Load slice
+    x, y, params = default_collate([dataset[i]])
+    x, y, params = (x, y.to(device), {k: (v.to(device) if isinstance(v, torch.Tensor) else v) for (k, v) in params.items()})
+    
+    physics.update(**params)
 
+    # Compute baseline reconstructions
+    x_adj = physics.A_adjoint(y).detach().cpu()
+    x_dag = physics.A_dagger(y).detach().cpu()
+
+    # Evaluate model
+    with torch.no_grad():
+        x_hat = model(y, physics).detach().cpu()
+
+    dinv.utils.plot({
+        "GT": x,
+        "Adjoint": crop(x_adj, x),
+        "SENSE": crop(x_dag, x),
+        "Trained": crop(x_hat, x),
+    }, subtitles=[
+        "",
+        f"{metric(x_adj, x).item():.2f} dB",
+        f"{metric(x_dag, x).item():.2f} dB",
+        f"{metric(x_hat, x).item():.2f} dB",
+    ], save_fn=OUT_DIR / f"result_{i}.png", close=True, show=False)
 
 
 # %%
