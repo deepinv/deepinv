117e6cd7ae4e4147242b4b68700aed83ae8fef34
Andrewwango <andrew.wang27@gmail.com>
2025-10-30 19:31:51 +0000

    fix test

--- deepinv/tests/test_datasets.py
+++ deepinv/tests/test_datasets.py
@@ -673,7 +673,7 @@ def mock_lidc_idri():
             patch.object(pd, "read_csv", return_value=dummy_df),
             patch.object(os, "listdir", return_value=["Slice1.dcm", "Slice2.dcm"]),
             # We use patch instead of patch.object to avoid cluttering the namespace.
-            patch("deepinv.utils.io.dcmread", return_value=dummy_dicom),
+            patch("pydicom.dcmread", return_value=dummy_dicom),
         ):
             yield "/dummy"
     else:
