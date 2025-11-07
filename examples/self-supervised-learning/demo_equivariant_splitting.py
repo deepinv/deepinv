fc432d385a5931b751a372f47b6a727815d72f73
Jérémy Scanvic <jscanvicz@gmail.com>
2025-11-07 07:57:17 +0100

    change EILoss to ESLoss

--- examples/self-supervised-learning/demo_equivariant_splitting.py
+++ examples/self-supervised-learning/demo_equivariant_splitting.py
@@ -136,23 +136,26 @@ batch_size = 16 if torch.cuda.is_available() else 1
 
 # choose self-supervised training losses
 # generates 4 random rotations per image in the batch
-losses = [dinv.loss.MCLoss(), dinv.loss.EILoss(dinv.transform.Rotate(n_trans=4))]
+mask_generator = dinv.physics.generator.MultiplicativeSplittingMaskGenerator(
+    (1, *img_size), split_generator, device=device
+)
+# A random transformation from the group D4
+train_transform = Rotate(n_trans=1, multiples=90, positive=True) * Reflect(
+    n_trans=1, dim=[-1]
+)
+# # All of the transformations from the group D4
+eval_transform = None  # use same as train
+
+losses = [dinv.loss.MCLoss(), dinv.loss.ESLoss(
+    mask_generator=mask_generator,
+    noise_model=physics.noise_model,
+    train_transform=train_transform,
+    eval_transform=eval_transform,
+)]
 
 # choose optimizer and scheduler
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-8)
 scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=int(epochs * 0.8) + 1)
-
-# start with a pretrained model to reduce training time
-file_name = "new_demo_ei_ckp_150_v3.pth"
-url = get_weights_url(model_name="demo", file_name=file_name)
-ckpt = torch.hub.load_state_dict_from_url(
-    url,
-    map_location=lambda storage, loc: storage,
-    file_name=file_name,
-)
-# load a checkpoint to reduce training time
-model.load_state_dict(ckpt["state_dict"])
-optimizer.load_state_dict(ckpt["optimizer"])
 
 # %%
 # Train the network
