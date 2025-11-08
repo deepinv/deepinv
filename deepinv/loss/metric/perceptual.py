8331786a92ab7e3aa6625803af276c41a252d107
nucli-vicky <vic.deridder@nuclivision.com>
2025-11-08 21:25:45 +0100

    move scipy import

--- deepinv/loss/metric/perceptual.py
+++ deepinv/loss/metric/perceptual.py
@@ -5,7 +5,6 @@ import math
 import torch
 import torch.nn.functional as F
 import torch.nn.functional as F
-from scipy.io import loadmat
 
 from deepinv.loss.metric.metric import Metric, import_pyiqa
 from deepinv.physics.functional.convolution import conv2d
@@ -154,7 +153,10 @@ class NIQE(Metric):
         #     "https://huggingface.co/chaofengc/IQA-PyTorch-Weights/resolve/main/niqe_modelparameters.mat",
         #     timeout=2.5,
         # )
-
+        try:
+            from scipy.io import loadmat
+        except:  # pragma: no cover
+            raise ImportError("NIQE requires scipy. Please install it")
         resp = requests.get(
             "https://huggingface.co/chaofengc/IQA-PyTorch-Weights/resolve/main/niqe_matlab_params.mat",
             timeout=2.5,
