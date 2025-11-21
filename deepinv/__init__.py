a2b4953e242233f2319db16a4b2f2904b6b69e47
hossein <alimohammadi.hossein90@gamil.com>
2025-11-21 06:41:15 +0100

    Fix formatting: add newline at end of file

--- deepinv/__init__.py
+++ deepinv/__init__.py
@@ -72,8 +72,8 @@ import warnings
 # Check Python version
 if sys.version_info < (3, 10):  # pragma: no cover
     warnings.warn(
-        "You are using a Python version lower than 3.10. deepinv officially supports "
-        "Python >= 3.10. Running on < 3.10 may work, but it is unsupported, unstable "
-        "and may lead to unexpected bugs. For more information on updating to Python "
-        "3.10, see https://www.python.org/downloads"
+        "You are using a Python version lower than 3.10. deepinv officially supports Python >= 3.10. "
+        "Running on < 3.10 may work, but it is unsupported, unstable and may lead to unexpected bugs. "
+        "For more information on updating to Python 3.10, see https://www.python.org/downloads"
     )
+
