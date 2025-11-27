ef42f89e77601a502d60f4413822ae4bd3ed952f
samuro95 <huraultsamuel@gmail.com>
2025-11-27 11:06:44 +0100

    MMSSE

--- deepinv/models/mmse.py
+++ deepinv/models/mmse.py
@@ -1,7 +1,8 @@
+from __future__ import annotations
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from .base import Denoiser
+from deepinv.models import Denoiser
 
 
 class MMSE(Denoiser):
@@ -25,31 +26,35 @@ class MMSE(Denoiser):
         self.register_buffer("atoms", dataset.clone().detach())
 
     def forward(self, x: torch.Tensor, sigma: torch.tensor | float) -> torch.Tensor:
-        B = x.shape[0]
-        atoms = self.atoms
-        sigma2 = self.sigma ** 2
-
-        # Squared distance: ||x - a_k||²
+        if isinstance(sigma, torch.Tensor):
+            assert len(sigma) == x.shape[0]
+            sigma = sigma[:, None]
+        # distance: ||x - a_k||
         dist = torch.norm(
-            x.view(B, 1, -1) - atoms.view(1, atoms.shape[0], -1), dim=2
+            x.view(x.shape[0], 1, -1) - self.atoms.view(1, self.atoms.shape[0], -1), dim=2
         ) # (B, N)
-    
         # Posterior weights
-        alpha = F.softmax(-dist2 / sigma**2, dim=1) 
-
+        alpha = F.softmax(-dist**2 / sigma**2, dim=1) 
         # MMSE estimate
-        denoised_flat = weights @ atoms_flat     # (B, D)
+        denoised_flat = alpha @ self.atoms.view(self.atoms.shape[0], -1)
         return denoised_flat.view_as(x)
 
 
 if __name__ == "__main__":
-    devie = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     # Example usage
-    dataset = torch.randn(100, 1, 28, 28).to(device)  # Example dataset of 100 images
+    from torchvision import datasets
+    import torchvision.transforms as T
+    dataset = datasets.MNIST(root='/lustre/fswork/projects/rech/kkn/uuw19zo/datasets', train=False, download=True)
+    dataset = torch.stack([T.ToTensor()(img) for img, _ in dataset])
+    #dataset = torch.randn(100, 1, 28, 28).to(device)  # Example dataset of 100 images
+    print(dataset.shape)
     denoiser = MMSE(dataset).to(device)
 
     noisy_images = torch.randn(10, 1, 28, 28).to(device)  # Batch of 10 noisy images
-    sigma = 0.1
+    sigma = 0.1 * torch.ones(10).to(device)
 
     denoised_images = denoiser(noisy_images, sigma)
-    print(denoised_images.shape)  # Should be (10, 1, 28, 28)
+    from deepinv.utils import plot
+    plot(denoised_images[0], save_dir="test.png")
+
