e3259ad281ef6915451d6fccc33064dd7ffd262c
Samuel Hurault <samuel.hurault@u-bordeaux.fr>
2025-11-28 13:48:24 +0100

    example FM

--- examples/sampling/demo_flow_matching.py
+++ examples/sampling/demo_flow_matching.py
@@ -28,34 +28,34 @@ from deepinv.models import MMSE
 #
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+num_workers = 0 if device.type == 'cpu' else 4
 dtype = torch.float64
 figsize = 2.5
 
 # We use the closed-form MMSE denoiser defined using as atoms the testset of MNIST.
 # The deepinv MMSE denoiser takes as input a dataloader.
-dataset = datasets.MNIST(root='.', train=False, download=True, transform=transforms.ToTensor())
-dataloader = torch.utils.data.DataLoader(dataset, batch_size=500, shuffle=False, num_workers=4)
+dataset = datasets.MNIST(root='.', train=True, download=True, transform=transforms.ToTensor())
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=100, shuffle=False, num_workers=num_workers)
 denoiser = dinv.models.MMSE(dataloader=dataloader, device=device, dtype=torch.float32)
 
 
-# %% Define the Flow-Matching ODE 
+# %% Define the Flow-Matching ODE and perform unconditional generation 
 # ----------------------------------------------------------------
 # The FlowMatching module takes as input the denoiser and the ODE solver.
 # 
 
 num_steps = 10
 
-# With FM
 rng = torch.Generator(device).manual_seed(42)
 timesteps = torch.linspace(0.99, 0., num_steps)
 solver = EulerSolver(timesteps=timesteps, rng=rng)
-model = FlowMatching(
+sde = FlowMatching(
     denoiser=denoiser,
     solver=solver,
     device=device
 )
 
-sample, trajectory = model(
+sample, trajectory = sde(
     x_init=(1, 1, 28, 28),
     seed=42,
     get_trajectory=True,
@@ -69,7 +69,7 @@ dinv.utils.plot(
 )
 
 dinv.utils.save_videos(
-    trajectory.cpu()[::1],
+    trajectory.cpu(),
     time_dim=0,
     titles=["FM Trajectory"],
     save_fn="FM_trajectory.gif",
@@ -77,7 +77,53 @@ dinv.utils.save_videos(
 )
 
 
+# %% Perform posterior sampling
+# ----------------------------------------------------------------
+# Now, we can use the Flow-Matching model to perform posterior sampling.
+# When the data fidelity is given, together with the measurements and the physics, we can perform posterior sampling for inverse problems.
+# For example, consider the inpainting problem, where we have a noisy image and we want to recover the original image.
+# We can use the :class:`deepinv.sampling.DPSDataFidelity` as the data fidelity term.
 
-# With score model
-from deepinv.models import NCSNpp
-denoiser = NCSNpp(pretrained="download").to(device)
+x = next(iter(dataloader))[0][0].to(device).unsqueeze(0)
+mask = torch.ones_like(x)
+mask[..., 10:20, 10:20] = 0.0
+physics = dinv.physics.Inpainting(img_size=x.shape[1:], mask=mask, device=device)
+y = physics(x)
+
+weight = 1e-2  # guidance strength
+dps_fidelity = DPSDataFidelity(denoiser=denoiser, weight=weight)
+
+model = PosteriorDiffusion(
+    data_fidelity=dps_fidelity,
+    sde=sde,
+    solver=solver,
+    dtype=dtype,
+    device=device,
+    verbose=True,
+)
+# To perform posterior sampling, we need to provide the measurements, the physics and the solver.
+# Moreover, when the physics is given, the initial point can be inferred from the physics if not given explicitly.
+seed_1 = 1
+x_hat, trajectory = model(
+    y,
+    physics,
+    seed=seed_1,
+    get_trajectory=True,
+)
+
+# Here, we plot the original image, the measurement and the posterior sample
+dinv.utils.plot(
+    [x, y, x_hat],
+    show=True,
+    titles=["Original", "Measurement", "Posterior sample"],
+    figsize=(figsize * 3, figsize),
+    save_fn="FM_posterior.png",
+)
+# We can also save the trajectory of the posterior sample
+dinv.utils.save_videos(
+    trajectory,
+    time_dim=0,
+    titles=["Posterior sample with FM"],
+    save_fn="FM_posterior_trajectory.gif",
+    figsize=(figsize, figsize),
+)
