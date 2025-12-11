5328c359ccd664013904d13bebdd020814fb65b2
Jérémy Scanvic <jscanvicz@gmail.com>
2025-12-11 14:11:29 +0100

    refactor the code

--- deepinv/benchmark/urban100_gaussian_denoising.py
+++ deepinv/benchmark/urban100_gaussian_denoising.py
@@ -3,29 +3,40 @@ import deepinv as dinv
 import torchvision.transforms as transforms
 import numpy as np
 from tqdm.auto import tqdm
+from typing import Any
+
+class Benchmark:
+    r"""
+    Benchmark for Gaussian Denoising on Urban100 dataset
+
+    .. note::
+
+        The noise standard deviation is set to 25/255 for images normalized between 0 and 1.
+    """
+    def run(self, model: dinv.models.Denoiser, *, device: torch.device | str = torch.device("cpu")) -> Any:
+        """Run the benchmark on the given model"""
+        dataset = dinv.datasets.Urban100HR("data/Urban100", download=True, transform=transforms.ToTensor())
+
+        rng = torch.Generator(device)
+        physics = dinv.physics.Denoising(dinv.physics.GaussianNoise(sigma=25/255, rng=rng)).to(device)
+
+        psnr_fn = dinv.metric.PSNR(min_pixel=0.0, max_pixel=1.0).to(device)
+
+        psnrs = []
+        for k, x in enumerate(tqdm(dataset)):
+            x = x.unsqueeze(0).to(device)
+            y = physics(x, seed=k)
+
+            with torch.no_grad():
+                x_hat = model(y, physics.noise_model.sigma)
+
+            psnr = psnr_fn(x_hat, x).item()
+            psnrs.append(psnr)
+
+        return np.mean(psnrs), np.std(psnrs)
 
 device = dinv.utils.get_freer_gpu() if torch.cuda.is_available() else "cpu"
-
-dataset = dinv.datasets.Urban100HR("data/Urban100", download=True, transform=transforms.ToTensor())
-
-rng = torch.Generator()
-physics = dinv.physics.Denoising(dinv.physics.GaussianNoise(sigma=25/255, rng=rng)).to(device)
-
+benchmark = Benchmark()
 model = dinv.models.DRUNet().to(device).eval()
-
-psnr_fn = dinv.metric.PSNR(min_pixel=0.0, max_pixel=1.0).to(device)
-
-psnrs = []
-for k, x in enumerate(tqdm(dataset)):
-    x = x.unsqueeze(0).to(device)
-    y = physics(x, seed=k)
-
-    with torch.no_grad():
-        x_hat = model(y, physics.noise_model.sigma)
-
-    psnr = psnr_fn(x_hat, x).item()
-    psnrs.append(psnr)
-
-psnr_avg, psnr_std = np.mean(psnrs), np.std(psnrs)
-
+psnr_avg, psnr_std = benchmark.run(model, device=device)
 print(f"PSNR (dB): {psnr_avg:.2f} ± {psnr_std:.2f}")
