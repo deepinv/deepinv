1d5f3cca8387b92dc136c307c3cf19c98a416e0b
Matthieu Terris <matthieu.terris@gmail.com>
2026-01-09 16:02:44 +0100

    fix

--- deepinv/models/noise_level_estimation.py
+++ deepinv/models/noise_level_estimation.py
@@ -24,9 +24,9 @@ class WaveletNoiseEstimator(nn.Module):
         >>> import torch
         >>> from deepinv.models import WaveletNoiseEstimator
         >>> # set seed
-        >>> torch.Generator('cpu').manual_seed(0)
+        >>> rng = torch.Generator('cpu').manual_seed(0)
         >>> sigma_true = 0.1
-        >>> noise = sigma_true * torch.randn(1, 1, 256, 256)
+        >>> noise = sigma_true * torch.randn(1, 1, 256, 256, generator=rng)
         >>> noise_estimator = WaveletNoiseEstimator()
         >>> sigma_est = noise_estimator(noise)
         >>> print(sigma_est)
