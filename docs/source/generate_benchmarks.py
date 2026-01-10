7d987fbde4edfb6c279ca25ed1611086b78e78f9
Julián Tachella <tachellajulian@gmail.com>
2026-01-10 23:59:25 +0100

    update to benchopt

--- docs/source/generate_benchmarks.py
+++ docs/source/generate_benchmarks.py
@@ -1,46 +1,62 @@
 import os
 from sphinx.application import Sphinx
 import subprocess
+import shutil
 
 benchmark_mapping = {}
 
 
-def generate_rst_from_parquet(parquet_path, output_dir, benchmark_name):
+def process_parquet_file(parquet_path, benchmark_name):
     import pandas as pd
 
     df = pd.read_parquet(parquet_path)
-    dataset = df.attrs.get("dataset", "Unknown")
-    physics = df.attrs.get("physics", "Unknown")
-    noise = df.attrs.get("noise", "Unknown")
-    os.makedirs(output_dir, exist_ok=True)
-    rst_path = os.path.join(output_dir, f"{benchmark_name}.rst")
+
+    dataset = (
+        df["dataset_name"][0].split("[")[0]
+        if "dataset_name" in df.columns
+        else "Unknown"
+    )
+    physics = (
+        df["p_dataset_physics"][0] if "p_dataset_physics" in df.columns else "Unknown"
+    )
+    noise = df["p_dataset_noise"][0] if "p_dataset_noise" in df.columns else "Unknown"
 
     lines = [
+        ".. |plusminus| unicode:: U+00B1 .. plus-minus sign",
         f"""
 .. _{benchmark_name.replace('-', '_').replace(' ', '_')}:
 
 {benchmark_name.replace('-', ' ').replace('_', ' ')}
 {'=' * len(benchmark_name)}
 
-Benchmark results for {benchmark_name.replace('-', ' ').replace('_', ' ')}.
 
-**Benchmark information**:
-"""
+
+.. dropdown:: Benchmark information 
+    :chevron: down-up
+
+    """,
     ]
 
-    df = pd.read_parquet(parquet_path)
+    lines.append(f"    - *Dataset*: :sclass:`deepinv.datasets.{dataset}` \n")
+    lines.append(f"    - *Physics*: :sclass:`deepinv.physics.{physics}` \n")
+    lines.append(f"    - *Noise model*: :sclass:`deepinv.physics.{noise}` \n")
 
-    if df.attrs:
-        for key, value in df.attrs.items():
-            if key == "physics":
-                value = f":sclass:`deepinv.physics.{value}`\n"
-            elif key == "dataset":
-                value = f":sclass:`deepinv.datasets.{value}`\n"
-            elif key == "noise":
-                value = f":sclass:`deepinv.physics.{value}`\n"
+    # search for entries starting as "objective_" except for "objective_name"
+    dataset_cols = [
+        col
+        for col in df.columns
+        if col.startswith("p_dataset")
+        and col != "p_dataset_physics"
+        and col != "p_dataset_noise"
+    ]
 
-            lines.append(f"{key.title()}: {value}")
-        lines.append("")
+    for col in dataset_cols:
+        value = df.at[0, col]
+        col_name = col.replace("p_dataset_", "")
+        if col_name != "debug":
+            lines.append(f"    - *{col_name}*: {value} \n")
+
+    lines.append("\n")
 
     # Table directive
     lines.append(".. list-table::")
@@ -49,15 +65,39 @@ Benchmark results for {benchmark_name.replace('-', ' ').replace('_', ' ')}.
     lines.append("")
 
     # Add link to 'model' column if exists
-    if "model" in df.columns and "file" in df.columns:
+    if "solver_name" in df.columns and "file" in df.columns:
         for i, row in df.iterrows():
-            model_name = row["model"]
-            file_link = row["file"]
-            link_cell = f"`{model_name} <{file_link}>`_"
-            df.at[i, "model"] = link_cell  # Update the DataFrame directly
+            model_name = row["solver_name"]
+            # file_link = row["file"]
+            link_cell = f"`{model_name}"  # f"`{model_name} <{file_link}>`_"
+            df.at[i, "solver_name"] = link_cell  # Update the DataFrame directly
 
-        # remove 'file' column from display
-        df = df.drop(columns=["file"])
+    # extract metrics
+    metric_cols = [
+        col
+        for col in df.columns
+        if col.startswith("objective_") and col != "objective_name"
+    ]
+    std_cols = [col for col in metric_cols if col.endswith("_std")]
+    mean_cols = [col for col in metric_cols if col not in std_cols]
+
+    kept_cells = ["solver_name", "objective_runtime"] + mean_cols + std_cols
+
+    # Filter the dataframe to keep only these columns
+    df = df[df.columns.intersection(kept_cells)]
+
+    # perform the mean +- std formatting
+    for col in mean_cols:
+        std_col = col + "_std"
+        if std_col in df.columns:  # Check if it exists in the filtered DF
+            df[col] = df.apply(
+                lambda row: f"{row[col]:.2f} |plusminus| {row[std_col]:.2f}", axis=1
+            )
+            # Safe to drop now
+            df = df.drop(columns=[std_col])
+
+    # remove "objective_" prefix from metric columns
+    df = df.rename(columns={col: col.replace("objective_", "") for col in df.columns})
 
     # Header row
     header_cells = df.columns.tolist()
@@ -68,19 +108,26 @@ Benchmark results for {benchmark_name.replace('-', ' ').replace('_', ' ')}.
         row_cells = []
         for val in row:
             if isinstance(val, float):
-                row_cells.append(f"{val:.4g}")
+                row_cells.append(f"{val:.2f}")
             else:
                 row_cells.append(str(val))
         lines.append("   * - " + "\n     - ".join(row_cells))
 
+    return lines, dataset, physics, noise
+
+
+def generate_rst_from_parquet(parquet_path, output_dir, benchmark_name):
     # Write rst
+    lines, dataset, physics, noise = process_parquet_file(parquet_path, benchmark_name)
+    os.makedirs(output_dir, exist_ok=True)
+    rst_path = os.path.join(output_dir, f"{benchmark_name}.rst")
     with open(rst_path, "w") as f:
         f.write("\n".join(lines))
 
-    return benchmark_name, dataset, physics, noise
+    return dataset, physics, noise
 
 
-def generate_benchmarks_rst(benchmark_info, output_dir):
+def generate_main_rst(benchmark_info, output_dir):
     benchmarks_rst_path = os.path.join(output_dir, "benchmarks.rst")
     benchmarks_content = """Benchmarks
 =================
@@ -118,6 +165,18 @@ and then running:
         f.write(benchmarks_content)
 
 
+def on_rm_error(func, path, exc_info):
+    """
+    Error handler for shutil.rmtree.
+    On Windows, some files in .git are read-only. This changes
+    permissions and retries the removal.
+    """
+    import stat
+
+    os.chmod(path, stat.S_IWRITE)
+    func(path)
+
+
 def on_builder_inited(app):
     # Define the root directory of the benchmarks repository
     benchmarks_root = os.path.join(os.path.dirname(__file__), "deepinv-benchmarks")
@@ -139,6 +198,13 @@ def on_builder_inited(app):
                 text=True,
             )
             print("Repository cloned successfully.")
+
+            git_dir = os.path.join(benchmarks_root, ".git")
+            if os.path.exists(git_dir):
+                print("Stripping Git metadata...")
+                # Use the error handler to ensure Windows compatibility
+                shutil.rmtree(git_dir, onerror=on_rm_error)
+                print("Successfully converted to a standard directory.")
         except subprocess.CalledProcessError as e:
             print(f"Failed to clone repository: {e.stderr}")
             raise
@@ -161,31 +227,34 @@ def on_builder_inited(app):
 
     benchmark_info = []
     for folder, parquet_file in parquet_files:
-        benchmark_name = os.path.basename(folder)
+        parent_folder_path = os.path.dirname(folder)
+        benchmark_name = os.path.basename(parent_folder_path)
         try:
-            name, dataset, physics, noise = generate_rst_from_parquet(
+            dataset, physics, noise = generate_rst_from_parquet(
                 parquet_file, output_dir, benchmark_name
             )
-            benchmark_info.append((name, dataset, physics, noise))
+            benchmark_info.append((benchmark_name, dataset, physics, noise))
 
             # Add mapping for dataset and physics
             if dataset not in benchmark_mapping:
-                benchmark_mapping[dataset] = [name]
+                benchmark_mapping[dataset] = [benchmark_name]
             else:
-                benchmark_mapping[dataset].append(name)
+                benchmark_mapping[dataset].append(benchmark_name)
+
             if physics not in benchmark_mapping:
-                benchmark_mapping[physics] = [name]
+                benchmark_mapping[physics] = [benchmark_name]
             else:
-                benchmark_mapping[physics].append(name)
+                benchmark_mapping[physics].append(benchmark_name)
+
             if noise not in benchmark_mapping:
-                benchmark_mapping[noise] = [name]
+                benchmark_mapping[noise] = [benchmark_name]
             else:
-                benchmark_mapping[noise].append(name)
+                benchmark_mapping[noise].append(benchmark_name)
 
         except Exception as e:
             print(f"Failed to process {parquet_file}: {str(e)}")
 
-    generate_benchmarks_rst(benchmark_info, source_dir)
+    generate_main_rst(benchmark_info, source_dir)
 
 
 def add_benchmark_section(app, what, name, obj, options, lines):
