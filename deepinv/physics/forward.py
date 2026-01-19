139f82cadbe6178ac98b77442d0279c5a438427d
romainvo <romain.vo@hotmail.fr>
2026-01-19 14:08:57 +0100

    Revert changes: remove non tensor attribute update in update_parameters

--- deepinv/physics/forward.py
+++ deepinv/physics/forward.py
@@ -254,16 +254,16 @@ class Physics(torch.nn.Module):  # parent class for forward models
         """
         if kwargs:
             for key, value in kwargs.items():
-                if value is not None and hasattr(self, key):
+                if (
+                    value is not None
+                    and hasattr(self, key)
+                    and isinstance(value, torch.Tensor)
+                ):
 
-                    if isinstance(value, torch.Tensor):
-                        # Move `value` to the buffer's device before updating
-                        # regardless of where the `value` tensor is located.
-                        # Also performs type casting if necessary.
-                        setattr(self, key, value.to(getattr(self, key)))
-                    else:
-                        # Some parameters are not torch.Tensor, e.g. self.factor is a float in deepinv.physics.Downsampling
-                        setattr(self, key, value)
+                    # Move `value` to the buffer's device before updating
+                    # regardless of where the `value` tensor is located.
+                    # Also performs type casting if necessary.
+                    setattr(self, key, value.to(getattr(self, key)))
 
     # NOTE: Physics instances can hold instances of torch.Generator as
     # (possibly nested) attributes and they cannot be copied using deepcopy
