0d68245dc033febf75c0b62464fc7506b75b682b
Dylan Sechet <dylan.sechet@inria.fr>
2026-01-23 15:41:39 +0100

    test: add extra regression tests for backward with anderson acceleration

--- deepinv/tests/test_unfolded.py
+++ deepinv/tests/test_unfolded.py
@@ -14,9 +14,15 @@ UNFOLDED_ALGO = [
     "FISTA",
 ]
 
+UNFOLDED_ALGO_PARAMS = [
+    (algo, anderson)
+    for algo in UNFOLDED_ALGO
+    for anderson in ([True, False] if algo in ["PGD", "HQS", "GD"] else [False])
+]
 
-@pytest.mark.parametrize("unfolded_algo", UNFOLDED_ALGO)
-def test_unfolded(unfolded_algo, imsize, dummy_dataset, device):
+
+@pytest.mark.parametrize("unfolded_algo,and_acc", UNFOLDED_ALGO_PARAMS)
+def test_unfolded(unfolded_algo, and_acc, imsize, dummy_dataset, device):
     pytest.importorskip("ptwt")
 
     # Select the data fidelity term
@@ -73,6 +79,7 @@ def test_unfolded(unfolded_algo, imsize, dummy_dataset, device):
         data_fidelity=data_fidelity,
         max_iter=max_iter,
         prior=prior,
+        anderson_acceleration=and_acc,
     )
     model.to(device)
 
