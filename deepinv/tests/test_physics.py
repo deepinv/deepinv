0a04c8a67eaa6809c83cc44af97f1522e8ff8156
Julián Tachella <tachellajulian@gmail.com>
2026-02-18 18:12:45 +0100

    skip test if scipy is not available

--- deepinv/tests/test_physics.py
+++ deepinv/tests/test_physics.py
@@ -2365,10 +2365,10 @@ def test_scattering_mie(device, wavenumber, contrast, wave_type):
 
     We limit the number of tests, since this is a rather long test
     """
-    if version.parse(torch.__version__) < version.parse("2.6.0"):
-        pytest.skip(
-            "Scattering physics requires PyTorch 2.6.0 or higher, as it relies on torch.special"
-        )
+    try:
+        import scipy
+    except ImportError:
+        pytest.skip("Scipy is required for this test.")
 
     wavenumber = torch.tensor([wavenumber])
     cylinder_contrast = contrast
