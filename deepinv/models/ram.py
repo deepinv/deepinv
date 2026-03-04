75c30a4a1cc0ff02258d488a902456b7efa62c55
mh-nguyen712 <minh-hai.nguyen@irit.fr>
2026-03-04 18:26:27 +0100

    rollback doctests

--- deepinv/models/ram.py
+++ deepinv/models/ram.py
@@ -43,15 +43,16 @@ class RAM(Reconstructor, Denoiser):
 
     |sep|
 
-    ::
+    :Examples:
 
-      import deepinv as dinv
-      x = dinv.utils.load_example("butterfly.png")
-      physics = dinv.physics.Downsampling(filter="bicubic", noise_model=dinv.physics.GaussianNoise(0.01))
-      y = physics(x)
-      model = dinv.models.RAM()
-      x_hat = model(y, physics) # run model
-      dinv.metric.PSNR()(x_hat, x) > 29.75 # tensor([True])
+        >>> import deepinv as dinv
+        >>> x = dinv.utils.load_example("butterfly.png")
+        >>> physics = dinv.physics.Downsampling(filter="bicubic", noise_model=dinv.physics.GaussianNoise(0.01))
+        >>> y = physics(x)
+        >>> model = dinv.models.RAM()
+        >>> x_hat = model(y, physics) # run model
+        >>> dinv.metric.PSNR()(x_hat, x) > 29.75 #
+        ... tensor([True])
 
     """
 
