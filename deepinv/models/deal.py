98e28d863ff0f69c07b33d9ebd356b19abf2e876
Hossein Alimohammadi <103756773+Holimmo7@users.noreply.github.com>
2026-03-08 00:41:05 +0100

    Update deal.py

--- deepinv/models/deal.py
+++ deepinv/models/deal.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import torch
 from deepinv.physics import LinearPhysics
 from .base import Reconstructor
-from . import deal_lib
+from . import deal_impl as deal_lib
 
 
 class DEAL(Reconstructor):
