468421771ccaf53183f446b108dbc9f0ae061f0c
Jérémy Scanvic <jscanvicz@gmail.com>
2026-03-27 23:01:51 +0100

    get rid of self.splitting_loss

--- deepinv/loss/es.py
+++ deepinv/loss/es.py
@@ -1,8 +1,9 @@
 import deepinv as dinv
-import math
 import deepinv as dinv
 
 from deepinv.loss.loss import Loss
+from deepinv.loss.mc import MCLoss
+from deepinv.loss.metric.distortion import MSE
 from deepinv.loss.measplit import SplittingLoss
 from deepinv.loss.r2r import R2RLoss
 
@@ -25,30 +26,37 @@ class ESLoss(Loss):
         equivariant_model: bool = False,
     ):
         super().__init__()
-        self.splitting_loss = SplittingLoss()
         if not isinstance(noise_model, dinv.physics.ZeroNoise):
             consistency_loss = R2RLoss(alpha=alpha, eval_n_samples=eval_n_samples)
         else:
             consistency_loss = None
         self.consistency_loss = consistency_loss
+        self.prediction_loss = MCLoss(metric=MSE())
         self.transform = transform
         self.eval_transform = eval_transform
         self.equivariant_model = equivariant_model
 
     def forward(self, x_net, y, physics, model, **kwargs):
-        masks = self.splitting_model.get_masks()
+        splitting_model = getattr(model, "_splitting_model")
+        masks = splitting_model.get_masks()
         loss_total = 0
         N_masks = 0
         for mask in masks:
             mask = mask * getattr(physics, "mask", 1.0)
             mask2 = getattr(physics, "mask", 1.0) - mask
-            y2, physics2 = self.splitting_loss.split(mask2, y, physics)
-            l = self.splitting_loss.metric(physics2.A(x_net), y2)
-
-            loss_value = l / mask2.mean() if self.splitting_loss.normalize_loss else l
+            y2, physics2 = SplittingLoss.split(mask2, y, physics)
+            loss_value = self.prediction_loss(
+                x_net=x_net,
+                y=y2,
+                physics=physics2,
+                model=model,
+                **kwargs,
+            )
+            # Normalization
+            loss_value = loss_value / mask2.mean()
 
             if self.consistency_loss is not None:
-                y1, physics1 = self.splitting_loss.split(mask, y, physics)
+                y1, physics1 = SplittingLoss.split(mask, y, physics)
                 loss_value = loss_value + self.consistency_loss(
                     x_net=x_net,
                     y=y1,
@@ -61,7 +69,7 @@ class ESLoss(Loss):
         return loss_total / N_masks
 
     def adapt_model(self, model):
-        if not getattr(model, "_ESLoss_adapted", False):
+        if not hasattr(model, "_splitting_model"):
             # if the model is not already equivariant, we make it so using Reynolds averaging
             if not self.equivariant_model:
                 model = dinv.models.EquivariantReconstructor(
@@ -69,9 +77,21 @@ class ESLoss(Loss):
                     transform=self.transform,
                     eval_transform=self.eval_transform,
                 )
-            model = self.splitting_loss.adapt_model(model)
-            self.splitting_model = model
+
+            splitting_model = SplittingLoss.SplittingModel(
+                model,
+                split_ratio=0.9,
+                mask_generator=None,
+                eval_n_samples=5,
+                eval_split_input=True,
+                eval_split_output=False,
+                pixelwise=True,
+            )
+
             if self.consistency_loss is not None:
-                model = self.consistency_loss.adapt_model(model)
-            setattr(model, "_ESLoss_adapted", True)
+                model = self.consistency_loss.adapt_model(splitting_model)
+            else:
+                model = splitting_model
+
+            setattr(model, "_splitting_model", splitting_model)
         return model
