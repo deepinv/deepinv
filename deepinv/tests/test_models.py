b95d73592a99d67ad33e95038c103fcaa42b1982
Julián Tachella <tachellajulian@gmail.com>
2026-05-11 12:16:03 +0200

    black

--- deepinv/tests/test_models.py
+++ deepinv/tests/test_models.py
@@ -1771,19 +1771,26 @@ def test_gaussian_noise_estimators(
         model(y), torch.cat([model(_y.unsqueeze(0)) for _y in y]), rtol=1e-2, atol=1e-2
     )
 
-@pytest.mark.parametrize("sigma", [0., .05])
-@pytest.mark.parametrize("gain", [.05, .5])
+
+@pytest.mark.parametrize("sigma", [0.0, 0.05])
+@pytest.mark.parametrize("gain", [0.05, 0.5])
 def test_anscombe_transform(sigma, gain, device, rng, load_example_image):
     x = torch.ones((1, 1, 16, 16), device=device)
-    physics = dinv.physics.Denoising(dinv.physics.PoissonGaussianNoise(sigma=sigma, gain=gain))
+    physics = dinv.physics.Denoising(
+        dinv.physics.PoissonGaussianNoise(sigma=sigma, gain=gain)
+    )
     y = physics(x)
     z = dinv.models.generalized_anscombe_transform(y, sigma=sigma, gain=gain)
 
     # std(GAT(y)) \approx gain
-    assert torch.allclose(z.std(), torch.tensor(gain, device=device), atol=0.05, rtol=0.05)
+    assert torch.allclose(
+        z.std(), torch.tensor(gain, device=device), atol=0.05, rtol=0.05
+    )
 
     # IGAT(GAT(y))\approx y
-    y_inv = dinv.models.inverse_generalized_anscombe_transform(z, sigma=sigma, gain=gain)
+    y_inv = dinv.models.inverse_generalized_anscombe_transform(
+        z, sigma=sigma, gain=gain
+    )
     assert torch.allclose(y, y_inv, atol=0.1, rtol=0.1)
 
     x = load_example_image(
@@ -1795,7 +1802,7 @@ def test_anscombe_transform(sigma, gain, device, rng, load_example_image):
     denoiser = dinv.models.DRUNet(device=device)
     ans_denoiser = dinv.models.AnscombeDenoiser(denoiser)
     with torch.no_grad():
-        x_base = denoiser(y, sigma=(sigma**2 + gain)**(.5))
+        x_base = denoiser(y, sigma=(sigma**2 + gain) ** (0.5))
         x_ans = ans_denoiser(y, sigma=sigma, gain=gain)
         psnr_ans = metric(x_ans, x)
         psnr_raw = metric(y, x)
