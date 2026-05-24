15f6530f52c0635453605ae807b9c719e1cd9c35
Vicky De Ridder <78253029+nucli-vicky@users.noreply.github.com>
2026-05-24 15:56:58 +0200

    Add SRResNet (#1164)

--- deepinv/models/__init__.py
+++ deepinv/models/__init__.py
@@ -48,6 +48,7 @@ from .poisson2sparse import ConvLista, Poisson2Sparse
 from .kernel_network import KernelIdentificationNetwork
 from .bilateral import BilateralFilter
 from .noise_level_estimation import WaveletNoiseEstimator, PatchCovarianceNoiseEstimator
+from .srresnet import SRResNet
 from .ffdnet import FFDNet
 
 from .third_party import PromptIR
