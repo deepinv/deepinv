0ef4647fd3e60d580421c7400316529aa9a204aa
Pierre WEISS <pierre.weiss@irit.fr>
2026-07-01 14:06:59 +0200

    typo

--- deepinv/physics/generator/blur.py
+++ deepinv/physics/generator/blur.py
@@ -973,8 +973,8 @@ def bump_function(x, a=1.0, b=1.0):
     transition = torch.exp(-1.0 / (1.0 - safe_t**2)) / np.exp(-1.0)
     return torch.where(
         abs_x <= a,
-        1.0
-        torch.where(abs_x < a + b, transition, 0.),
+        1.0,
+        torch.where(abs_x < a + b, transition, 0.0),
     )
 
 
