68e53cac6ca3a3712d444fa18df3f64e5557298f
msteinberg02 <mauricesteinberg54@gmail.com>
2026-07-08 22:26:45 +0200

    Update lsmr.py

--- deepinv/optim/linear/lsmr.py
+++ deepinv/optim/linear/lsmr.py
@@ -45,7 +45,7 @@ def lsmr(
     """
 
     if stagtol is None:
-        stagtol = 10.0 * torch.finfo(b.dtype).eps
+        stagtol = 8.0 * torch.finfo(b.dtype).eps
 
     if isinstance(parallel_dim, int):
         parallel_dim = [parallel_dim]
