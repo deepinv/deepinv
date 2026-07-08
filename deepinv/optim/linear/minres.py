7c3c3298d139255d18517d3aec679aedcdad83bc
msteinberg02 <mauricesteinberg54@gmail.com>
2026-07-08 22:27:12 +0200

    Update minres.py

--- deepinv/optim/linear/minres.py
+++ deepinv/optim/linear/minres.py
@@ -41,7 +41,7 @@ def minres(
     """
 
     if stagtol is None:
-        stagtol = 10.0 * torch.finfo(b.dtype).eps
+        stagtol = 8.0 * torch.finfo(b.dtype).eps
 
     if isinstance(parallel_dim, int):
         parallel_dim = [parallel_dim]
