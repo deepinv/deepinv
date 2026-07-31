6fff07525e58a2a5a3c1626200fbe0f6c2f87916
Thibaut Modrzyk <52449813+Tmodrzyk@users.noreply.github.com>
2026-07-31 09:58:40 +0200

    revert changes related to the plot function

--- deepinv/utils/plotting.py
+++ deepinv/utils/plotting.py
@@ -450,13 +450,8 @@ def plot(
 
     for i, row_imgs in enumerate(imgs):
         for r, img in enumerate(row_imgs):
-            display_kwargs = imshow_kwargs.copy()
-            is_rgb = img.ndim == 3 and img.shape[-1] in (3, 4)
-            if not is_rgb and "norm" not in display_kwargs:
-                display_kwargs.setdefault("vmin", 0.0)
-                display_kwargs.setdefault("vmax", 1.0)
             im = axs[r, i].imshow(
-                img, cmap=cmap, interpolation=interpolation, **display_kwargs
+                img, cmap=cmap, interpolation=interpolation, **imshow_kwargs
             )
             if cbar:
                 from mpl_toolkits.axes_grid1 import make_axes_locatable
@@ -472,7 +467,6 @@ def plot(
                     true_labels = [
                         f"{t * (vmax_true - vmin_true) + vmin_true:.3g}" for t in ticks
                     ]
-                    colbar.set_ticks(ticks)
                     colbar.set_ticklabels(true_labels)
             if titles and r == 0:
                 axs[r, i].set_title(titles[i], wrap=True)
