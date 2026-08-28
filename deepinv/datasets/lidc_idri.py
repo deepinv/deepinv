4b6df0e2b6fdde1b6b46b863660235feb35893f6
PAUL BERNARD <74709745+PAUL-BERNARD@users.noreply.github.com>
2026-08-28 16:51:57 +0200

    Fix some issues in the documentation (#1341)
    * fix doc 1
    
    * Fix changelog
    
    * fix doc gaussian_blur takes a named argument
    
    * gaussian_blur sigma is a tuple for 4D kernels

--- deepinv/datasets/lidc_idri.py
+++ deepinv/datasets/lidc_idri.py
@@ -23,7 +23,7 @@ class LidcIdriSliceDataset(ImageDataset):
 
     .. warning::
         To download the raw dataset, you will need to install the `NBIA Data Retriever <https://wiki.cancerimagingarchive.net/display/NBIA/Downloading+TCIA+Images>`_,
-        then download the manifest file (.tcia file)`here <https://www.cancerimagingarchive.net/collection/lidc-idri/>`_, and open it by double clicking.
+        then download the manifest file (.tcia file) `here <https://www.cancerimagingarchive.net/collection/lidc-idri/>`_, and open it by double clicking.
 
 
     **Raw data file structure:** ::
