6dfe333120ec170a38954411fc87fc1fd168e2f6
Samuel Hurault <huraultsamuel@gmail.com>
2026-09-01 11:17:13 +0200

    basic start

--- deepinv/optim/algorithms/gd.py
+++ deepinv/optim/algorithms/gd.py
@@ -1,12 +1,16 @@
 from deepinv.optim.base_optim import BaseOptim
+import deepinv as dinv
+from deepinv.optim.potential import Potential
 
 class GD(BaseOptim):
-    def __init__(self, F, stepsize):
+    def __init__(self, F : Potential, stepsize : float, max_iter : int):
         super().__init__(F)
         self.stepsize = stepsize
         self.F = F
+        self.max_iter = max_iter
 
-    def forward(self, y, physics, init=None, x_gt=None):
-        x = x - self.stepsize * self.F.grad(x, y, physics)
+    def forward(self, init=None):
+        x = init
+        for it in range(self.max_iter):
+            x = x - self.stepsize * self.F.grad(x)
         return x
-        
--- deepinv/optim/algorithms/pgd.py
+++ deepinv/optim/algorithms/pgd.py
@@ -1,13 +1,18 @@
 from deepinv.optim.base_optim import BaseOptim
+import deepinv as dinv
+from deepinv.optim.potential import Potential
 
 class PGD(BaseOptim):
-    def __init__(self, F, G, stepsize):
+    def __init__(self, F : Potential, G : Potential, stepsize : float, max_iter : int):
         super().__init__(F)
         self.stepsize = stepsize
         self.F = F
         self.G = G
+        self.max_iter = max_iter
 
-    def forward(self, y, physics, init=None, x_gt=None):
-        x = x - self.stepsize * self.F.grad(x, y, physics)
-        x = self.G.prox(x, gamma=self.stepsize)
+    def forward(self, init=None):
+        x = init
+        for it in range(self.max_iter):
+            x = x - self.stepsize * self.F.grad(x)
+            x = self.G.prox(x, gamma=self.stepsize)
         return x
