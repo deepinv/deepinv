c8d6e1a0b5d31c1638562a709cd43e4a185d8050
PAUL BERNARD <Paul.Bernard@inria.fr>
2026-09-08 18:07:00 +0200

    More tags bis

--- examples/sampling/demo_ddrm.py
+++ examples/sampling/demo_ddrm.py
@@ -11,7 +11,7 @@ The DDRM method requires that:
 * The noise is Gaussian with known standard deviation (i.e., the noise model is :class:`deepinv.physics.GaussianNoise`).
 """
 
-# sphinx_gallery_tags = ["Diffusion"]
+# sphinx_gallery_tags = ["Diffusion", "Denoising"]
 
 # %%
 import deepinv as dinv
