c6abd10a1c0007cbf022ebd53dfd1bb9b12a5cf0
Andrew Wang <andrew.wang27@gmail.com>
2026-09-15 10:09:00 +0100

    More pretrained MRI examples (#1374)
    * cherry pick from #1311
    
    * towards DIRECT model
    
    * add prospective MRI
    
    * small
    
    * clean
    
    * cleaning
    
    * fix deps issue
    
    * use dict output
    
    * fix cpu doc
    
    * no new examples run on cpu
    
    * vicky comments part 1
    
    * vicky comments part 2
    
    * fixes
    
    * mini comments
    
    * docs
    
    * user guide
    
    * changelog
    
    * cglg
    
    * add tests and do todos
    
    * load_ismrmrd_raw test
    
    * fix doc build
    
    * fix doc + codecov
    
    * skip DIRECT tests because of python >3.12
    
    * use dict output, sorry romain
    
    * fix gpu doc
    
    * hotfix 2detect pr
    
    * Update deepinv/models/direct_mri.py
    
    Co-authored-by: Romain Vo <43675867+romainvo@users.noreply.github.com>
    
    * in progress
    
    * temp
    
    * add DIRECT to test CPU/GPU
    
    * romain final comments
    
    * black
    
    * fix doc
    
    * remove DIRECT from CPU test altogether due to torchvision problem
    
    * romain comments
    
    ---------
    
    Co-authored-by: romainvo <romain.vo@hotmail.fr>
    Co-authored-by: Romain Vo <43675867+romainvo@users.noreply.github.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -323,6 +323,8 @@ class MySortKey(_SortKey):
 gpu_dependent_files = [
     r".*demo_astra_tomography\.py",
     r".*demo_custom_niqe\.py",
+    r".*demo_mri_pretrained\.py",
+    r".*demo_prospective_mri\.py",
     r".*demo_astra_2detect\.py",
 ]
 # Create the ignore pattern based on GPU availability,
@@ -504,4 +506,5 @@ nitpick_ignore = [
     ("py:class", "torchvision.transforms.InterpolationMode"),
     ("py:class", "nib.arrayproxy.ArrayProxy"),
     ("py:class", "brainweb.Act"),
+    ("py:class", "blosc2.ndarray.NDArray"),
 ]
