6fce4937616cb2a4888adb8ef74899d78807aa62
PAUL BERNARD <Paul.Bernard@inria.fr>
2026-09-16 10:46:56 +0200

    DEEPINV_TEST_DEVICE environment variable

--- deepinv/tests/conftest.py
+++ deepinv/tests/conftest.py
@@ -1,3 +1,5 @@
+import warnings
+import os
 import pytest
 
 import torch
@@ -90,11 +92,24 @@ def pytest_terminal_summary(terminalreporter, exitstatus, config):
     )
 
 
-@pytest.fixture(
-    params=list(
-        dict.fromkeys([torch.device("cpu"), dinv.utils.get_device(verbose=False)])
-    )
-)
+def get_device_list():
+    env = os.environ.copy()
+    cpu = torch.device("cpu")
+    gpu = dinv.utils.get_device(verbose=False)
+
+    if env.get("DEEPINV_TEST_DEVICE") == "gpu":
+        if gpu.type == "cpu":
+            warnings.warn(
+                "DEEPINV_TEST_DEVICE variable environment is set to 'gpu', but no GPU device was found"
+            )
+        return [gpu]
+    if env.get("DEEPINV_TEST_DEVICE") == "cpu":
+        return [cpu]
+    else:
+        return list(dict.fromkeys([cpu, gpu]))
+
+
+@pytest.fixture(params=get_device_list())
 def device(request):
     return request.param
 
