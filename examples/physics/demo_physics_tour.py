dd9e077e23d514c4515df9deb96f26fd89efda2b
PAUL BERNARD <Paul.Bernard@inria.fr>
2026-09-17 16:52:19 +0200

    Demosaicing

--- examples/physics/demo_physics_tour.py
+++ examples/physics/demo_physics_tour.py
@@ -8,7 +8,7 @@ We restrict ourselves to operators where the signal is a 2D image. The full list
 
 """
 
-# sphinx_gallery_tags = ["Tomography", "Denoising", "Single-pixel"]
+# sphinx_gallery_tags = ["Tomography", "Denoising", "Single-pixel", "Demosaicing"]
 
 import torch
 
